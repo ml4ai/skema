31d0c240807465a061e0a1a9aab037abfe7a4aff
Free-Quarks <lieffers@email.arizona.edu>
2022-09-03 16:21:58 -0700

    added the ability to serialize

--- rust/src/bin/morae.rs
+++ rust/src/bin/morae.rs
@@ -4,13 +4,20 @@ use std::fs;
 
 use skema::Gromet; // This brings in the Gromet Data struct
 
-/* TODO: Recursive Function Netoworks? */
 /* TODO: Process data for generic type post deserialization. */
-
+/* TODO: Reserialize to make sure ingesting correctly. Including making tests.  */
+/* TODO: Write up latex documentation in docs. */
 fn main() {
-    // let path_example = "../data/gromet/examples/while2/while2--Gromet-FN-auto.json";
-    let path_example = "../data/epidemiology/CHIME/CHIME_SIR_model/gromet/FN_0.1.2/CHIME_SIR_while_loop--Gromet-FN-auto.json";
+    let path_example = "../data/gromet/examples/exp0/exp0--Gromet-FN-auto.json";
+    // let path_example = "../data/epidemiology/CHIME/CHIME_SIR_model/gromet/FN_0.1.2/CHIME_SIR_while_loop--Gromet-FN-auto_v2.json";
+
     let data = fs::read_to_string(path_example).expect("Unable to read file");
-    let res: Gromet = serde_json::from_str(&data).expect("Unable to parse"); // right now f32 works for all value types we currently have..
-    println!("{}", res.r#fn.b[0].function_type) // The fact these are 1 dimensional arrays makes accessing attributes ugly and more convolved than it should be.
+
+    let res: Gromet = serde_json::from_str(&data).expect("Unable to parse");
+
+    let res_serialized = serde_json::to_string(&res).unwrap();
+
+    println!("{} \n {}", res_serialized, data)
+
+    // println!("{}", res.r#fn.b[0].function_type)
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,11 @@
 // Stub for SKEMA library
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use strum_macros::Display; // used for macro on enums
 
 use std::string::ToString; // used for macro on enums
 
-// AST for the Gromet Data Structure
-#[derive(Deserialize)]
+/******** AST for the Gromet Data Structure ********/
+#[derive(Deserialize, Serialize)]
 #[serde(rename_all = "UPPERCASE")] // Allows variants to match to uppercase json values
 #[derive(strum_macros::Display)] // Allows variants to be printed as strings if needed
 pub enum FnType {
@@ -13,7 +13,7 @@ pub enum FnType {
     Import,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 #[serde(rename_all = "UPPERCASE")]
 #[derive(strum_macros::Display)]
 pub enum FunctionType {
@@ -25,127 +25,192 @@ pub enum FunctionType {
     Literal,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Value {
     pub value_type: String, // could be enum?
     pub value: f32,         // This is the generic problem
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct GrometBox {
     pub function_type: FunctionType,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub contents: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub value: Option<Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Vec<Metadata>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct GrometPort {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<u8>,
     #[serde(rename = "box")]
     pub r#box: u8,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Vec<Metadata>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct GrometWire {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
-    pub src: i32,
-    pub tgt: i32, // These can be negative?? Not natural numbers?
+    pub src: u8,
+    pub tgt: u8,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Vec<Metadata>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct GrometBoxLoop {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub condition: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub init: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub body: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Vec<Metadata>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct GrometBoxConditional {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub condition: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub body_if: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub body_else: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Vec<Metadata>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct FunctionNet {
     pub b: [GrometBox; 1],
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub opi: Option<Vec<GrometPort>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub opo: Option<Vec<GrometPort>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wopio: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub bf: Option<Vec<GrometBox>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub pif: Option<Vec<GrometPort>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub pof: Option<Vec<GrometPort>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wfopi: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wfl: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wff: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wfc: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wfopo: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub bl: Option<Vec<GrometBoxLoop>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub pil: Option<Vec<GrometPort>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub pol: Option<Vec<GrometPort>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wlopi: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wll: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wlf: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wlc: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wlopo: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wl_iiargs: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wl_ioargs: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wl_cargs: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub bc: Option<Vec<GrometBoxConditional>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub pic: Option<Vec<GrometPort>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub poc: Option<Vec<GrometPort>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wcopi: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wcl: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wcf: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wcc: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wcopo: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub wc_cargs: Option<Vec<GrometWire>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Vec<Metadata>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Attributes {
     #[serde(rename = "type")]
     pub r#type: FnType,
     pub value: FunctionNet,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Vec<Metadata>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Provenance {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub method: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub timestamp: Option<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Metadata {
-    pub metadata_type: Option<String>,   // Could be enum?
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub metadata_type: Option<String>, // Could be enum?
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub source_language: Option<String>, // Could be enum?
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub source_language_version: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub data_type: Option<String>, // Could be enum?
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub code_file_reference_uid: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub line_begin: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub line_end: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub col_begin: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub col_end: Option<u32>,
-    pub provenance: Provenance,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub provenance: Option<Provenance>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Gromet {
     pub name: String,
     #[serde(rename = "fn")]
     pub r#fn: FunctionNet,
-    pub attributes: Option<Vec<Attributes>>,
+    pub attributes: Vec<Attributes>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Vec<Metadata>>,
 }
 
