77d1506244a743277b3081a59fab663b00cc142f
Adarsh Pyarelal <adarsh@arizona.edu>
2022-09-27 15:42:36 -0700

    docstring extraction is now working

--- skema/skema-rs/get_comments/src/lib.rs
+++ skema/skema-rs/get_comments/src/lib.rs
@@ -1,95 +1,115 @@
 use nom::{
     branch::alt,
     bytes::complete::{is_not, tag, take_until, take_while},
-    character::complete::{anychar, none_of, not_line_ending, line_ending, space0, alpha1, alphanumeric0},
+    character::complete::{
+        alpha1, alphanumeric0, anychar, line_ending, none_of, not_line_ending, space0,
+    },
     character::is_alphanumeric,
-    combinator::{opt, recognize},
+    combinator::{opt, recognize, not, value},
     multi::{fold_many0, many0},
-    sequence::{pair, preceded, tuple, delimited},
+    sequence::{delimited, pair, preceded, tuple},
     IResult,
 };
-use std::collections::HashMap;
+use strum_macros::Display; // used for macro on enums // used for macro on enums
 use nom_locate::{position, LocatedSpan};
+use std::collections::HashMap;
 
 type Span<'a> = LocatedSpan<&'a str>;
 
-/// Whole line comments
-fn whole_line_comment(input: Span) -> IResult<Span, Span> {
-    delimited(tuple((space0, tag("#"))), not_line_ending, line_ending)(input)
+
+#[derive(Debug, Display, Clone)]
+enum Statement {
+    WholeLineComment(u32, String),
+    Docstring { object_name: String, contents: Vec<String> },
+    Other
 }
 
+
 /// Triple quoted strings
-fn triple_quoted_string(input:Span) -> IResult<Span, Span> {
-    alt((delimited(tag("'''"), take_until("'''"), tag("'''")),
-    delimited(tag("\"\"\""), take_until("\"\"\""), tag("\"\"\""))))(input)
+fn triple_quoted_string(input: Span) -> IResult<Span, Span> {
+    alt((
+        delimited(tag("'''"), take_until("'''"), tag("'''")),
+        delimited(tag("\"\"\""), take_until("\"\"\""), tag("\"\"\"")),
+    ))(input)
 }
 
 fn is_valid_name_character(c: char) -> bool {
     c.is_alphanumeric() || c == '_'
 }
 
-fn name(input:Span) -> IResult<Span, Span> {
+fn name(input: Span) -> IResult<Span, Span> {
     take_while(is_valid_name_character)(input)
 }
 
 
-#[derive(Debug, Default)]
-struct Docstring {
-    pub object_name: String,
-    pub contents: Vec<String>
-}
-
 #[derive(Debug, PartialEq, Default)]
 pub struct Comments {
-    pub lineno: usize,
-    pub whole_line_comments: HashMap<usize, String>,
-    pub docstrings: HashMap<String, Vec<String>>
+    pub whole_line_comments: Vec<(u32, String)>,
+    pub docstrings: HashMap<String, Vec<String>>,
 }
 
-#[derive(Debug)]
-enum Comment {
-    String,
-    Docstring,
+/// Whole line comments
+fn whole_line_comment(input: Span) -> IResult<Span, Statement> {
+    let (s, x) = delimited(tuple((space0, tag("#"))), not_line_ending, line_ending)(input)?;
+    let (s, pos) = position(s)?;
+    let line = pos.location_line() - 1;
+    Ok((s, Statement::WholeLineComment(line, x.to_string())))
 }
 
-
-fn docstring(input: Span) -> IResult<Span, Span> {
-    let (s, (function_name)) = tuple(space0, tag("def "), name, tag("("), take_until())
-    //preceded(tuple((tag("def "), name, tag("("), take_until("):"))), triple_quoted_string)(input)
+fn docstring(input: Span) -> IResult<Span, Statement> {
+    let mut func_declaration = delimited(
+        tuple((space0, tag("def "))),
+        name,
+        tuple((tag("("), take_until("):"), tag("):"), line_ending, space0)),
+    );
+    let (input, (func_name, docstring_contents, _, _)) = tuple((func_declaration, triple_quoted_string, space0, line_ending))(input)?;
+    let object_name = func_name.to_string();
+    let contents: Vec<String> = docstring_contents.to_string().split('\n').map(|x| {x.to_string()}).collect();
+    Ok((input, Statement::Docstring { object_name, contents }))
 }
 
-
-fn comment(input: Span) -> IResult<Span, Span> {
+fn comment(input: Span) -> IResult<Span, Statement> {
     alt((whole_line_comment, docstring))(input)
 }
 
+fn other(input: Span) -> IResult<Span, Statement> {
+    let (s, _) = tuple((not_line_ending, line_ending))(input)?;
+    Ok((s, Statement::Other))
+}
+
+fn statement(input: Span) -> IResult<Span, Statement> {
+    alt((comment, other))(input)
+}
+
+
 fn comments(input: Span) -> IResult<Span, Comments> {
-    fold_many0(
-        comment,
-        Comments::default,
-        |mut acc: Comments, item| {
-            acc.lineno += 1;
-            acc.whole_line_comments.insert(acc.lineno, item.to_string());
-            acc
-        },
-    )(input)
+    fold_many0(statement, Comments::default, |mut acc: Comments, item| {
+        match item {
+            Statement::WholeLineComment(line, x) => {
+                acc.whole_line_comments.push((line, x));
+            }
+            Statement::Docstring {object_name, contents} => {
+                acc.docstrings.insert(object_name, contents);
+            }
+            _ => ()
+        }
+        acc
+    })(input)
 }
 
 fn parse(input: Span) -> IResult<Span, Comments> {
     let (remaining_input, matched) = comments(input)?;
     let (s, pos) = position(remaining_input)?;
-    dbg!(pos);
+    //dbg!(pos);
 
-    Ok((
-        remaining_input,
-        matched
-    ))
+    Ok((remaining_input, matched))
 }
 
 #[test]
 fn test_parser() {
-    let contents = std::fs::read_to_string("tests/data/python_example.py").unwrap();
+    let contents = std::fs::read_to_string("../../../data/epidemiology/CHIME/CHIME_SIR_model/code/CHIME_SIR.py").unwrap();
+    //let contents = std::fs::read_to_string("tests/data/python_example.py").unwrap();
     let span = Span::new(&contents);
-    let state = comments(span);
-    dbg!(state);
+    let (_, comments) = comments(span).unwrap();
+    dbg!(comments);
 }
--- skema/skema-rs/get_comments/tests/data/python_example.py
+++ skema/skema-rs/get_comments/tests/data/python_example.py
@@ -1,3 +1,12 @@
-# This is a comment
-# This is another comment
-"""This is a docstring"""
+# This is the first comment
+
+# This is the second comment
+
+def func1(x, y):
+    """This is a docstring
+    Arg
+    Return
+    """
+    pass
+
+# This is the third comment
