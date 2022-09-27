bf6a7da2f74f40bf9ff54e9e9fb7f928460f246b
Adarsh Pyarelal <adarsh@arizona.edu>
2022-09-26 17:02:06 -0700

    wip

--- skema/skema-rs/get_comments/src/lib.rs
+++ skema/skema-rs/get_comments/src/lib.rs
@@ -1,8 +1,9 @@
 use nom::{
     branch::alt,
     bytes::complete::{is_not, tag, take_until, take_while},
-    character::complete::{anychar, none_of, not_line_ending, space0},
-    combinator::opt,
+    character::complete::{anychar, none_of, not_line_ending, line_ending, space0},
+    combinator::{opt, recognize},
+    multi::fold_many0,
     sequence::{pair, preceded, tuple},
     IResult,
 };
@@ -12,30 +13,40 @@ pub struct Comment {
     pub comment: String,
 }
 
-fn parse(input: &str) -> IResult<&str, Comment> {
-    let (remaining_input, (_, _, matched)) =
-        tuple((opt(space0), tag("#"), not_line_ending))(input)?;
+#[derive(Debug, Default)]
+struct ParserState {
+    pub lineno: usize,
+    pub comments: Vec<String>
+}
+
+fn whole_line_comment(input: &str) -> IResult<&str, &str> {
+    preceded(pair(space0, tag("#")), recognize(not_line_ending))(input)
+}
+
+fn comments(input: &str) -> IResult<&str, ParserState> {
+    fold_many0(
+        whole_line_comment,
+        ParserState::default,
+        |mut acc: ParserState, item| {
+            acc.lineno += 1;
+            acc.comments.push(item.to_string());
+            acc
+        },
+    )(input)
+}
+
+fn parse(input: &str) -> IResult<&str, ParserState> {
+    let (remaining_input, matched) = comments(input)?;
+
     Ok((
         remaining_input,
-        Comment {
-            comment: matched.to_string(),
-        },
+        matched
     ))
 }
 
 #[test]
 fn test_parser() {
-    let contents = std::fs::read_to_string(
-        "tests/data/python_example.py",
-    )
-    .unwrap();
-    assert_eq!(
-        parse(&contents),
-        Ok((
-            "\n",
-            Comment {
-                comment: " This is a comment".to_string()
-            }
-        ))
-    );
+    let contents = std::fs::read_to_string("tests/data/python_example.py").unwrap();
+    let state = comments(&contents);
+    dbg!(state);
 }
