98d2c201273cb4ea05f83ef2d19c0095c0732fba
Adarsh Pyarelal <adarsh@arizona.edu>
2022-10-10 09:58:45 -0700

    wip

--- skema/skema-rs/mathml/src/lib.rs
+++ skema/skema-rs/mathml/src/lib.rs
@@ -6,7 +6,9 @@ use ast::{
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_until},
+    character::complete::multispace0,
     combinator::map,
+    error::ParseError,
     multi::many0,
     sequence::{delimited, pair, preceded},
     IResult,
@@ -16,6 +18,17 @@ use nom_locate::{position, LocatedSpan};
 use std::collections::HashMap;
 
 type Span<'a> = LocatedSpan<&'a str>;
+
+/// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
+/// trailing whitespace, returning the output of `inner`.
+fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(
+    inner: F,
+) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+where
+    F: Fn(&'a str) -> IResult<&'a str, O, E>,
+{
+    delimited(multispace0, inner, multispace0)
+}
 
 fn mi(input: Span) -> IResult<Span, MathExpression> {
     let (s, element) = delimited(tag("<mi>"), take_until("</mi>"), tag("</mi>"))(input)?;
