2e837921a43d88e19c9e0bb66cd936d50f20938b
Adarsh Pyarelal <adarsh@arizona.edu>
2022-10-10 15:56:50 -0700

    Implemented skipping whitespace

--- skema/skema-rs/mathml/src/lib.rs
+++ skema/skema-rs/mathml/src/lib.rs
@@ -8,10 +8,8 @@ use nom::{
     bytes::complete::{tag, take_until},
     character::complete::multispace0,
     combinator::map,
-    error::ParseError,
     multi::many0,
     sequence::{delimited, pair, preceded},
-    IResult,
 };
 use nom_locate::{position, LocatedSpan};
 
@@ -19,56 +17,107 @@ use std::collections::HashMap;
 
 type Span<'a> = LocatedSpan<&'a str>;
 
-/// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
-/// trailing whitespace, returning the output of `inner`.
-fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(
-    inner: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+#[derive(Debug, PartialEq)]
+pub struct ParseError<'a> {
+    span: Span<'a>,
+    message: String,
+}
+
+impl<'a> ParseError<'a> {
+    pub fn new(message: String, span: Span<'a>) -> Self {
+        Self { message, span }
+    }
+
+    pub fn span(&self) -> &Span {
+        &self.span
+    }
+
+    pub fn line(&self) -> u32 {
+        self.span().location_line()
+    }
+
+    pub fn offset(&self) -> usize {
+        self.span().location_offset()
+    }
+}
+
+impl<'a> nom::error::ParseError<Span<'a>> for ParseError<'a> {
+    fn from_error_kind(input: Span<'a>, kind: nom::error::ErrorKind) -> Self {
+        Self::new(format!("parse error {:?}", kind), input)
+    }
+
+    fn append(_input: Span<'a>, _kind: nom::error::ErrorKind, other: Self) -> Self {
+        other
+    }
+
+    fn from_char(input: Span<'a>, c: char) -> Self {
+        Self::new(format!("unexpected character '{}'", c), input)
+    }
+}
+
+impl<'a> nom::error::ContextError<Span<'a>> for ParseError<'a> {
+    fn add_context(input: Span<'a>, ctx: &'static str, other: Self) -> Self {
+        let message = format!("{}: {}", ctx, other.message);
+        ParseError::new(message, input)
+    }
+}
+
+pub type IResult<'a, O> = nom::IResult<Span<'a>, O, ParseError<'a>>;
+
+/// A combinator that takes a parser `inner` and produces a parser that also consumes both leading
+/// and trailing whitespace, returning the output of `inner`.
+fn ws<'a, F: 'a, O>(inner: F) -> impl FnMut(Span<'a>) -> IResult<O>
 where
-    F: Fn(&'a str) -> IResult<&'a str, O, E>,
+    F: FnMut(Span<'a>) -> IResult<O>,
 {
     delimited(multispace0, inner, multispace0)
 }
 
-fn mi(input: Span) -> IResult<Span, MathExpression> {
-    let (s, element) = delimited(tag("<mi>"), take_until("</mi>"), tag("</mi>"))(input)?;
-    //let element = element.to_string();
+fn mi(input: Span) -> IResult<MathExpression> {
+    let (s, element) = ws(delimited(tag("<mi>"), take_until("</mi>"), tag("</mi>")))(input)?;
     Ok((s, Mi(&element)))
 }
 
-fn mo(input: Span) -> IResult<Span, MathExpression> {
-    let (s, element) = delimited(tag("<mo>"), take_until("</mo>"), tag("</mo>"))(input)?;
-    //let element = element.to_string();
+fn mo(input: Span) -> IResult<MathExpression> {
+    let (s, element) = ws(delimited(tag("<mo>"), take_until("</mo>"), tag("</mo>")))(input)?;
     Ok((s, Mo(&element)))
 }
 
-fn mrow(input: Span) -> IResult<Span, MathExpression> {
-    let (s, elements) = delimited(tag("<mrow>"), many0(math_expression), tag("</mrow>"))(input)?;
+fn mrow(input: Span) -> IResult<MathExpression> {
+    let (s, elements) = ws(delimited(
+        tag("<mrow>"),
+        many0(math_expression),
+        tag("</mrow>"),
+    ))(input)?;
     Ok((s, Mrow(elements)))
 }
 
-fn math_expression(input: Span) -> IResult<Span, MathExpression> {
-    alt((mi, mo, mrow))(input)
+fn math_expression(input: Span) -> IResult<MathExpression> {
+    ws(alt((mi, mo, mrow)))(input)
 }
 
-fn mfrac(input: Span) -> IResult<Span, MathExpression> {
+fn mfrac(input: Span) -> IResult<MathExpression> {
     let (s, result) = map(
-        delimited(
+        ws(delimited(
             tag("<mfrac>"),
             pair(math_expression, math_expression),
             tag("</mfrac>"),
-        ),
+        )),
         |(numerator, denominator)| Mfrac(Box::new(numerator), Box::new(denominator)),
     )(input)?;
     Ok((s, result))
 }
 
-fn math(input: Span) -> IResult<Span, Math> {
-    let (s, elements) = delimited(tag("<math>"), many0(math_expression), tag("</math>"))(input)?;
+fn math(input: Span) -> IResult<Math> {
+    let (s, elements) = ws(delimited(
+        tag("<math>"),
+        many0(math_expression),
+        tag("</math>"),
+    ))(input)?;
     Ok((s, Math { content: elements }))
 }
 
-fn parse(input: &str) -> IResult<Span, Math> {
+fn parse(input: &str) -> IResult<Math> {
     let span = Span::new(input);
     let (remaining, math) = math(span)?;
     Ok((remaining, math))
@@ -107,9 +156,16 @@ fn test_math_expression() {
 #[test]
 fn test_parser() {
     assert_eq!(
-        parse("<math><mrow><mo>-</mo><mi>b</mi></mrow></math>")
-            .unwrap()
-            .1,
+        parse(
+            "<math>
+                <mrow>
+                    <mo>-</mo>
+                    <mi>b</mi>
+                </mrow>
+            </math>"
+        )
+        .unwrap()
+        .1,
         Math {
             content: vec![Mrow(vec![Mo("-"), Mi("b")])]
         }
