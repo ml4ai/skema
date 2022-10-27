8883c542fa080c81de7c25d0effb782cd7e5fae9
Keith Alcock <github@keithalcock.com>
2022-10-27 00:10:05 -0700

    Start webapp

--- skema/text_reading/text_reading/webapp/app/controllers/HomeController.scala
+++ skema/text_reading/text_reading/webapp/app/controllers/HomeController.scala
@@ -16,6 +16,7 @@ import org.clulab.aske.automates.data.CosmosJsonDataLoader
 import org.clulab.aske.automates.data.ScienceParsedDataLoader
 import org.clulab.aske.automates.scienceparse.ScienceParseClient
 import org.clulab.aske.automates.serializer.AutomatesJSONSerializer
+import org.clulab.aske.automates.utils.{AlignmentJsonUtils, DisplayUtils}
 import org.clulab.grounding.{SVOGrounder, WikidataGrounder, sparqlWikiResult}
 import org.clulab.odin.serialization.json.JSONSerializer
 import upickle.default._
@@ -25,8 +26,6 @@ import ujson.json4s.Json4sJson
 import ujson.play.PlayJson
 import org.clulab.odin.{EventMention, Mention, RelationMention, TextBoundMention}
 import org.clulab.processors.{Document, Sentence}
-import org.clulab.utils.AlignmentJsonUtils.SeqOfGlobalVariables
-import org.clulab.utils.{AlignmentJsonUtils, DisplayUtils}
 import org.slf4j.{Logger, LoggerFactory}
 import org.json4s
 import org.ml4ai.grounding.{GroundingCandidate, MiraEmbeddingsGrounder}
--- skema/text_reading/text_reading/webapp/build.sbt
+++ skema/text_reading/text_reading/webapp/build.sbt
@@ -1,15 +1,15 @@
-name := """webapp"""
-scalaVersion := "2.12.4"
+name := "webapp"
 
 libraryDependencies ++= Seq(
-  "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
+  "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
+  guice
 )
 
 //libraryDependencies ++= Seq(
 //  "org.scalatestplus.play" %% "scalatestplus-play" % "5.1" % "test"
 //)
 // Adds additional packages into Twirl
-//TwirlKeys.templateImports += "org.clulab.controllers._"
+TwirlKeys.templateImports += "org.clulab.controllers._"
 
 // Adds additional packages into conf/routes
-// play.sbt.routes.RoutesKeys.routesImport += "org.clulab.binders._"
+play.sbt.routes.RoutesKeys.routesImport += "org.clulab.binders._"
