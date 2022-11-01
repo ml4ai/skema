80791915fe0dc995cb698722d5cfcc20ffee01fb
Isha Talegaonkar <ishatalegaonkar@Ishas-MacBook-Air.local>
2022-11-01 13:50:07 -0600

    made changes

--- skema/moviz/scripts/code_summarization/utils/dataset_scores.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import sys
-
-import utils.utils as utils
-
-clfs_path = utils.CODE_CORPUS / "results" / sys.argv[1]
-classifications = pickle.load(open(clfs_path, "rb"))
-scores = utils.score_classifier(classifications)
-print("(P, R, F1) = ({}, {}, {})".format(*scores))
