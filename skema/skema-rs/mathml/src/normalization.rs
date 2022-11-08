9b709944d67cf7f417e938f4d7676703e99f3045
Adarsh Pyarelal <adarsh@arizona.edu>
2022-11-08 15:09:24 -0700

    wip

--- skema/skema-rs/mathml/src/normalization.rs
+++ skema/skema-rs/mathml/src/normalization.rs
@@ -1,24 +1,35 @@
-use crate::parsing::parse;
 use crate::ast::{
-    Math,
-    MathExpression,
+    Math, MathExpression,
     MathExpression::{
         Mfrac, Mi, Mn, Mo, MoLine, Mover, Mrow, Mspace, Msqrt, Mstyle, Msub, Msubsup, Msup, Mtext,
         Munder,
     },
 };
-use std::rc::Rc;
-
+use crate::parsing::parse;
 
 impl MathExpression {
-    fn collapse_subscripts(&self) -> Option<MathExpression> {
+    fn collapse_subscripts(&mut self) {
         match self {
             Msub(base, subscript) => {
                 let mut combined = String::from(&base.get_string_repr());
                 combined.push_str(&subscript.get_string_repr());
-                Some(Mi(combined))
+                *self = Mi(combined);
+                //Some(Mi(combined))
             }
-            _ => None
+            //Mn(x) => add_to_graph_0(graph, parent_index, x),
+            //Msqrt(x) => add_to_graph_n!(graph, parent_index, "msqrt", x),
+            //Msup(x1, x2) => add_to_graph_n!(graph, parent_index, "msup", x1, x2),
+            //Msub(x1, x2) => add_to_graph_n!(graph, parent_index, "msub", x1, x2),
+            //Mfrac(x1, x2) => add_to_graph_n!(graph, parent_index, "mfrac", x1, x2),
+            //Mrow(xs) => add_to_graph_many0(graph, parent_index, "mrow", xs),
+            //Munder(xs) => add_to_graph_many0(graph, parent_index, "munder", xs),
+            //Mover(xs) => add_to_graph_many0(graph, parent_index, "mover", xs),
+            //Msubsup(xs) => add_to_graph_many0(graph, parent_index, "msubsup", xs),
+            //Mtext(x) => add_to_graph_0(graph, parent_index, x),
+            //Mstyle(xs) => add_to_graph_many0(graph, parent_index, "mstyle", xs),
+            //Mspace(x) => add_to_graph_0(graph, parent_index, x),
+            //MoLine(x) => add_to_graph_0(graph, parent_index, x),
+            _ => (),
         }
     }
 
@@ -26,23 +37,21 @@ impl MathExpression {
         match self {
             Mi(x) => x.to_string(),
             Mo(x) => x.to_string(),
-            Mrow(xs) => {
-                xs.iter().map(|x| x.get_string_repr()).collect::<Vec<String>>().join("")
-            }
+            Mrow(xs) => xs
+                .iter()
+                .map(|x| x.get_string_repr())
+                .collect::<Vec<String>>()
+                .join(""),
             _ => {
                 panic!("Unhandled type!");
             }
         }
     }
-
 }
 
-fn normalize(math: Math) {
-    for mut expr in math.content {
-        let mut storage = Vec::<String>::new();
-        if let Some(e) = &mut expr.collapse_subscripts() {
-            expr = *e;
-        }
+fn normalize(math: &mut Math) {
+    for expr in &mut math.content {
+        expr.collapse_subscripts();
     }
 }
 
@@ -50,12 +59,38 @@ fn normalize(math: Math) {
 fn test_get_string_repr() {
     assert_eq!(Mi("t".to_string()).get_string_repr(), "t".to_string());
     assert_eq!(Mo("+".to_string()).get_string_repr(), "+".to_string());
-    assert_eq!(Mrow(vec![Mi("t".into()), Mo("+".into()), Mi("1".to_string())]).get_string_repr(), "t+1".to_string());
+    assert_eq!(
+        Mrow(vec![Mi("t".into()), Mo("+".into()), Mi("1".to_string())]).get_string_repr(),
+        "t+1".to_string()
+    );
 }
 
 #[test]
 fn test_subscript_collapsing() {
-    let expr = Msub(Box::new(Mi("S".to_string())), Box::new(Mrow(vec![Mi("t".to_string()), Mo("+".to_string()), Mi("1".to_string())])));
-    let mut storage = Vec::<String>::new();
-    assert_eq!(expr.collapse_subscripts().unwrap(), Mi("St+1".to_string()));
+    let mut expr = Msub(
+        Box::new(Mi("S".to_string())),
+        Box::new(Mrow(vec![
+            Mi("t".to_string()),
+            Mo("+".to_string()),
+            Mi("1".to_string()),
+        ])),
+    );
+    expr.collapse_subscripts();
+    assert_eq!(expr, Mi("St+1".to_string()));
+}
+
+#[test]
+fn test_normalize() {
+    let mut math = Math {
+        content: vec![Msub(
+            Box::new(Mi("S".to_string())),
+            Box::new(Mrow(vec![
+                Mi("t".to_string()),
+                Mo("+".to_string()),
+                Mi("1".to_string()),
+            ])),
+        )],
+    };
+    normalize(&mut math);
+    assert_eq!(&math.content[0], &Mi("St+1".to_string()));
 }
