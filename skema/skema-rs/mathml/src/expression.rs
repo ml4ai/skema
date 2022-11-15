2f377008dbd496f6c2fbd99debdccb0f28cbbb11
ualiangzhang <liangzh@arizona.edu>
2022-11-15 11:28:54 -0700

    update expression.rs

--- skema/skema-rs/mathml/src/expression.rs
+++ skema/skema-rs/mathml/src/expression.rs
@@ -17,17 +17,34 @@ enum Expr {
     Expression { op: Operator, args: Vec<Expr> },
 }
 
+#[derive(Debug, PartialEq, Clone)]
+struct PreExp {
+    op: Operator,
+    args: Vec<Expr>,
+}
+
 impl MathExpression {
-    fn to_expr(&self, &mut pre: &mut Expr) -> Expr {
+    fn to_expr(self, mut pre: &mut PreExp) {
         match self {
             Mi(x) => {
-                Expr::Atom(Atom::Identifier(x.clone()))
-            },
-            Mo(x) => Expr::Atom(Atom::Operator(x.clone())),
-            Mrow(xs) => Expr::Expression {
-                op: Operator::Add,
-                args: Vec::<Expr>::new(),
-            },
+                pre.args.push(Expr::Atom(Atom::Identifier(x.to_string())));
+            }
+            Mo(x) => {
+                match x {
+                    // Operator::Other(_) => { pre.op = x.clone(); }
+                    _ => { pre.op = x.clone(); }
+                }
+            }
+            Mrow(xs) => {
+                let mut pre_exp = PreExp {
+                    op: Operator::Other("".to_string()),
+                    args: Vec::<Expr>::new(),
+                };
+                for mut x in xs {
+                    &x.to_expr(&mut pre_exp);
+                }
+                pre.args.push(Expr::Expression { op: pre_exp.op, args: pre_exp.args }.clone());
+            }
             _ => {
                 panic!("Unhandled type!");
             }
@@ -44,22 +61,30 @@ fn test_to_expr() {
         Mo(Operator::Add),
         Mi("b".to_string()),
     ]);
-    let mut pre_exp = Expr::Expression {
-        op: Operator::Other("".to_string()),
+    let mut pre_exp = PreExp {
+        op: Operator::Other("root".to_string()),
         args: Vec::<Expr>::new(),
     };
-    match pre_exp {
-        Expr::Expression(ref op1, ref args) => println!("value!"),
-        _ => println!("Something else"),
-    }
-    assert_eq!(
-        math_expression.to_expr(&mut pre_exp),
-        Expr::Expression {
-            op: Operator::Add,
-            args: vec![
-                Expr::Atom(Atom::Identifier("a".to_string())),
-                Expr::Atom(Atom::Identifier("b".to_string())),
-            ],
+
+    math_expression.to_expr(&mut pre_exp);
+    match &pre_exp.args[0] {
+        Expr::Atom(_) => {}
+        Expr::Expression { op, args } => {
+            assert_eq!(op, &Operator::Add);
+            assert_eq!(args[0], Expr::Atom(Atom::Identifier("a".to_string())));
+            assert_eq!(args[1], Expr::Atom(Atom::Identifier("b".to_string())));
+            println!("Success!");
         }
-    );
+    }
+
+    // assert_eq!(
+    //     pre_exp.args[1],
+    //     Expr::Expression {
+    //         op: Operator::Add,
+    //         args: vec![
+    //             Expr::Atom(Atom::Identifier("a".to_string())),
+    //             Expr::Atom(Atom::Identifier("b".to_string())),
+    //         ],
+    //     }
+    // );
 }
