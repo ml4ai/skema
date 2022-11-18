f78ef9f8b1f775681f333f542901bd01db314a7c
Adarsh Pyarelal <adarsh@arizona.edu>
2022-11-18 14:04:20 -0700

    test fix

--- skema/skema-rs/skema/tests/test_service.rs
+++ skema/skema-rs/skema/tests/test_service.rs
@@ -1,11 +1,11 @@
 use actix_web::{test, web, App};
-use skema::services::comment_extraction::{get_comments, CommentExtractionRequest};
+use skema::services::comment_extraction::{get_comments, CommentExtractionRequest, Language};
 
 #[actix_web::test]
 async fn test_get_comments() {
     let app = test::init_service(App::new().service(get_comments)).await;
     let payload = CommentExtractionRequest::new(
-        "python",
+        Language::Python,
         std::fs::read_to_string("../comment_extraction/tests/data/CHIME_SIR.py").unwrap(),
     );
     let request = test::TestRequest::get()
