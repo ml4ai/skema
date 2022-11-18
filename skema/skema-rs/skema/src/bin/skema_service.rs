ae73b8ae49ee7958b327beedecd22c9d47638eff
Adarsh Pyarelal <adarsh@arizona.edu>
2022-11-18 12:55:32 -0700

    api docs minimal example

--- skema/skema-rs/skema/src/bin/skema_service.rs
+++ skema/skema-rs/skema/src/bin/skema_service.rs
@@ -1,12 +1,36 @@
 use actix_web::{web, App, HttpServer};
-use skema::services::comment_extraction::get_comments;
+use skema::services::comment_extraction::{get_comments, CommentExtractionRequest, CommentExtractionResponse, SingleLineComment, Docstring};
+use utoipa_swagger_ui::SwaggerUi;
+use utoipa::OpenApi;
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(skema::services::comment_extraction::get_comments),
+        components(
+            schemas(
+                CommentExtractionRequest,
+                CommentExtractionResponse,
+                SingleLineComment,
+                Docstring
+            )
+        ),
+        tags(
+            (name = "SKEMA", description = "SKEMA web services.")
+        ),
+    )]
+    struct ApiDoc;
+
+    let openapi = ApiDoc::openapi();
+    HttpServer::new(move || {
         App::new()
-            .route("/get_comments", web::get().to(get_comments))
+            .service(get_comments)
+            .service(
+                SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-doc/openapi.json", openapi.clone()),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- skema/skema-rs/skema/src/services/comment_extraction.rs
+++ skema/skema-rs/skema/src/services/comment_extraction.rs
@@ -1,12 +1,54 @@
 use serde::{Deserialize, Serialize};
-use actix_web::{web, HttpResponse};
+use actix_web::{get, web, HttpResponse};
 use comment_extraction::languages::python::get_comments_from_string as get_python_comments;
+use comment_extraction::languages::python::Comments;
+use utoipa::{ToSchema, IntoParams};
+use utoipa;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
 pub struct CommentExtractionRequest {
+    #[schema(example = "Python")]
     pub language: String,
     pub code: String,
 }
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct SingleLineComment {
+    line: u32,
+    contents: String
+}
+
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct  Docstring {
+        object_name: String,
+        contents: Vec<String>,
+}
+
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct CommentExtractionResponse {
+    single_line_comments: Vec<SingleLineComment>,
+    docstrings: Vec<Docstring>
+}
+
+impl CommentExtractionResponse {
+    fn new(comments: Comments) -> Self {
+        let mut single_line_comments = Vec::new();
+        for (line, comment) in comments.comments.iter() {
+            single_line_comments.push(SingleLineComment {line: *line, contents: comment.to_string()});
+        }
+        let mut docstrings = Vec::new();
+        for (object_name, contents) in comments.docstrings {
+            docstrings.push(Docstring {object_name: object_name, contents: contents});
+        }
+        Self {
+            single_line_comments: single_line_comments,
+            docstrings: docstrings
+        }
+    }
+}
+
 
 impl CommentExtractionRequest {
     pub fn new(language: &str, code: String) -> Self {
@@ -16,6 +58,14 @@ impl CommentExtractionRequest {
         }
     }
 }
+
+#[utoipa::path(
+    request_body = CommentExtractionRequest,
+    responses(
+        (status = 200, description = "Get comments", body = CommentExtractionResponse)
+    )
+)]
+#[get("/get_comments")]
 pub async fn get_comments(payload: web::Json<CommentExtractionRequest>) -> HttpResponse {
     HttpResponse::Ok().json(web::Json(get_python_comments(&payload.code)))
 }
