747e50a49870b532aa7d5b32bafb653db340b21f
Adarsh Pyarelal <adarsh@email.arizona.edu>
2022-11-22 10:19:08 -0700

    Dockerization of skema-rs REST API (#33)
    This PR Dockerizes the skema-rs REST API. 
    
    - Adds a `Dockerfile` and `docker-compose.yml` file, and updates the
    `README.md` files for the `skema-rs` and `skema` directories.
    - Adds a `/ping` endpoint to check whether the REST API is running and
    accessible.
    - Adds command-line arguments for the service to set the host and port.
    
    Co-authored-by: Adarsh Pyarelal <adarsh@arizona.edu>

--- skema/skema-rs/skema/README.md
+++ skema/skema-rs/skema/README.md
@@ -1,17 +1,4 @@
-# SKEMA
+# skema
 
-This package contains code that is very specific to the SKEMA project.
-
-## skema_service
-
-This is a web service to expose the functionality of the SKEMA Rust components
-via REST APIs. To run the program, do:
-
-```
-cargo run --bin skema_service
-```
-
-Currently, there is one API endpoint implemented: comment extraction.
-
-OpenAPI documentation for the services can be found at
-`http://localhost:8080/api-docs/`.
+This package contains code for working with GroMEt, as well as a web service to
+expose SKEMA functionality via a REST API.
--- skema/skema-rs/skema/src/bin/skema_service.rs
+++ skema/skema-rs/skema/src/bin/skema_service.rs
@@ -1,13 +1,38 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, HttpResponse, get};
 use skema::services::comment_extraction::{
     get_comments, CommentExtractionRequest, CommentExtractionResponse, Docstring, Language,
     SingleLineComment,
 };
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
+use clap::Parser;
+use std::net::{IpAddr, Ipv4Addr};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    /// Host
+    #[arg(long, default_value_t = String::from("localhost"))]
+    host: String,
+
+    /// Port
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16
+}
+
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Ping")
+    )
+)]
+
+#[get("/ping")]
+pub async fn ping() -> HttpResponse {
+    HttpResponse::Ok().body("The SKEMA Rust web services are running.")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
     #[derive(OpenApi)]
     #[openapi(
         paths(skema::services::comment_extraction::get_comments),
@@ -27,12 +52,17 @@ async fn main() -> std::io::Result<()> {
     struct ApiDoc;
 
     let openapi = ApiDoc::openapi();
+
+    let args = Cli::parse();
     HttpServer::new(move || {
-        App::new().service(get_comments).service(
-            SwaggerUi::new("/api-docs/{_:.*}").url("/api-doc/openapi.json", openapi.clone()),
-        )
+        App::new()
+            .service(get_comments)
+            .service(
+                SwaggerUi::new("/api-docs/{_:.*}").url("/api-doc/openapi.json", openapi.clone()),
+            )
+            .service(ping)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((args.host, args.port))?
     .run()
     .await
 }
