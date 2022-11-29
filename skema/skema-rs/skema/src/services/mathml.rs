c62b632e250172dbbd116c7e246a02d82bf29c19
Adi <abanerjee@email.arizona.edu>
2022-11-28 22:39:52 -0700

    Added parsing (WIP)

--- skema/skema-rs/skema/src/services/mathml.rs
+++ skema/skema-rs/skema/src/services/mathml.rs
@@ -1,28 +1,37 @@
 use actix_web::{get, web, HttpResponse};
 use std::string::String;
-use mathml::parsing;
+use mathml::parsing::parse;
 use serde::{Deserialize, Serialize};
 use utoipa;
 use utoipa::ToSchema;
+use petgraph::dot::{Config, Dot, Graph};
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
-pub struct MatmlVisualizeRequest {
-    pub xmlString: String,
+pub struct MathmlParseRequest {
+    pub input: String,
 }
 
-impl MatmlVisualizeRequest {
-    pub fn new(xmlString: String) -> Self {
-        Self { xmlString }
+impl MathmlParseRequest {
+    pub fn new(input: String) -> Self {
+        Self { input }
     }
 }
+
 
 #[utoipa::path(
     request_body = MatmlVisualizeRequest,
     responses(
-        (status = 200, description = "Visualize XML", body = String)
+        (status = 200, description = "Visualize XML", body = Dot<&Graph<String, u32>>)
     )
 )]
-#[get("/visualize_xml")]
-pub async fn visualize_xml(payload: web::Json<MatmlVisualizeRequest>) -> HttpResponse {
-    HttpResponse::Ok().body("Hello")
+#[get("/mathml_parse")]
+pub async fn visualize_xml(payload: web::Json<MathmlParseRequest>) -> HttpResponse {
+    let contents = &payload.input;
+    let (_, mut math) = parse(&contents).expect(format!("Unable to parse file {contents}!").as_str());
+    math.normalize();
+
+    let g = math.to_graph(); 
+    let dotRepresentation = Dot::with_config(&g, &[Config::EdgeNoLabel]);
+    
+    return HttpResponse::Ok().json(web::Json(dotRepresentation));
 }
