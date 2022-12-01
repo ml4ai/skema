ce69722d18e2a6080603d710b7dbf242a8addd90
Sushma Akoju <8979477+sushmaakoju@users.noreply.github.com>
2022-11-30 21:52:25 -0700

    add bruteforce search for hyperparameters tuning

--- skema/text_reading/text_reading/grounding/src/test/scala/org/ml4ai/grounding/TestMiraEmbeddingsGrounder.scala
+++ skema/text_reading/text_reading/grounding/src/test/scala/org/ml4ai/grounding/TestMiraEmbeddingsGrounder.scala
@@ -1,38 +1,39 @@
 package org.ml4ai.grounding
 
+import breeze.linalg._
 import com.typesafe.config.ConfigFactory
 import org.clulab.utils.FileUtils
 import org.json4s.JsonDSL.boolean2jvalue
 import org.ml4ai.skema.common.test.Test
 import org.scalatest.OptionValues._
+import breeze.stats.distributions._
 
 class TestMiraEmbeddingsGrounder extends Test {
 
-  // Lazily load the grounder. We assume it's state and behavior is immutable
-  // So we can build it once and reuse it as necessary in the suite
-  val lambda = 10
-  val alpha = 0.25.toFloat
+//  // Lazily load the grounder. We assume it's state and behavior is immutable
+//  // So we can build it once and reuse it as necessary in the suite
+
   val miraEmbeddingsGrounder: MiraEmbeddingsGrounder = {
     val config = ConfigFactory.load().getConfig("Grounding")
     val ontologyPath = config.getString("ontologyPath")
     // val embeddingsPath = config.getString("embeddingsPath")
 
-    MiraEmbeddingsGrounder(ontologyPath, None, lambda, alpha)
+    MiraEmbeddingsGrounder(ontologyPath, None, 10, 0.25.toFloat)
   }
 
-  def correctGrounding(shouldable: Shouldable, text:String, groundingID:String): Unit = {
+  def correctGrounding(shouldable: Shouldable, text: String, groundingID: String): Unit = {
     shouldable should s"ground $text to the grounding concept with id $groundingID" in {
       val groundedConcept = miraEmbeddingsGrounder.ground(text)
 
-      groundedConcept.value.id should be (groundingID)
+      groundedConcept.value.id should be(groundingID)
     }
   }
 
   behavior of "Exact matches"
 
   // These are sanity checks. The input text is exactly the description of the entity, so it should match perfectly with the corresponding concept
-  correctGrounding(failingTest, "COVID-19","doid:0080600")
-  correctGrounding(passingTest, "cell part","caro:0000014")
+  correctGrounding(failingTest, "COVID-19", "doid:0080600")
+  correctGrounding(passingTest, "cell part", "caro:0000014")
   // This is a slightly harder entity to ground
   correctGrounding(failingTest, "junctional epidermolysis bullosa non-Herlitz type", "doid:0060738")
 
@@ -40,8 +41,8 @@ class TestMiraEmbeddingsGrounder extends Test {
 
   // These matches are to synonyms. Are not exactly the same, but should be handled correctly by the grounding algorithm
   correctGrounding(failingTest, "junctional epidermolysis bullosa generalisata mitis", "doid:0060738")
-  correctGrounding(passingTest, "covid19","doid:0080600")
-  correctGrounding(passingTest, "s-block compounds","chebi:33674")
+  correctGrounding(passingTest, "covid19", "doid:0080600")
+  correctGrounding(passingTest, "s-block compounds", "chebi:33674")
 
   behavior of "Accuracy of the matches"
 
@@ -67,21 +68,51 @@ class TestMiraEmbeddingsGrounder extends Test {
 
       accuracy should be >= 0.65f // TODO: .7f is the goal
 
-      val predictions1 =
-        for {
-          (text, groundingId) <- targets
-        } yield miraEmbeddingsGrounder.ground(text) match {
-          case Some(concept) => concept.id + "," + groundingId + "," + concept.name + "," + concept.synonyms.map(_.toString) + "," + (concept.id == groundingId).toString
-          case None => "none" + text
-        }
-
-      val this_preds = predictions.map {
-        _.productIterator.map(_.toString)
-      }.mkString("\n")
-      println(this_preds)
-      predictions.foreach(println)
-      predictions1.foreach(println)
 
     }
   }
+
+  // Gridsearch - bruteforce
+  var hyperparam_size :Int = 2;
+  val lambdas = Uniform(10, 1000).samplesVector(hyperparam_size)
+  val alphas = Gaussian(0.0.toFloat, 1.0.toFloat).samplesVector(hyperparam_size).toDenseVector
+
+  val acc_map = new scala.collection.mutable.HashMap[(Float, Float), Float]()
+
+  for (lambda <- lambdas.toScalaVector().par) yield {
+    for (alpha <- alphas.toScalaVector().par) yield {
+      val this_acc = DenseVector[Float](getAccuracyForThisHyperParams(lambda.toFloat, alpha.toFloat))
+      acc_map.update((lambda.toFloat, alpha.toFloat), this_acc)
+    }
+  }
+
+  def getAccuracyForThisHyperParams(lambda: Float, alpha: Float) {
+    val miraEmbeddingsGrounderGS: MiraEmbeddingsGrounder = {
+      val config = ConfigFactory.load().getConfig("Grounding")
+      val ontologyPath = config.getString("ontologyPath")
+      // val embeddingsPath = config.getString("embeddingsPath")
+
+      MiraEmbeddingsGrounder(ontologyPath, None, lambda, alpha)
+    }
+
+
+      val targets = {
+        // Drop the first line that is the header
+        FileUtils.getTextFromResource("/grounding_tests.tsv").split("\n").drop(1) map { line =>
+          val tokens = line.split("\t")
+          (tokens(0), tokens(1))
+        }
+      }
+
+      val predictions =
+        for {(text, groundingId) <- targets
+             } yield miraEmbeddingsGrounderGS.ground(text) match {
+          case Some(concept) => concept.id == groundingId
+          case None => false
+        }
+      val accuracy = (predictions.count(identity).floatValue() / predictions.length).floatValue()
+      accuracy
+  }
+
+
 }
