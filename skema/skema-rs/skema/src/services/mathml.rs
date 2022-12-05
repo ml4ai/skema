a0d55309eb479c3e825e784fa4e36b934d076440
ualiangzhang <liangzh@arizona.edu>
2022-12-05 10:43:57 -0700

    editing for the API generating graphs

--- skema/skema-rs/skema/src/services/mathml.rs
+++ skema/skema-rs/skema/src/services/mathml.rs
@@ -17,9 +17,9 @@ use petgraph::dot::{Config, Dot};
 #[put("/mathml/ast-graph")]
 pub async fn get_ast_graph(payload: String) -> String {
     let contents = &payload;
-    let (_, math) = parse(&contents).expect(format!("Unable to parse payload!").as_str());
-
-    let g = math.to_graph(); 
-    let dot_representation = Dot::with_config(&g, &[Config::EdgeNoLabel]);
+    let (_, mut math) = parse(&contents).expect(format!("Unable to parse payload!").as_str());
+    math.normalize();
+    let g = &mut math.content[0].clone().to_graph();
+    let dot_representation = Dot::new(&*g);
     dot_representation.to_string()
 }
