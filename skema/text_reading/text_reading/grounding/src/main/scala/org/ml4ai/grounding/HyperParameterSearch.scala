6fb7529725c85210eff67187d17f921ebe598ade
Sushma Akoju <8979477+sushmaakoju@users.noreply.github.com>
2022-12-08 15:27:29 -0700

    updates as per Keith's review comments

--- skema/text_reading/text_reading/grounding/src/main/scala/org/ml4ai/grounding/HyperParameterSearch.scala
+++ skema/text_reading/text_reading/grounding/src/main/scala/org/ml4ai/grounding/HyperParameterSearch.scala
@@ -9,7 +9,7 @@ import ujson.True
 
 import scala.collection.immutable.ListMap
 
-/***
+/** *
   * HyperParameterSearch : generic, bruteforce Hyper parameter search using breeze (numpy style) calculations.
   * Follows:
   * For each concept.name
@@ -19,10 +19,10 @@ import scala.collection.immutable.ListMap
   */
 object HyperParameterSearch extends App {
 
-
-  /***
+  /** *
     * Get accuracy for this lambda, alpha values provided.
     * MiraEmbeddingsGrounder is initialized for lambda, alpha value.
+    *
     * @param lambda
     * @param alpha
     * @return accuracy
@@ -34,7 +34,6 @@ object HyperParameterSearch extends App {
       // val embeddingsPath = config.getString("embeddingsPath")
       MiraEmbeddingsGrounder(ontologyPath, None, lambda, alpha)
     }
-
 
     val targets = {
       // Drop the first line that is the header
@@ -58,33 +57,36 @@ object HyperParameterSearch extends App {
     acc
   }
 
-  def apply(resourcePath: String, isSimulation:Boolean): ListMap[(Float, Float), Float] = {
+  // Grid search -bruteforce
+  var hyperparam_size: Int = 2;
 
-    // Grid search -bruteforce
-    var hyperparam_size: Int = 2;
-    var path: String = resourcePath
-    var lambdas = DenseVector[Float](1f, 10f, 100f) //Uniform(10, 1000).samplesVector(hyperparam_size)
-    var alphas = DenseVector[Float](0.25f, 0.5f, 0.75f) //Gaussian(0.0.toFloat, 1.0.toFloat).samplesVector(hyperparam_size).toDenseVector
-    if (isSimulation){
-      lambdas = Uniform(10f, 1000f).samplesVector(hyperparam_size).values.map(_.toFloat).toDenseVector
-      alphas = Gaussian(0.0f, 1.0f).samplesVector(hyperparam_size).values.map(_.toFloat).toDenseVector
-    }
-    //  val lambdas = DenseVector(1, 3, 5, 7, 10, 50, 75, 100) //Uniform(10, 1000).samplesVector(hyperparam_size)
-    //  val alphas = DenseVector(0.25, 0.5, 0.75) //Gaussian(0.0.toFloat, 1.0.toFloat).samplesVector(hyperparam_size).toDenseVector
-
-    val acc_map = new scala.collection.mutable.HashMap[(Float, Float), Float]()
-
-    // for each lambda, alpha value chosen just for a base case, loop mover to calculate accuracy
-    for (lambda <- lambdas.toScalaVector()) yield {
-      for (alpha <- alphas.toScalaVector()) yield {
-        val this_acc = getAccuracyForThisHyperParams(lambda.toFloat, alpha.toFloat, path)
-        acc_map.update((lambda, alpha), this_acc)
-      }
-    }
-    val sorted_accuracy_map = ListMap(acc_map.toSeq.sortWith(_._2 > _._2):_*)
-    println(acc_map)
-    sorted_accuracy_map
+  // Keith's review comment use args.lift for commandline arguments for standalone script
+  val resourcePath = args.lift(0).getOrElse("defaultvalue")
+  val isSimulation = args.lift(1).map(_ == "true").getOrElse(false)
+  var lambdas = DenseVector[Float](1f, 10f) //, 100f) //Uniform(10, 1000).samplesVector(hyperparam_size)
+  var alphas = DenseVector[Float](0.25f, 0.5f) //, 0.75f) //Gaussian(0.0.toFloat, 1.0.toFloat).samplesVector(hyperparam_size).toDenseVector
+  if (isSimulation) {
+    lambdas = Uniform(10f, 1000f).samplesVector(hyperparam_size).values.map(_.toFloat).toDenseVector
+    alphas = Gaussian(0.0f, 1.0f).samplesVector(hyperparam_size).values.map(_.toFloat).toDenseVector
   }
 
+  val acc_map = new scala.collection.mutable.HashMap[(Float, Float), Float]()
+
+  // for each lambda, alpha value chosen just for a base case, loop mover to calculate accuracy
+  // Keith's review comment use flatmap instead of for loop over ScalaVectors for lambda & alpha values
+  val key_accuracy_pairs = lambdas.toScalaVector.flatMap{ lambda =>
+    alphas.toScalaVector().map { alpha =>
+      (lambda, alpha)  -> getAccuracyForThisHyperParams(lambda, alpha, resourcePath)
+    }
+  }.sortBy(-_._2)
+
+  val sorted_accuracy_map = ListMap(key_accuracy_pairs:_*)
+  println(acc_map)
+  print(sorted_accuracy_map)
+  print(lambdas)
+  (sorted_accuracy_map)
 }
+
+
+
 
