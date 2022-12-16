9e841b61493f6426906906910e93eb09c9114410
Keith Alcock <github@keithalcock.com>
2022-12-15 23:23:57 -0700

    Checkpoint running state on Keith's computer

--- skema/text_reading/mention_linking/main.py
+++ skema/text_reading/mention_linking/main.py
@@ -1,13 +1,18 @@
+import os
+
 """ Temporary test script for development This will change to unit tests and example usage script """
 
-from skema.text_reading.mention_linking.gromet_linker import align_and_link
-
 from automates.utils.fold import dictionary_to_gromet_json, del_nulls
+from skema.text_reading.mention_linking.gromet_linker.align_comments import align_and_link
 
 if __name__ == "__main__":
-	gromet_path = "data/gromet/CHIME_SIR--Gromet-FN-auto.json"
-	comments_path = "data/comments/CHIME_SIR.json"
-	extractions_path = 'data/extractions/CHIME_SIR.json'
+	gromet_path = "../../../../mention_linking_files/gromet/CHIME_SIR--Gromet-FN-auto.json"
+	comments_path = "../../../../mention_linking_files/comments/CHIME_SIR.json"
+	extractions_path = "../../../../mention_linking_files/extractions/CHIME_SIR.json"
+
+	# gromet_path = "data/gromet/CHIME_SIR--Gromet-FN-auto.json"
+	# comments_path = "data/comments/CHIME_SIR.json"
+	# extractions_path = 'data/extractions/CHIME_SIR.json'
 
 	# gromet_path = "data/gromet/chime_penn--Gromet-FN-auto.json"
 	# comments_path = "data/comments/CHIME_full_penn.json"
@@ -17,9 +22,16 @@ if __name__ == "__main__":
 	# comments_path = "data/comments/BUCKY.json"
 	# extractions_path = 'data/extractions/BUCKY.json'
 
-	embeddings_path = "/data/covid_comments_models/xdd_covid_19_1x_word2vec/alternate/embeddings.kv"
+	embeddings_path = "../../../../word_embeddings/epi+code_comments/embeddings.kv"
+	# embeddings_path = "/data/covid_comments_models/xdd_covid_19_1x_word2vec/alternate/embeddings.kv"
+
+	for path in [gromet_path, comments_path, extractions_path, embeddings_path]:
+		if not os.path.exists(path):
+			raise Exception(f"A file doesn't seem to exist: {path}")
+
 	enriched_gromet = align_and_link(gromet_path, comments_path, extractions_path, embeddings_path, debug=True)
 
 	# Save gromet file with the new metadata aligned
 	with open("test.json", 'w') as f:
 		f.write(dictionary_to_gromet_json(del_nulls(enriched_gromet.to_dict())))
+
