21ad244e3c4b662c2e69581dccb091a655294ae4
Keith Alcock <github@keithalcock.com>
2022-12-29 23:38:09 -0700

    Keep lines next to the comments

--- skema/text_reading/mention_linking/gromet_linker/comment_aligner.py
+++ skema/text_reading/mention_linking/gromet_linker/comment_aligner.py
@@ -30,36 +30,33 @@ class CommentAligner():
 		gromet_fn_module = GrometHelper.json_to_gromet(self.gromet_path)
 		gromet_fns = [attribute.value for attribute in gromet_fn_module.attributes if attribute.type == "FN"]
 
-		container_gromet_box_function = None # The function below uses this mutable value.
-
-		def get_info_for_gromet_box_function(gromet_object, key):
+		def get_info_for_gromet_box_function(container_gromet_box_function, gromet_object, key):
 			condition = container_gromet_box_function and container_gromet_box_function.function_type == "FUNCTION"
 			# TODO: Why don't docstrings have line numbers?
 			docstrings = self.src_comments.doc_strings.get(container_gromet_box_function.name, []) if condition else []
 
 			condition = gromet_object != container_gromet_box_function
-			aligned_docstrings = self.variable_name_matcher.match(gromet_object.name, docstrings) if condition else docstrings
+			aligned_docstrings = self.variable_name_matcher.match_comment(gromet_object.name, docstrings) if condition else docstrings
 
 			line_range = GrometHelper.get_element_line_numbers(gromet_object, gromet_fn_module)
 
 			# Get both kinds?
-			aligned_comments = self.align_comments(gromet_object, key, container_gromet_box_function, gromet_fn_module, line_range)
-			info = self.align_mentions(gromet_object, gromet_fn_module, line_range, aligned_docstrings, aligned_comments)
+			box_aligned_comments, function_aligned_comments = self.align_comments(gromet_object, key, container_gromet_box_function, gromet_fn_module, line_range)
+			info = self.align_mentions(gromet_object, gromet_fn_module, line_range, aligned_docstrings, box_aligned_comments, function_aligned_comments)
 			debugger.add_info(info)
 
-		def get_info_for_gromet_port(gromet_object, key):
+		def get_info_for_gromet_port(container_gromet_box_function, gromet_object, key):
 			condition = container_gromet_box_function and container_gromet_box_function.function_type == "FUNCTION"
 			# TODO: Why don't docstrings have line numbers?
 			docstrings = self.src_comments.doc_strings.get(container_gromet_box_function.name, []) if condition else []
 
-			condition = gromet_object != container_gromet_box_function
-			aligned_docstrings = self.variable_name_matcher.match(gromet_object.name, docstrings) if condition else docstrings
+			aligned_docstrings = self.variable_name_matcher.match_comment(gromet_object.name, docstrings) if condition else docstrings
 
 			line_range = GrometHelper.get_element_line_numbers(gromet_object, gromet_fn_module)
 
 			# Get both kinds?
-			aligned_comments = self.align_comments(gromet_object, key, container_gromet_box_function, gromet_fn_module, line_range)
-			info = self.align_mentions(gromet_object, gromet_fn_module, line_range, aligned_docstrings, aligned_comments)
+			box_aligned_comments, function_aligned_comments = self.align_comments(gromet_object, key, container_gromet_box_function, gromet_fn_module, line_range)
+			info = self.align_mentions(gromet_object, gromet_fn_module, line_range, aligned_docstrings, box_aligned_comments, function_aligned_comments)
 			debugger.add_info(info)
 
 		# Identify the output ports with variable names
@@ -70,28 +67,26 @@ class CommentAligner():
 
 		for gromet_fn in gromet_fns:
 			# b: The FN Outer Box (although not enforced, there is always only 1).
-			if gromet_fn.b:
+			assert gromet_fn.b and len(gromet_fn.b) == 1
 				# This is sticky.  Attributes are in order and the container function appears before anything contained.
-				assert len(gromet_fn.b) == 1
-				container_gromet_box_function = gromet_fn.b[0]
-				for gromet_box_function in gromet_fn.b:
-					if gromet_box_function.name:
-						get_info_for_gromet_box_function(gromet_box_function, "b")
+			container_gromet_box_function = gromet_fn.b[0]
+			if container_gromet_box_function.name:
+				get_info_for_gromet_box_function(container_gromet_box_function, container_gromet_box_function, "b")
 			# opi: The Outer Port Inputs of the FN Outer Box (b)
 			if gromet_fn.opi:
 				for gromet_port in gromet_fn.opi:
 					if gromet_port.name:
-						get_info_for_gromet_port(gromet_port, "opi")
+						get_info_for_gromet_port(container_gromet_box_function, gromet_port, "opi")
 			# pof: The Port Outputs of the GrometBoxFunctions (bf).
 			if gromet_fn.pof:
 				for gromet_port in gromet_fn.pof:
 					if gromet_port.name:
-						get_info_for_gromet_port(gromet_port, "pof")
+						get_info_for_gromet_port(container_gromet_box_function, gromet_port, "pof")
 			# bf: The GrometBoxFunctions within this GrometFN.
 			if gromet_fn.bf:
 				for gromet_box_function in gromet_fn.bf:
 					if True: # A name is not required here.
-						get_info_for_gromet_box_function(gromet_box_function, "bf")
+						get_info_for_gromet_box_function(container_gromet_box_function, gromet_box_function, "bf")
 			# poc: The Port Outputs of the GrometBoxConditionals (bc)
 			# TODO: Is this redundant?
 			# for poc in v.poc:
@@ -102,51 +97,55 @@ class CommentAligner():
 		return gromet_fn_module
 
 	# Get the comments in contiguous lines above the function.
-	def get_function_comments(self, gromet_box_function, gromet_fn_module, line_comments) -> list[str]:
+	def get_function_comments(self, gromet_box_function, gromet_fn_module, line_comments) -> list[(int, str)]:
 		""" Gets the block of comments adjacent to the function's definition """
 		comments = list()
 		line_numbers = GrometHelper.get_element_line_numbers(gromet_box_function, gromet_fn_module)
+		# TODO: This range is likely too wide!  It shouldn't go all the way to the top of the file.
 		for line_num in range(line_numbers.start - 1, -1, -1): # decreasing line_num counter
 			if line_num in line_comments:
-				comments.append(line_comments[line_num])
+				comments.append((line_num, line_comments[line_num]))
 			else:
 				break
 		comments.reverse()
 		return comments
 
 	# This gromet_object should be either a gromet_box_function or gromet_port.
-	def align_comments(self, gromet_object, key, container_gromet_box_function, gromet_fn_module, line_range: range):
+	def align_comments(self, gromet_object, key, container_gromet_box_function, gromet_fn_module, line_range: range) -> tuple[list[tuple[int, str]], list[tuple[int, str]]]:
 		# Identify the variables with the same name to each output port.
 		# In case of a tie, resolve the correct variable using the containing line spans.
 
-		aligned_comments = list()
+		box_aligned_comments = list()
+		function_aligned_comments = list()
 
 		# So b and bf are special for the boxes.
 		if key in {"b", "bf"} and gromet_object.function_type not in {"PRIMITIVE", "LITERAL"}:
 			for line_num in line_range:
 				if line_num in self.src_comments.line_comments:
-					aligned_comments.append((line_num, self.src_comments.line_comments[line_num]))
+					box_aligned_comments.append((line_num, self.src_comments.line_comments[line_num]))
 
 		# Use comments not in the same line, but likely of the container function.
 		name = gromet_object.name
 		if name:
 			function_comments = self.get_function_comments(container_gromet_box_function, gromet_fn_module, self.src_comments.line_comments)
-			aligned_comments.extend(self.variable_name_matcher.match(name, function_comments))
+			function_aligned_comments = self.variable_name_matcher.match_line_comment(name, function_comments)
 
-		return aligned_comments
+		return box_aligned_comments, function_aligned_comments
 
-	def align_mentions(self, gromet_object, gromet_fn_module, line_range, aligned_docstrings, aligned_comments) -> CommentInfo:
+	def align_mentions(self, gromet_object, gromet_fn_module, line_range: range, aligned_docstrings: list[str], box_aligned_comments: list[tuple[int, str]], function_aligned_comments: list[tuple[int, str]]) -> CommentInfo:
 		name = gromet_object.name
 
 		# Build new metadata object and append it to the metadata list of each port.
-		comments = [name] if name else [] + aligned_docstrings + [c if type(c) == str else c[1] for c in aligned_comments]
+		comments = [name] if name else [] + aligned_docstrings + [comment for _, comment in box_aligned_comments] + [comment for _, comment in function_aligned_comments]
 		aligned_mentions = self.linker.align_to_comments(comments)
 
 		## Build metadata object for each comments aligned
 		# Get the comment reference
 		code_file_ref = Utils.get_code_file_ref(self.src_comments.file_name, gromet_fn_module)
 		# aligned_comments
-		for comment in aligned_comments:
+		for comment in box_aligned_comments:
+			Utils.build_comment_metadata(self.time_stamper, comment, code_file_ref, gromet_object, gromet_fn_module)
+		for _, comment in function_aligned_comments:
 			Utils.build_comment_metadata(self.time_stamper, comment, code_file_ref, gromet_object, gromet_fn_module)
 		# aligned docstring
 		for docstring in aligned_docstrings:
@@ -156,4 +155,4 @@ class CommentAligner():
 			doc_file_ref = Utils.get_doc_file_ref(self.time_stamper, self.uid_stamper, mention, self.linker, gromet_fn_module)
 			Utils.build_tr_mention_metadata(self.time_stamper, mention, doc_file_ref, gromet_object, gromet_fn_module)
 
-		return CommentInfo(line_range, name, aligned_docstrings, aligned_comments, aligned_mentions)
+		return CommentInfo(line_range, name, aligned_docstrings, box_aligned_comments + function_aligned_comments, aligned_mentions)
--- skema/text_reading/mention_linking/gromet_linker/variable_name_matcher.py
+++ skema/text_reading/mention_linking/gromet_linker/variable_name_matcher.py
@@ -2,7 +2,11 @@ import re
 
 class LanguageMatcher():
 
-	def match(self, name: str, comments: list[str]) -> list[str]:
+	# Use this one for docstrings which don't seem to have lines.
+	def match_comment(self, name: str, comments: list[str]) -> list[str]:
+		return list()
+
+	def match_line_comment(self, name: str, line_comments: list[tuple[int, str]]) -> list[tuple[int, str]]:
 		return list()
 
 class FortranMatcher(LanguageMatcher):
@@ -12,11 +16,18 @@ class PythonMatcher(LanguageMatcher):
 	def __init__(self):
 		self.separator = r"[:,\.\s]"
 
-	def match(self, name: str, comments: list[str]) -> list[str]:
+	def match_comment(self, name: str, comments: list[str]) -> list[str]:
 		# TODO: Use re.escape() after regression testing
 		# pattern = self.separator + re.escape(name) + self.separator
 		pattern = self.separator + name + self.separator
 		matches = [comment for comment in comments if re.search(pattern, comment)]
+		return matches
+
+	def match_line_comment(self, name: str, line_comments: list[tuple[int, str]]) -> list[tuple[int, str]]:
+		# TODO: Use re.escape() after regression testing
+		# pattern = self.separator + re.escape(name) + self.separator
+		pattern = self.separator + name + self.separator
+		matches = [(line, comment) for (line, comment) in line_comments if re.search(pattern, comment)]
 		return matches
 
 class VariableNameMatcher(LanguageMatcher):
@@ -29,8 +40,14 @@ class VariableNameMatcher(LanguageMatcher):
 		else:
 			self.matcher = PythonMatcher() # TODO: raise an exception?
 
-	def match(self, name: str, comments: list[str]) -> list[str]:
+	def match_comment(self, name: str, comments: list[str]) -> list[str]:
 		if name:
-			return self.matcher.match(name, comments)
+			return self.matcher.match_comment(name, comments)
+		else:
+			return list()
+
+	def match_line_comment(self, name: str, line_comments: list[tuple[int, str]]) -> list[tuple[int, str]]:
+		if name:
+			return self.matcher.match_line_comment(name, line_comments)
 		else:
 			return list()
