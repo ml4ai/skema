3afef76dc9eee92bd4d50807f54b21ec1808b0fc
Keith Alcock <github@keithalcock.com>
2023-01-06 15:37:12 -0700

    Take another pass (or ten) over mention linking (#55)
    Here's a draft showing how I understand the mention linking code. AFAICT
    it produces the same output file as the previous version does. Further
    changes would probably start to deviate from the original, so this is a
    good time to take stock. This version is using a debug time stamper and
    uid stamper so that the output stays consistent. I recall the hash
    function for the uid strangely producing different output on the same
    input. There are several TODOS:
    
    * escape the names
    * add line numbers to comments that don't have them
    * adjust dynamically for programming language
    * actually deserialize all the json
    * update the Scala version :-)
    * improve the alignment

---
+++ skema/text_reading/mention_linking/gromet_linker/comment_info.py
@@ -0,0 +1,19 @@
+from typing import Any, List, Mapping, NamedTuple, Optional, Tuple, Union
+
+class CommentInfo(NamedTuple):
+	line_range: Optional[range]
+	name: Optional[str]
+	line_docstrings: List[tuple[int, str]]
+	comments: List[Union[str, Tuple[int, str]]]
+	mentions: Mapping[str, Any]
+
+	def print(self) -> None:
+		docstrings = [docstring for _, docstring in self.line_docstrings]
+		print("===================")
+		print(f"{self.line_range} {self.name if self.name else ''}:")
+		print()
+		print("\n".join(docstrings))
+		print("\n".join(c[1] if type(c) == tuple else c for c in self.comments))
+		if len(self.mentions) > 0:
+			print("Aligned mentions:\n" + '\n'.join(f"{s}: {m['text']}" for m, s in self.mentions))
+		print()
