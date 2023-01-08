11400bd2c10fcf8e529b4bcf97da16e02ea2d9c7
Adarsh Pyarelal <adarsh@arizona.edu>
2023-01-07 21:06:13 -0700

    removing commented out line

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(name='skema',
         "requests",
         "pygraphviz",
         "pytest"
-        #"automates @ https://github.com/danbryce/automates/archive/e5fb635757aa57007615a75371f55dd4a24851e0.zip#sha1=f9b3c8a7d7fa28864952ccdd3293d02894614e3f"
       ],
       tests_require=["unittest"],
       zip_safe=False
