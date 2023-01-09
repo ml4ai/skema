4a27cbd291296ea880674a75cd6017cb9b0c1fb6
Joseph Astier <jastier@gmail.com>
2023-01-09 14:18:11 -0700

    Added test files

---
+++ skema/skema-rs/comment_extraction/tests/data/hello_world.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* comment 1*/
+
+/*
+ * comment 2 */
+
+/*
+
+  comment
+
+  3
+
+*/
+
+int main() {
+
+    /* comment 4 */
+
+    printf("Hello World from C!\n");
+    return 0;
+}
---
+++ skema/skema-rs/comment_extraction/tests/data/hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// C++ comment
+
+int main() {
+    // C++ comment
+
+    std::cout << "Hello World from C++!" << std::endl;
+    return 0;
+}
+
