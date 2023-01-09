c815435a8a26902620b19ddba6ec22a0e7670ed1
Adarsh Pyarelal <adarsh@arizona.edu>
2023-01-09 14:29:30 -0700

    test passing

--- skema/program_analysis/tests/test_code2fn.py
+++ skema/program_analysis/tests/test_code2fn.py
@@ -1,16 +1,20 @@
 import json
+from pathlib import Path
 from skema.program_analysis.multi_file_ingester import process_file_system
 from skema.gromet.fn import GrometFNModuleCollection
 
-
 def test_code2fn():
+    """This is simply a smokescreen test to see if the PA pipeline runs to
+    completion without crashing. It does not test the actual outputs.
+
+    This is because the output JSON contains randomly generated UUIDs, making
+    deterministic testing difficult. In the future, it would be good to add a
+    flag or postprocessing function to be able to compare two GroMEts modulo
+    their random components."""
+
+    data_dir = Path(__file__).parents[3] / "data"
     module_collection: GrometFNModuleCollection = process_file_system(
         "chime_penn",
-        "../../data/epidemiology/CHIME/CHIME_penn_full_model/code/penn_chime",
-        "../../data/epidemiology/CHIME/CHIME_penn_full_model/code/system_filepaths.txt",
+        str(data_dir / "epidemiology/Bucky/code/bucky_v2"),
+        str(data_dir / "epidemiology/Bucky/code/system_filepaths.txt"),
     )
-
-    with open("../../data/epidemiology/CHIME/CHIME_penn_full_model/gromet/chime_penn--Gromet-FN-auto.json") as f:
-        test_data = json.load(f)
-
-    assert(test_data == module_collection.to_dict())
