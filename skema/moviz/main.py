39cd13ee58e669fcffaa522ea68af6d7237823e6
Adarsh Pyarelal <adarsh@arizona.edu>
2023-01-11 11:02:46 -0700

    Removed unnecessary imports in main.py

--- skema/moviz/main.py
+++ skema/moviz/main.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
 
 import os
-import shutil
-import glob
 from pathlib import Path
-import argparse
 
 from skema.moviz.utils.create_json import run_cast_to_gromet_pipeline
 from skema.moviz.utils.create_viz import draw_graph
@@ -19,11 +16,8 @@ def execute():
 
     filepath = Path(__file__).parents[0] / "../../data/gromet/examples/while3/while3.py"
     program_name = filepath.stem
-
     cast = python_to_cast(str(filepath), cast_obj=True)
     gromet = run_cast_to_gromet_pipeline(cast)
-
     draw_graph(gromet, program_name)
     full_filename = os.path.join("static", f"{program_name}.png")
-
     return render_template("index.html", output_image=full_filename)
