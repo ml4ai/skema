ffa196a6853110959841adcb59c894b94925db50
Adarsh Pyarelal <adarsh@email.arizona.edu>
2023-01-12 15:30:08 -0700

    Im2MML updates (#72)
    This PR updates pyproject.toml to properly work with extra features,
    adding extra optional dependencies for moviz and im2mml. It also fixes a
    bug in im2mml_demo.py
    
    Co-authored-by: Adarsh Pyarelal <adarsh@arizona.edu>

--- skema/im2mml/generate_mathml/im2mml_demo.py
+++ skema/im2mml/generate_mathml/im2mml_demo.py
@@ -6,7 +6,7 @@ def get_mml(image_path: str) -> str:
     It sends the http requests to put in an image to translate it into MathML.
     """
     with open(image_path, "rb") as f:
-        r = requests.put("http://127.0.0.1:8000/get_mml", files={"file": f})
+        r = requests.put("http://127.0.0.1:8000/get-mml", files={"file": f})
     return r.text
 
 
