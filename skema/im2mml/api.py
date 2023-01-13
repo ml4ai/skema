476d5f7781380a8c6e0318c626bd746606899182
Adarsh Pyarelal <adarsh@arizona.edu>
2023-01-13 15:47:30 -0700

    robustifying paths

--- skema/im2mml/api.py
+++ skema/im2mml/api.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 from skema.im2mml.translate import Image2Tensor, render_mml
 
 def get_mathml_from_bytes(data: bytes):
@@ -12,39 +13,23 @@ def get_mathml_from_bytes(data: bytes):
     imagetensor = imagetensor.unsqueeze(0)
 
     # read config file
-    config_path = "configs/ourmml_xfmer_config.json"
+    cwd = Path(__file__).parents[0]
+    config_path = cwd/"configs/ourmml_xfmer_config.json"
     with open(config_path, "r") as cfg:
         config = json.load(cfg)
 
     # read vocab.txt
-    vocab = open("vocab.txt").readlines()
+    with open(cwd/"vocab.txt") as f:
+        vocab = f.readlines()
 
-    model_path = "trained_models/cnn_xfmer_OMML-90K_best_model_RPimage.pt"
+    model_path = cwd/"trained_models/cnn_xfmer_OMML-90K_best_model_RPimage.pt"
 
     return render_mml(config, model_path, vocab, imagetensor)
 
 def get_mathml_from_file(filepath):
-    # convert png image to tensor
+    """Read an equation image file and convert it to MathML"""
 
     with open(filepath, "rb") as f:
         data = f.read()
 
-    i2t = Image2Tensor()
-    imagetensor = i2t(data)
-    print("image done!")
-
-    # change the shape of tensor from (C_in, H, W)
-    # to (1, C_in, H, w) [batch =1]
-    imagetensor = imagetensor.unsqueeze(0)
-
-    # read config file
-    config_path = "configs/ourmml_xfmer_config.json"
-    with open(config_path, "r") as cfg:
-        config = json.load(cfg)
-
-    # read vocab.txt
-    vocab = open("vocab.txt").readlines()
-
-    model_path = "trained_models/cnn_xfmer_OMML-90K_best_model_RPimage.pt"
-
-    return render_mml(config, model_path, vocab, imagetensor)
+    return get_mathml_from_bytes(data)
