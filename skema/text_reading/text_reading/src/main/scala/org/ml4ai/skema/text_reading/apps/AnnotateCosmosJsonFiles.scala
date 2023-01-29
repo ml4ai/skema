4aaae130d4d9126cd5299d1b08d85337021ebd1d
Enrique Noriega <enoriega@arizona.edu>
2023-01-29 14:49:27 -0700

    TR Entry point crashes with multiple inputs Fixes #101

--- skema/text_reading/text_reading/src/main/scala/org/ml4ai/skema/text_reading/apps/AnnotateCosmosJsonFiles.scala
+++ skema/text_reading/text_reading/src/main/scala/org/ml4ai/skema/text_reading/apps/AnnotateCosmosJsonFiles.scala
@@ -7,7 +7,8 @@ import java.io.{File, FileOutputStream, PrintWriter}
 
 object AnnotateCosmosJsonFiles extends App with Logging{
 
-    private lazy val textReadingPipeline = new CosmosTextReadingPipeline
+    private val textReadingPipeline = new CosmosTextReadingPipeline
+    textReadingPipeline.
 
     logger.info(s"Starting process with ${args.length} arguments")
 
@@ -16,17 +17,22 @@ object AnnotateCosmosJsonFiles extends App with Logging{
       if path.endsWith(".json")
     } {
       val inputFile = new File(path)
-      if(inputFile.exists()){
-        val outputFile = new File("extractions_" + inputFile.getName)
-        logger.info(s"Extraction mentions from ${inputFile.getAbsolutePath}")
-        val jsonContents = textReadingPipeline.serializeToJson(path)
-        val writer = new PrintWriter(new FileOutputStream(outputFile))
-        writer.write(jsonContents)
-        writer.close()
-        logger.info(s"Wrote output to ${outputFile.getAbsolutePath}")
+      try {
+        if(inputFile.exists()){
+          val outputFile = new File("extractions_" + inputFile.getName)
+          logger.info(s"Extraction mentions from ${inputFile.getAbsolutePath}")
+          val jsonContents = textReadingPipeline.serializeToJson(path)
+          val writer = new PrintWriter(new FileOutputStream(outputFile))
+          writer.write(jsonContents)
+          writer.close()
+          logger.info(s"Wrote output to ${outputFile.getAbsolutePath}")
+        }
+        else
+          logger.error(s"Didn't find ${inputFile.getAbsolutePath}")
+      } catch {
+        case e:Exception =>
+          logger.error(s"Failes annotating: ${inputFile.getAbsolutePath} with $e")
       }
-      else
-        logger.error(s"Didn't find ${inputFile.getAbsolutePath}")
 
     }
 
