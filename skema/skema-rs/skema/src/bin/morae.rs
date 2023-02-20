cd8a5d1a340a5c899e7df34a88c83fb6f13c10c9
Free-Quarks <lieffers@email.arizona.edu>
2023-02-20 14:48:06 -0700

    ability to pull wired subgraphs of expressions

--- skema/skema-rs/skema/src/bin/morae.rs
+++ skema/skema-rs/skema/src/bin/morae.rs
@@ -26,16 +26,140 @@ fn main() {
         expressions.push(subgraph2petgraph(graph[expression_nodes[i]].id.clone()));
     }
 
+    // initialize vector to collect all expression wiring graphs
+    let mut expressions_wiring =
+        Vec::<petgraph::Graph<rsmgclient::Node, rsmgclient::Relationship>>::new();
+    for i in 0..expression_nodes.len() {
+        // grab the wiring subgraph of the given expression
+        expressions_wiring.push(subgraph_wiring(graph[expression_nodes[i]].id.clone()).unwrap());
+    }
+
     // debugging outputs
     /*for i in 0..expression_nodes.len() {
         println!("{:?}", graph[expression_nodes[i]].id);
-        println!("Nodes in subgraph: {}", expressions[i].node_count());
-        println!("Edges in subgraph: {}", expressions[i].edge_count());
+        println!(
+            "Nodes in wiring subgraph: {}",
+            expressions_wiring[i].node_count()
+        );
+        println!(
+            "Edges in wiring subgraph: {}",
+            expressions_wiring[i].edge_count()
+        );
+    }*/
+
+    let (nodes1, edges1) = expressions_wiring[1].clone().into_nodes_edges();
+
+    for edge in edges1 {
+        let source = edge.source();
+        let target = edge.target();
+        let edge_weight = edge.weight;
+        println!(
+            "Edge from {:?} to {:?} with weight {:?}",
+            source, target, edge_weight
+        );
     }
-    println!(
+
+    for node in nodes1 {
+        println!("{:?}", node);
+    }
+
+    /*println!(
         "{:?}",
         Dot::with_config(&expressions[1], &[Config::EdgeNoLabel])
     );*/
+}
+
+fn subgraph_wiring(
+    module_id: i64,
+) -> Result<petgraph::Graph<rsmgclient::Node, rsmgclient::Relationship>, MgError> {
+    // Connect to Memgraph.
+    let connect_params = ConnectParams {
+        host: Some("localhost".to_string()),
+        ..Default::default()
+    };
+    let mut connection = Connection::connect(&connect_params)?;
+
+    // create query for nodes
+    let query1 = format!(
+        "MATCH (n)-[*]->(m) WHERE id(n) = {}
+        MATCH q = (l)<-[r:Wire]-(m)
+        WITH reduce(output = [], m IN nodes(q) | output + m ) AS nodes1
+        UNWIND nodes1 AS nodes2
+        WITH DISTINCT nodes2
+        return collect(nodes2)",
+        module_id
+    );
+
+    // Run Query for nodes
+    connection.execute(&query1, None)?;
+
+    // collect nodes into list
+    let mut node_list = Vec::<Node>::new();
+    if let Value::List(xs) = &connection.fetchall()?[0].values[0] {
+        node_list = xs
+            .iter()
+            .filter_map(|x| match x {
+                Value::Node(x) => Some(x.clone()),
+                _ => None,
+            })
+            .collect();
+    }
+    connection.commit()?;
+
+    // create query for edges
+    let query2 = format!(
+        "MATCH (n)-[*]->(m) WHERE id(n) = {}
+        MATCH q = (l)<-[r:Wire]-(m)
+        WITH reduce(output = [], m IN relationships(q) | output + m ) AS edges1
+        UNWIND edges1 AS edges2
+        WITH DISTINCT edges2
+        return collect(edges2)",
+        module_id
+    );
+
+    // Run Query for edges
+    connection.execute(&query2, None)?;
+
+    // collect edges into list
+    let mut edge_list = Vec::<Relationship>::new();
+    if let Value::List(xs) = &connection.fetchall()?[0].values[0] {
+        edge_list = xs
+            .iter()
+            .filter_map(|x| match x {
+                Value::Relationship(x) => Some(x.clone()),
+                _ => None,
+            })
+            .collect();
+    }
+    connection.commit()?;
+
+    // Create a petgraph graph
+    let mut graph: petgraph::Graph<rsmgclient::Node, rsmgclient::Relationship> = Graph::new();
+
+    // Add nodes to the petgraph graph and collect their indexes
+    let mut nodes = Vec::<NodeIndex>::new();
+    for node in node_list {
+        let n1 = graph.add_node(node);
+        nodes.push(n1.clone());
+    }
+
+    // add the edges to the petgraph
+    for edge in edge_list {
+        let mut src = Vec::<NodeIndex>::new();
+        let mut tgt = Vec::<NodeIndex>::new();
+        for node_idx in nodes.clone() {
+            if graph[node_idx].id == edge.start_id {
+                src.push(node_idx.clone());
+            }
+            if graph[node_idx].id == edge.end_id {
+                tgt.push(node_idx.clone());
+            }
+        }
+        if (src.len() + tgt.len()) == 2 {
+            graph.add_edge(src[0].clone(), tgt[0].clone(), edge);
+        }
+    }
+    return Ok(graph);
 }
 
 fn subgraph2petgraph(
