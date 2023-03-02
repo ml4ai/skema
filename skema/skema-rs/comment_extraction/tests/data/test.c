acd1f6526fd73e051f1f0570b6450921fa5cc114
Joseph Astier <jastier@gmail.com>
2023-03-02 12:49:36 -0700

    Added C comment test file

---
+++ skema/skema-rs/comment_extraction/tests/data/test.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>  // an include file
+#include <stdio.h>   /* another include file */
+
+/* module level comment */
+
+int main(
+    int argc,
+    char* argv[])
+{
+
+    // "quotes in comment "
+    char* foo = " /* comment in quotes */ ";
+
+    /* multi-line comment
+       " "
+       with quotes
+     */  int x = 5;
+
+    char* bar = " /* multi-line comment \
+    in quotes */ ";
+
+    printf("foo = %s\n", foo);
+    printf("bar = %s\n", bar);
+    printf("x   = %d\n", x);
+
+    return 0;
+}
+
