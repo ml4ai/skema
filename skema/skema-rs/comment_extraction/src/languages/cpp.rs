4809b77ac4af08b935566d117de1eca8b5b675f6
Joseph Astier <jastier@gmail.com>
2023-03-03 11:24:38 -0700

    Added parser to skip quoted text

--- skema/skema-rs/comment_extraction/src/languages/cpp.rs
+++ skema/skema-rs/comment_extraction/src/languages/cpp.rs
@@ -32,7 +32,7 @@ fn find_cpp_comment(s: &str) -> IResult<&str, &str> {
         Ok((i,_)) => {
             match take_until("\n")(i) {
                 Ok((_,o)) => {
-                    let n: usize = o.len() + 3;
+                    let n: usize = o.len() + 2;
                     take(n)(s)
                 }
                 Err(e) => Err(e)
@@ -42,16 +42,47 @@ fn find_cpp_comment(s: &str) -> IResult<&str, &str> {
     }
 }
 
+// ignore comments within strings, i.e. char* a = " // not a comment ";
+fn skip_quoted_slice(s: &str) -> IResult<&str, &str> {
+    match tag("\"")(s) {
+        Ok((i,_)) => {
+            match take_until("\"")(i) {
+                Ok((_,o)) => {
+                    let n: usize = o.len() + 2;
+                    match take(n)(s) {
+                        Ok((i,_)) => Ok((i,"")),
+                        Err(e) => Err(e)
+                    }
+                }
+                Err(e) => Err(e)
+            }
+        }
+        Err(e) => Err(e)
+    }
+}
+
+// advance slice by one element
+fn advance_by_1(s: &str) -> IResult<&str, &str> {
+    match take(1usize)(s) {
+        Ok((i,_)) => Ok((i,"")),
+        Err(e) => Err(e)
+    }
+}
+
 fn parse(s: &str) -> IResult<&str,Vec<&str>> {
     fold_many0(
         alt((
             find_c_comment,  // try to enter C comment
             find_cpp_comment,  // try to enter C++ comment
-            take(1usize)  // else advance slice by 1 element 
+            skip_quoted_slice,  // ignore comments within strings
+            advance_by_1  // nothing found, keep looking
         )),
         Vec::new,
         | mut acc: Vec<_>, item | {
-            acc.push(item);
+            if item.len() > 0  {
+                acc.push(item);
+            }
+            else {}
             acc
         }
     )(s)
