42e015caac3d1ab10f12dd3c49b544199f040280
gauravsh0812 <gauravs@arizona.edu>
2023-03-06 17:07:49 -0700

    update

--- skema/img2mml/data_generation/latex2mathml.py
+++ skema/img2mml/data_generation/latex2mathml.py
@@ -84,6 +84,7 @@ def main(year):
                     ]
                 )
 
+        print("pooline...")
         with Pool(num_cpus) as pool:
             result = pool.map(creating_final_equations, temp)
 
