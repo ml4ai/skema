7ac687cd5f2083ffffac37f5e4ec7c04d568c65d
Joseph Astier <jastier@users.noreply.github.com>
2023-03-08 15:27:45 -0700

    C/C++ comment extraction (#134)
    Adds support for extracting C/C++ comments.

---
+++ skema/skema-rs/comment_extraction/tests/data/comment_test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// module comment
+
+/* multiline module
+   comment */
+
+int main() {
+    // function comment
+
+    /* multiline
+     * function
+     * comment
+     */
+
+    std::cout << "Hello World from C++!" << std::endl;
+    return 0;
+}
+
