50bb8e11b5b9ac803a35bbe16b42c25771506717
Vincent Raymond <vincentraymond1995@gmail.com>
2023-03-17 15:46:54 -0400

    Adding initial support for Dimension type

--- skema/program_analysis/TS2CAST/ts2cast.py
+++ skema/program_analysis/TS2CAST/ts2cast.py
@@ -1,7 +1,20 @@
 import json
 from tree_sitter import Language, Parser
-from test import Module, SourceRef, Assignment, LiteralValue, Var, VarType, Name, Expr, Operator, AstNode, SourceCodeDataType, ModelImport
+from test import Module, SourceRef, Assignment, LiteralValue, Var, VarType, Name, Expr, Operator, AstNode, SourceCodeDataType, ModelImport, List
 from cast import CAST
+
+# Naming conventions
+# --------------------------------------------------------
+# CAST nodes: 
+# cast_nodetype
+
+# CAST SourceRef:
+# cast_sourceref_attachednode
+
+# Tree-Sitter nodes:
+# tree_nodetype_type
+# tree_nodetype_identifier
+# --------------------------------------------------------
 
 class TS2CAST(object):
   def __init__(self, source_file_path: str, tree_sitter_fortran_path: str):
@@ -25,18 +38,16 @@ class TS2CAST(object):
     self.parser.set_language(self.tree_sitter_fortran)
     self.source = """
         program test
-          use cons_module,only: rmassinv_o2,rmassinv_o1,rmassinv_n2, rmassinv_he,gask,t0
-          use addfld_module,only: addfld
-          use diags_module,only: mkdiag_MU_M
-          implicit none
+          integer,dimension(10) :: x = (/1,2,3,4,5,6,7,8,9,10/)
+          integer,dimension(10) :: y
+          real,dimension(lev0:lev1,lon0-2:lon1+2),intent(in) :: tn, o2, o1, n2, he
         end program test
         """
     self.tree = self.parser.parse(bytes("""
         program test
-          use cons_module,only: rmassinv_o2,rmassinv_o1,rmassinv_n2, rmassinv_he,gask,t0
-          use addfld_module,only: addfld
-          use diags_module,only: mkdiag_MU_M
-          implicit none
+          integer,dimension(10) :: x = (/1,2,3,4,5,6,7,8,9,10/)
+          integer,dimension(10) :: y
+          real,dimension(lev0:lev1,lon0-2:lon1+2),intent(in) :: tn, o2, o1, n2, he
         end program test
         """, "utf8")) # TODO: Set up loading from file
     
@@ -71,7 +82,6 @@ class TS2CAST(object):
     
     output = None
     node_type = node.type
-
     if node_type == "program": # TODO: Should module == translation_unit or program?
       self.visit_program(node, child_map, source_ref)
     elif node_type == "use_statement":
@@ -80,6 +90,8 @@ class TS2CAST(object):
       output = self.visit_assignment_statement(node, child_map, source_ref)
     elif node_type == "number_literal":
       output = self.visit_number_literal(node, child_map, source_ref)
+    elif node_type == "array_literal":
+      output= self.visit_array_literal(node, child_map, source_ref)
     elif node_type == "identifier":
        output = self.visit_identifier(node, child_map, source_ref)
     elif node_type == "math_expression":
@@ -109,35 +121,43 @@ class TS2CAST(object):
     self.module.source_refs = [source_ref]
 
   def visit_use_statement(self, node, child_map, source_ref):
-     imports = []
-     
-     module_name_node = child_map["module_name"]
-     module_name_node_source_ref = self.generate_source_ref(module_name_node)
-     module_name = self.get_identifier(module_name_node_source_ref)
+     # Getting the module information
+     _, module_name_source_ref, module_name_identifier = self.get_node_data(child_map["module_name"])
 
-     included_items_node = child_map["included_items"]
+     import_all = "included_items" not in child_map
+     import_alias = None # TODO: Look into local-name and use-name fields
+
+     # There are some children in the included_items node that are not actually imports
+     # This is a map keeping track of those
+     fake_import = {",", "only", ":"}
 
      # We need to check if this import is a full import of a module, i.e. use module
      # Or a partial import i.e. use module,only: sub1, sub2
-     
-     print(included_items_node.children)
-     if "only" in child_map:
-        print("HERE")
-     else:
-        pass
-     for child in included_items_node.children:
-        child_source_ref = self.generate_source_ref(child)
-
+     if import_all:
         cast_import = ModelImport()
-        cast_import.source_refs = [child_source_ref]
+        cast_import.source_refs = [source_ref] # TODO: What source ref should this be
         
-        cast_import.name = module_name
-        cast_import.alias = None
-        cast_import.symbol = self.get_identifier(child_source_ref)
+        cast_import.name = module_name_identifier
+        cast_import.alias = import_alias
+        cast_import.all = import_all
+        cast_import.symbol = None
+        
+        return cast_import
+     else:
+      imports = []
+      for child in child_map["included_items"].children:
+          _, child_source_ref, child_identifer = self.get_node_data(child)
 
-        imports.append(cast_import)
+          cast_import = ModelImport()
+          cast_import.source_refs = [child_source_ref]
+          
+          cast_import.name = module_name_identifier
+          cast_import.alias = import_alias
+          cast_import.all = import_all
+          cast_import.symbol = child_identifer
 
-     return imports
+          imports.append(cast_import)
+      return imports 
   
   def visit_assignment_statement(self, node, child_map, source_ref):
     cast_assignment = Assignment()
@@ -167,7 +187,22 @@ class TS2CAST(object):
       cast_literal.source_code_data_type = ["Fortran", "Fortran95", "Integer"]
     
     return cast_literal
+  
+  def visit_array_literal(self, node, child_map, source_ref):
+     cast_list = List()
+     cast_list.source_refs = [source_ref]
 
+     cast_list.values = []
+     for child in node.children:
+        _, child_source_ref, child_identifier = self.get_node_data(child)
+        # The array_literal node may have extraneous children (i.e. , \)
+        # We need to check if visiting the child produces a CAST node before appending it
+        cast_child = self.visit(child)
+        if(cast_child): 
+          cast_list.values.append(cast_child)
+     
+     return cast_list
+  
   def visit_identifier(self, node, child_map, source_ref):
      cast_var = Var()
      cast_var.source_refs = [source_ref]
@@ -219,46 +254,56 @@ class TS2CAST(object):
        "logical": "Bool",
        "character": "String", 
     }
-
-    type_node = child_map["intrinsic_type"]
-    type_node_source_ref = self.generate_source_ref(type_node)
-    intrinsic_type = self.get_identifier(type_node_source_ref)
-    cast_variable_type = type_map[intrinsic_type]
-    
-    # First check if this is an declaration and definition, or just a declaration
-    # If its a definition, then we return an Assignment, else a Var node
-    if "assignment_statement" in child_map:
-      cast_assignment = self.visit(child_map["assignment_statement"])
-      cast_var = cast_assignment.left 
-      cast_var.type = cast_variable_type
-
-      return cast_assignment
+    _, _, type_identifier = self.get_node_data(child_map["intrinsic_type"])
+    cast_variable_type = type_map[type_identifier]
   
-    # If just a declaration, create CAST Var node to represent the variable and attach source_ref
-    cast_var = Var()
-    cast_var.source_refs = [source_ref]
-    
-    # Default value comes from Pytohn keyword arguments i.e. def foo(a, b=10)
-    # Fortran does have optional arguments introduced in F90, but these do not specify a default
-    cast_var.default_value = None 
+    # We then need to determine if we are creating an array (dimension) or a single variable
+    is_dimension = False
+    for child in node.children:
+       child_type, _, child_identifier = self.get_node_data(child)
+       if child_type == "type_qualifier" and "dimension(": #TODO: Update this logic here
+          cast_variable_type = "List"
 
-    cast_var_name_node = child_map["identifier"]
-    cast_var_name_source_ref = self.generate_source_ref(cast_var_name_node)
-    
-    cast_var.val = Name()
-    cast_var.val.source_refs = [cast_var_name_source_ref]
+    # You can declare multiple variables of the same type in a single statement, so we need to create a Var node for each instance
+    vars = [] 
+    for child in node.children:
+      child_type, child_source_ref, child_identifier = self.get_node_data(child)
+      # First check if this is an declaration and definition, or just a declaration
+      # If its a definition, then we return an Assignment, else a Var node
+      if child_type == "assignment_statement":
+         cast_assignment = self.visit(child)
+         # Since we have information about the variable type, we are able to update the left side of this assignment
+         cast_assignment.left.type = cast_variable_type
 
-    # The variable name comes from the identifier child node
-    cast_var.val.name = self.get_identifier(cast_var_name_source_ref)
+         vars.append(cast_assignment)
+      elif child_type == "identifier":
+        # If just a declaration, create CAST Var node to represent the variable and attach source_ref
+        cast_var = Var()
+        cast_var.source_refs = [source_ref]
 
-    # The name id is incremented after every use to create a unique ID for every
-    # instance of a name. In the AnnCAST passes, incremental ids for each name will be
-    # created
-    cast_var.val.id = self.variable_id
-    self.variable_id += 1
+        cast_var.type = cast_variable_type
 
-    return cast_var
-  
+        # Default value comes from Pytohn keyword arguments i.e. def foo(a, b=10)
+        # Fortran does have optional arguments introduced in F90, but these do not specify a default
+        cast_var.default_value = None 
+      
+        cast_var.val = Name()
+        cast_var.val.source_refs = [child_source_ref]
+
+        # The variable name comes from the identifier child node
+        cast_var.val.name = child_identifier
+
+        # The name id is incremented after every use to create a unique ID for every
+        # instance of a name. In the AnnCAST passes, incremental ids for each name will be
+        # created
+        cast_var.val.id = self.variable_id
+        self.variable_id += 1
+
+        vars.append(cast_var)
+      else: 
+         continue
+
+    return vars
 
   def generate_source_ref(self, node):
     row_start, col_start = node.start_point
@@ -287,6 +332,13 @@ class TS2CAST(object):
             identifier += char
     
     return identifier
+    
+  def get_node_data(self, node):
+     node_type = node.type
+     source_ref = self.generate_source_ref(node)
+     identifier = self.get_identifier(source_ref)
+     
+     return node_type, source_ref, identifier
   
 walker = TS2CAST("exp0.f95", "tree-sitter-fortran")
 
