f36c0b16803af9f07e48b0b93abc3e086860c010
Adarsh Pyarelal <adarsh@arizona.edu>
2023-03-22 14:59:21 -0700

    directory comment extraction wip

--- skema/skema-rs/comment_extraction/src/bin/extract_comments.rs
+++ skema/skema-rs/comment_extraction/src/bin/extract_comments.rs
@@ -8,9 +8,8 @@ use std::fs::write;
 use std::path::Path;
 
 use comment_extraction::conventions::dssat::get_comments as get_fortran_comments;
+use comment_extraction::languages::cpp::get_comments as get_cpp_comments;
 use comment_extraction::languages::python::get_comments as get_python_comments;
-use comment_extraction::languages::cpp::get_comments as
-get_cpp_comments;
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -26,40 +25,48 @@ fn main() {
     pretty_env_logger::init();
     let args = Cli::parse();
     let input = &args.input;
-    let extension = Path::new(input)
-        .extension()
-        .expect("Unable to get extension for {input}!")
-        .to_str()
-        .expect("Unable to convert extension to a valid string!");
 
-    if extension == "f" || extension == "for" {
-        let comments = get_fortran_comments(input).unwrap();
-        let comments = serde_json::to_string(&comments).unwrap();
-        if let Some(path) = args.output {
-            write(path, comments).expect("Unable to write to file!");
+    let metadata = std::fs::metadata(input).unwrap();
+    if metadata.is_file() {
+        let extension = Path::new(input)
+            .extension()
+            .expect("Unable to get extension for {input}!")
+            .to_str()
+            .expect("Unable to convert extension to a valid string!");
+
+        if extension == "f" || extension == "for" {
+            let comments = get_fortran_comments(input).unwrap();
+            let comments = serde_json::to_string(&comments).unwrap();
+            if let Some(path) = args.output {
+                write(path, comments).expect("Unable to write to file!");
+            } else {
+                println!("{comments}");
+            }
+        } else if extension == "py" {
+            let comments = get_python_comments(input);
+            let comments = serde_json::to_string(&comments).unwrap();
+            if let Some(path) = args.output {
+                write(path, comments).expect("Unable to write to file!");
+            } else {
+                println!("{comments}");
+            }
+        } else if extension == "cpp" || extension == "c" {
+            let comments = get_cpp_comments(input);
+            let comments = serde_json::to_string(&comments).unwrap();
+            if let Some(path) = args.output {
+                write(path, comments).expect("Unable to write to file!");
+            } else {
+                println!("{comments}");
+            }
         } else {
-            println!("{comments}");
-        }
-    } else if extension == "py" {
-        let comments = get_python_comments(input);
-        let comments = serde_json::to_string(&comments).unwrap();
-        if let Some(path) = args.output {
-            write(path, comments).expect("Unable to write to file!");
-        } else {
-            println!("{comments}");
-        }
-    } else if extension == "cpp" || extension == "c" {
-        let comments = get_cpp_comments(input);
-        let comments = serde_json::to_string(&comments).unwrap();
-        if let Some(path) = args.output {
-            write(path, comments).expect("Unable to write to file!");
-        } else {
-            println!("{comments}");
-        }
-    } else {
-        panic!(
-            "Unable to infer programming language for file \"{input}\"! \
+            panic!(
+                "Unable to infer programming language for file \"{input}\"! \
             File extension must be one of the following: {{\".py\", \".f\", \".for\"}}"
-        )
+            )
+        }
+    } else if metadata.is_dir() {
+        for entry in walkdir::WalkDir::new(input) {
+            println!("{}", entry.unwrap().path().display());
+        }
     }
 }
