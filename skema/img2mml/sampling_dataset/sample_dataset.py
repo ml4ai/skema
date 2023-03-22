34425741d61da9be0b2ff0983bcf8439eb83cb2c
gauravsh0812 <gauravs@arizona.edu>
2023-03-21 20:05:11 -0700

    update

--- skema/img2mml/sampling_dataset/sample_dataset.py
+++ skema/img2mml/sampling_dataset/sample_dataset.py
@@ -130,67 +130,70 @@ def main():
         print(len(all_paths))
         if apidx%100==0:print(f"{apidx}, {ap}, count={count}")
         if count <= total_eqns:
-            yr, month, folder, type_of_eqn, eqn_num = ap.split("_")
-            mml_path = os.path.join(
-                root,
-                f"{yr}/{month}/mathjax_mml/{folder}/{type_of_eqn}_mml/{eqn_num}.xml",
-            )
-
-            mml = open(mml_path).readlines()[0]
-
-            simp_mml = simplification(mml)
-            length_mml = len(simp_mml.split())
-
-            # finding the bin
-            temp_dict = {}
-            for i in range(50, 400, 50):
-                if length_mml / i < 1:
-                    temp_dict[i] = length_mml / i
-
-            # get the bin
-            if len(temp_dict) >= 1:
-                max_bin_size = max(temp_dict, key=lambda k: temp_dict[k])
-                tgt_bin = f"{max_bin_size-50}-{max_bin_size}"
-            else:
-                tgt_bin = "350+"
-
-            if counter_dist_dict[tgt_bin] <= dist_dict[tgt_bin]:
-                counter_dist_dict[tgt_bin] += 1
-
-                # wrting path
-                paths_file.write(ap + "\n")
-
-                # writing MathML
-                if "\n" not in mml:
-                    mml = mml + "\n"
-                mml_file.write(mml)
-
-                # writing latex
-                latex_path = os.path.join(
+            try:
+                yr, month, folder, type_of_eqn, eqn_num = ap.split("_")
+                mml_path = os.path.join(
                     root,
-                    f"{yr}/{month}/latex_equations/{folder}/{type_of_eqn}_eqns/{eqn_num}.txt",
+                    f"{yr}/{month}/mathjax_mml/{folder}/{type_of_eqn}_mml/{eqn_num}.xml",
                 )
-                latex_arr = open(latex_path).readlines()
-                if len(latex_arr) > 1:
-                    latex = " "
-                    for l in latex_arr:
-                        latex = latex + l.replace("\n", "")
+
+                mml = open(mml_path).readlines()[0]
+
+                simp_mml = simplification(mml)
+                length_mml = len(simp_mml.split())
+
+                # finding the bin
+                temp_dict = {}
+                for i in range(50, 400, 50):
+                    if length_mml / i < 1:
+                        temp_dict[i] = length_mml / i
+
+                # get the bin
+                if len(temp_dict) >= 1:
+                    max_bin_size = max(temp_dict, key=lambda k: temp_dict[k])
+                    tgt_bin = f"{max_bin_size-50}-{max_bin_size}"
                 else:
-                    latex = latex_arr[0]
+                    tgt_bin = "350+"
 
-                if "\n" not in latex:
-                    latex = latex + "\n"
-                latex_file.write(latex)
+                if counter_dist_dict[tgt_bin] <= dist_dict[tgt_bin]:
+                    counter_dist_dict[tgt_bin] += 1
 
-                # copying image
-                img_src = os.path.join(
-                    root,
-                    f"{yr}/{month}/latex_images/{folder}/{type_of_eqn}_eqns/{eqn_num}.png",
-                )
-                img_dst = os.path.join(images_path, f"{count}.png")
-                CP(img_src, img_dst)
+                    # wrting path
+                    paths_file.write(ap + "\n")
 
-                count += 1
+                    # writing MathML
+                    if "\n" not in mml:
+                        mml = mml + "\n"
+                    mml_file.write(mml)
+
+                    # writing latex
+                    latex_path = os.path.join(
+                        root,
+                        f"{yr}/{month}/latex_equations/{folder}/{type_of_eqn}_eqns/{eqn_num}.txt",
+                    )
+                    latex_arr = open(latex_path).readlines()
+                    if len(latex_arr) > 1:
+                        latex = " "
+                        for l in latex_arr:
+                            latex = latex + l.replace("\n", "")
+                    else:
+                        latex = latex_arr[0]
+
+                    if "\n" not in latex:
+                        latex = latex + "\n"
+                    latex_file.write(latex)
+
+                    # copying image
+                    img_src = os.path.join(
+                        root,
+                        f"{yr}/{month}/latex_images/{folder}/{type_of_eqn}_eqns/{eqn_num}.png",
+                    )
+                    img_dst = os.path.join(images_path, f"{count}.png")
+                    CP(img_src, img_dst)
+
+                    count += 1
+            except:
+                pass
 
         else:
             break
