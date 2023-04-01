6bbb8501ce0881e521e2ae0abcded08fe9d80a7f
Enrique Noriega <enoriega@arizona.edu>
2023-04-01 16:09:56 -0700

    xDD Dockerization (#146)
    Updated the docker file to build an image which is xDD compliant.
    This PR include an improvement on `AnnotateCosmosJson` to handler
    command line parameters

--- skema/text_reading/text_reading/src/main/scala/org/ml4ai/skema/text_reading/apps/AnnotateCosmosJsonFiles.scala
+++ skema/text_reading/text_reading/src/main/scala/org/ml4ai/skema/text_reading/apps/AnnotateCosmosJsonFiles.scala
@@ -2,53 +2,87 @@ package org.ml4ai.skema.text_reading.apps
 
 import org.clulab.utils.Logging
 import org.ml4ai.skema.text_reading.CosmosTextReadingPipeline
+import scala.util.Using
+import scopt.OParser
 
 import java.io.{File, FileOutputStream, PrintWriter}
+case class ArgsConfig(
+                   outDir: File = new File("."),
+                   inputFiles: Seq[File] = Seq(),
+)
 
 object AnnotateCosmosJsonFiles extends App with Logging{
 
-    private val textReadingPipeline = new CosmosTextReadingPipeline
+  val builder = OParser.builder[ArgsConfig]
 
-    logger.info(s"Starting process with ${args.length} arguments")
+  val parser = {
+    import builder._
+    OParser.sequence(
+      programName("AnnotateCosmosJsonFiles"),
+      head("AnnotateCosmosJsonFiles", "1.0"),
+      // option -f, --foo
+      opt[File]('o', "output_dir")
+        .required()
+        .action((x, c) => c.copy(outDir = x)).withFallback( () => new File("."))
+        .text("directory to write the output files to"),
+      // more options here...
+      arg[Seq[File]]("<input file 1>...")
+        .unbounded()
+        .action((x, c) => c.copy(inputFiles = x))
+        .text("files to annotate"),
+    )
+  }
 
-    // If an argument is a directory, look at all the inner files, otherwise consider it a file to annotate
-    val pathsToAnnotate = args flatMap {
-      path => {
-        val p = new File(path)
-        
-        if(p.isDirectory)
-          p.listFiles()
-        else
-          Seq(p)
-      }
+  OParser.parse(parser, args, ArgsConfig()) match {
+    case Some(config) =>
+      val textReadingPipeline = new CosmosTextReadingPipeline
 
-    }
+      logger.info(s"Starting process with ${config.inputFiles} arguments")
 
-    for{
-      inputFile <- pathsToAnnotate.par
-      if inputFile.getName.endsWith(".json")
-    } {
-      
-      try {
-        if(inputFile.exists()){
-          val outputFile = new File("extractions_" + inputFile.getName)
-          logger.info(s"Extraction mentions from ${inputFile.getAbsolutePath}")
-          val jsonContents = textReadingPipeline.extractMentionsFromJsonAndSerialize(inputFile.getAbsolutePath)
-          val writer = new PrintWriter(new FileOutputStream(outputFile))
-          writer.write(jsonContents)
-          writer.close()
-          logger.info(s"Wrote output to ${outputFile.getAbsolutePath}")
+      // If an argument is a directory, look at all the inner files, otherwise consider it a file to annotate
+      val pathsToAnnotate = config.inputFiles flatMap {
+        p => {
+          if(p.isDirectory)
+            p.listFiles()
+          else
+            Seq(p)
         }
-        else
-          logger.error(s"Didn't find ${inputFile.getAbsolutePath}")
-      } catch {
-        case e:Exception =>
-          logger.error(s"Failed annotating: ${inputFile.getAbsolutePath} with $e")
+
       }
 
-    }
+      for{
+        inputFile <- pathsToAnnotate.par
+        if inputFile.getName.endsWith(".json")
+      } {
 
-    logger.info("Finished")
+        try {
+          if(inputFile.exists()){
+            val outputFile = new File(config.outDir, "extractions_" + inputFile.getName)
+            logger.info(s"Extraction mentions from ${inputFile.getAbsolutePath}")
+            val jsonContents = textReadingPipeline.extractMentionsFromJsonAndSerialize(inputFile.getAbsolutePath)
+            Using(new PrintWriter(new FileOutputStream(outputFile))) {
+              writer =>
+                writer.write(jsonContents)
+                writer.close()
+                logger.info(s"Wrote output to ${outputFile.getAbsolutePath}")
+            }
+          }
+          else
+            logger.error(s"Didn't find ${inputFile.getAbsolutePath}")
+        } catch {
+          case e:Exception =>
+            logger.error(s"Failed annotating: ${inputFile.getAbsolutePath} with $e")
+        }
+
+      }
+
+      logger.info("Finished")
+    case _ =>
+      // arguments are bad, error message will have been displayed
+  }
+
+
+
 
 
 }
--- skema/text_reading/text_reading/webapp/build.sbt
+++ skema/text_reading/text_reading/webapp/build.sbt
@@ -5,5 +5,6 @@ dependencyOverrides += "xml-apis" % "xml-apis" % "1.4.01"
 
 libraryDependencies ++= Seq(
   "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
+  "org.clulab"                 %% "model-streamed-trigram-ser"   % "1.0.0",
   guice
 )
