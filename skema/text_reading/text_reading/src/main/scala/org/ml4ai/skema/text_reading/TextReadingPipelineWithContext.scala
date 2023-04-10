b4d4a98ea8290cad729877133d2ae1e0748ea625
Keith Alcock <github@keithalcock.com>
2023-04-10 11:44:57 -0700

    Add the plain text to the webapp (#160)
    AFAIK, we don't have documentation for the output format. Let's address this in a separate PR

--- skema/text_reading/text_reading/src/main/scala/org/ml4ai/skema/text_reading/TextReadingPipelineWithContext.scala
+++ skema/text_reading/text_reading/src/main/scala/org/ml4ai/skema/text_reading/TextReadingPipelineWithContext.scala
@@ -7,19 +7,19 @@ import org.ml4ai.skema.text_reading.serializer.SkemaJSONSerializer
 /**
   * Runs extraction, grounding and context on input string objects
   */
-class TextReadingPipelineWithContext(contextWindowSize:Int) extends TextReadingPipeline{
+class TextReadingPipelineWithContext() extends TextReadingPipeline {
 
   /**
     * Extracts mentions and runs the context engine over the extractions
     * @param inputText text to annotate
+    * @param contextWindowSize Size of the context window
     * @return extractions with scenario context
     */
-  def extractMentionsWithContext(inputText:String):Seq[Mention] ={
-    val mentions = this.extractMentions(inputText, None)._2
+  def extractMentionsWithContext(inputText: String, contextWindowSize: Int): Seq[Mention] = {
+    val mentions = extractMentions(inputText, None)._2
+    val scenarioContextEngine = new ContextEngine(contextWindowSize, mentions, SentenceIndexOrderer)
+    val mentionsWithScenarioContext = mentions.map(scenarioContextEngine.resolveContext)
 
-    // Resolve scenario context
-    val scenarioContextEngine = new ContextEngine(windowSize = contextWindowSize, mentions, SentenceIndexOrderer)
-    val mentionsWithScenarioContext = mentions map scenarioContextEngine.resolveContext
     mentionsWithScenarioContext
   }
 
@@ -27,8 +27,13 @@ class TextReadingPipelineWithContext(contextWindowSize:Int) extends TextReadingP
     * Extracts the mentions and serializes them into a json string
     *
     * @param inputText Text to annotate
+    * @param contextWindowSize Size of the context window
     * @return string with the json representation of the extractions and the document annotations
     */
-  def extractMentionsWithContextAndSerialize(inputText: String): String = ujson.write(SkemaJSONSerializer.serializeMentions(this.extractMentionsWithContext(inputText)))
+  def extractMentionsWithContextAndSerialize(inputText: String, contextWindowSize: Int): String = {
+    val mentions = extractMentionsWithContext(inputText, contextWindowSize)
+    val json = ujson.write(SkemaJSONSerializer.serializeMentions(mentions))
 
+    json
+  }
 }
