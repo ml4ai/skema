c227cbe96769dc3b8495e65c6df973c34119291a
gauravsh0812 <gauravs@arizona.edu>
2023-04-21 11:42:24 -0700

    updating temp.py

--- skema/img2mml/create_dataset.py
+++ skema/img2mml/create_dataset.py
@@ -1,4 +1,5 @@
-from skema.img2mml.sampling_dataset.sample_dataset import main
+# from skema.img2mml.sampling_dataset.sample_dataset import main
+from skema.img2mml.sampling_dataset.temp import main
 
 if __name__ == "__main__":
     main()
