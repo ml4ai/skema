f32f09f6be31d00091906bb0bc52584670d3938d
Vincent Raymond <vincent@lum.ai>
2023-04-21 16:13:29 -0400

    Renaming build script to remove '-' so it can be imported from

--- skema/program_analysis/TS2CAST/build_tree_sitter_fortran.py
+++ skema/program_analysis/TS2CAST/build_tree_sitter_fortran.py
@@ -6,6 +6,7 @@ from tree_sitter import Language, Parser
 
 # TODO: Eventually, we can look into locking this to a specific version
 FORTRAN_CLONE_URL = "https://github.com/stadelmanma/tree-sitter-fortran.git"
+LANGUAGE_LIBRARY_REL_PATH = os.path.join("build, my-languages.so")
 
 # We are using __file__ to guarantee that the library is created relative to the TS2CAST directory no matter where its called from
 root_path = os.path.dirname(os.path.abspath(__file__))
