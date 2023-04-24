ccd9f4795fa400208c4b8fb6b38dda722c988e2f
gauravsh0812 <gauravs@arizona.edu>
2023-04-24 14:31:06 -0700

    update

--- skema/img2mml/sampling_dataset/simp.py
+++ skema/img2mml/sampling_dataset/simp.py
@@ -6,6 +6,7 @@ parser.add_argument("--idx")
 args = parser.parse_args()
 
 i = args.idx
+
 sm= open(f"{os.getcwd()}/sampling_dataset/temp_folder/sm_{i}.txt", "w")
 smr = open(f"{os.getcwd()}/sampling_dataset/temp_folder/smr_{i}.txt").readlines()[0].strip()
 
