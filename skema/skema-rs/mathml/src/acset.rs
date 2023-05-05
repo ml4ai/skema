b1a1404abdad1f9ed2bf403cc3786c9530b17437
Free-Quarks <lieffers@email.arizona.edu>
2023-05-05 14:25:15 -0700

    fixed json schema for modelrep

--- skema/skema-rs/mathml/src/acset.rs
+++ skema/skema-rs/mathml/src/acset.rs
@@ -45,250 +45,92 @@ pub struct ACSet {
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
 pub struct ModelRepPn {
-    #[serde(rename = "$schema")]
+    pub name: String,
     pub schema: String,
-    #[serde(rename = "type")]
-    pub r#type: String,
-    pub properties: Properties,
-    #[serde(rename = "$defs", skip_serializing_if = "Option::is_none")]
-    pub defs: Option<Defs>,
-    #[serde(rename = "additionalProperties")]
-    pub additionalproperties: bool,
+    pub description: String,
+    pub model_verison: String,
+    pub model: Model,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub required: Option<Vec<String>>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct StringType {
-    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
-    pub r#type: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub format: Option<String>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct ArrayType {
-    #[serde(rename = "type")]
-    pub r#type: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub items: Option<Vec<StringType>>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct Ref {
-    #[serde(rename = "$ref")]
-    pub r#ref: String,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct NumType {
-    pub r#type: i32,
+    pub metadata: Option<Metadatas>,
 }
 
 #[derive(
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
 pub struct Model {
-    #[serde(rename = "type")]
-    r#type: String,
-    properties: ModelProperties,
-    required: Option<Vec<String>>,
-    #[serde(rename = "additionalProperties")]
-    additionalproperties: Option<bool>,
+    pub states: Vec<States>,
+    pub transitions: Vec<Transitions>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub parameters: Option<Vec<Parameters>>,
 }
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct ModelProperties {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    states: Option<States>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    transitions: Option<Transitions>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    parameters: Option<Parameters>,
-}
-
 #[derive(
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
 pub struct States {
-    #[serde(rename = "type")]
-    r#type: String,
-    items: Vec<StateItems>,
+    pub id: String,
+    pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub grounding: Option<Grounding>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub initial: Option<Initial>,
 }
 
 #[derive(
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
-pub struct TransitionItems {
-    #[serde(rename = "type")]
-    r#type: String,
-    properties: TransitionProperties,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    required: Option<Vec<String>>,
+pub struct Grounding {
+    pub identifiers: Identifier,
 }
 
 #[derive(
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
-pub struct ParameterItems {
-    #[serde(rename = "type")]
-    r#type: String,
-    properties: ParameterProperties,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    required: Option<Vec<String>>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct StateItems {
-    #[serde(rename = "type")]
-    r#type: String,
-    properties: StateProperties,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    required: Option<Vec<String>>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct StateProperties {
-    id: StringType,
-    name: StringType,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    grounding: Option<Ref>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    initial: Option<Ref>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct Transitions {
-    #[serde(rename = "type")]
-    r#type: String,
-    items: Vec<TransitionItems>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct TransitionProperties {
-    id: StringType,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    input: Option<ArrayType>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    output: Option<ArrayType>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    grounding: Option<Ref>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    properties: Option<Ref>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct Parameters {
-    #[serde(rename = "type")]
-    r#type: String,
-    items: ParameterItems,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct ParameterProperties {
-    id: StringType,
-    description: StringType,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    value: Option<NumType>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    grounding: Option<Ref>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    distribution: Option<Ref>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct Properties {
-    name: StringType,
-    schema: StringType,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    description: Option<StringType>,
-    model_version: StringType,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    properties: Option<StringType>,
-    model: Model,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct Defs {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    initial: Option<Initial>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    properties: Option<DefProperties>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    rate: Option<Rate>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    distribution: Option<Distribution>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    grounding: Option<Groundings>,
-    #[serde(
-        rename = "additionalProperties",
-        skip_serializing_if = "Option::is_none"
-    )]
-    additionalproperties: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    required: Option<Vec<String>>,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct DefProperties {
-    #[serde(rename = "type")]
-    r#type: String,
-    properties: DefPropertiesProperties,
-    required: Vec<String>,
-    #[serde(rename = "additionalProperties")]
-    additionalproperties: bool,
+pub struct Identifier {
+    pub ido: i64,
 }
 
 #[derive(
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
 pub struct Initial {
-    #[serde(rename = "type")]
-    r#type: String,
-    properties: InitialProperties,
-    required: Vec<String>,
-    #[serde(rename = "additionalProperties")]
-    additionalproperties: bool,
+    pub expression: String,
+    pub expression_mathml: String,
 }
 
 #[derive(
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
-pub struct Rate {
-    #[serde(rename = "type")]
-    r#type: String,
-    properties: InitialProperties,
-    required: Vec<String>,
-    #[serde(rename = "additionalProperties")]
-    additionalproperties: bool,
+pub struct Transitions {
+    pub id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub input: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub output: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub properties: Option<Properties>,
+}
+
+#[derive(
+    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
+)]
+pub struct Properties {
+    pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub rate: Option<Initial>,
+}
+
+#[derive(
+    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
+)]
+pub struct Parameters {
+    pub id: String,
+    pub description: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub grounding: Option<Grounding>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub value: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub distribution: Option<Distribution>,
 }
 
 #[derive(
@@ -296,57 +138,24 @@ pub struct Rate {
 )]
 pub struct Distribution {
     #[serde(rename = "type")]
-    r#type: String,
-    properties: DistributionProperties,
-    required: Vec<String>,
-    #[serde(rename = "additionalProperties")]
-    additionalproperties: bool,
+    pub r#type: String,
+    pub parameters: ParametersD,
 }
 
 #[derive(
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
-pub struct Groundings {
-    #[serde(rename = "type")]
-    r#type: String,
-    properties: GroundingProperties,
-    required: Vec<String>,
-    #[serde(rename = "additionalProperties")]
-    additionalproperties: bool,
+pub struct ParametersD {
+    minimum: i64,
+    maximum: i64,
 }
 
 #[derive(
     Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
 )]
-pub struct InitialProperties {
-    expression: StringType,
-    expression_mathml: StringType,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct DefPropertiesProperties {
-    name: StringType,
-    grounding: Ref,
-    rate: Ref,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct DistributionProperties {
-    #[serde(rename = "type")]
-    r#type: StringType,
-    parameters: StringType,
-}
-
-#[derive(
-    Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, ToSchema,
-)]
-pub struct GroundingProperties {
-    identifiers: StringType,
-    context: StringType,
+pub struct Metadatas {
+    pub processed_at: i64,
+    pub processed_by: String,
 }
 
 // -------------------------------------------------------------------------------------------
@@ -355,148 +164,69 @@ pub struct GroundingProperties {
 pub fn PN_to_ModelRepPN(pn: ACSet) -> ModelRepPn {
     // -----------------------------------------------------
 
-    let mut state_items_vec = Vec::<StateItems>::new();
-    let mut transition_items_vec = Vec::<TransitionItems>::new();
-    let mut string_type_vec = Vec::<StringType>::new();
+    let mut states_vec = Vec::<States>::new();
+    let mut transitions_vec = Vec::<Transitions>::new();
 
     // -----------------------------------------------------
 
     for state in pn.S.iter() {
-        let string_type1 = StringType {
-            r#type: Some(state.sname.clone()),
-            format: None,
-        };
-        let string_type2 = StringType {
-            r#type: Some(state.sname.clone()),
-            format: None,
-        };
-        let state_properties = StateProperties {
-            id: string_type1.clone(),
-            name: string_type2.clone(),
+        let states = States {
+            id: state.sname.clone(),
+            name: state.sname.clone(),
             grounding: None,
             initial: None,
         };
-        let state_items = StateItems {
-            r#type: "object".to_string(),
-            properties: state_properties.clone(),
-            required: None,
-        };
-        state_items_vec.push(state_items.clone());
+        states_vec.push(states.clone());
     }
 
     for (i, trans) in pn.T.clone().iter().enumerate() {
         // convert transition index to base 1
         let transition = i.clone() + 1;
 
-        let string_type1 = StringType {
-            r#type: Some(trans.tname.clone()),
-            format: None,
-        };
         // construct array of incoming states
-        let mut string_type_vec1 = Vec::<StringType>::new();
+        let mut string_vec1 = Vec::<String>::new();
         for incoming in pn.I.clone().iter() {
             if incoming.it.clone() == transition {
                 let state_idx = incoming.is.clone() - 1; // 0 based to index the rust vec
                 let state_name = pn.S[state_idx as usize].sname.clone();
-                let string_type_temp = StringType {
-                    r#type: Some(state_name.clone()),
-                    format: None,
-                };
-                string_type_vec1.push(string_type_temp.clone());
+                string_vec1.push(state_name.clone());
             }
         }
-        let array_type1 = ArrayType {
-            r#type: "array".to_string(),
-            items: Some(string_type_vec1.clone()),
-        };
         // construct array of outgoing states
-        let mut string_type_vec2 = Vec::<StringType>::new();
+        let mut string_vec2 = Vec::<String>::new();
         for outgoing in pn.O.clone().iter() {
             if outgoing.ot.clone() == transition {
                 let state_idx = outgoing.os.clone() - 1; // 0 based to index the rust vec
                 let state_name = pn.S[state_idx as usize].sname.clone();
-                let string_type_temp = StringType {
-                    r#type: Some(state_name.clone()),
-                    format: None,
-                };
-                string_type_vec2.push(string_type_temp.clone());
+                string_vec2.push(state_name.clone());
             }
         }
-        let array_type2 = ArrayType {
-            r#type: "array".to_string(),
-            items: Some(string_type_vec2.clone()),
-        };
-        let transition_properties = TransitionProperties {
-            id: string_type1,
-            input: Some(array_type1),
-            output: Some(array_type2),
-            grounding: None,
+
+        let transitions = Transitions {
+            id: trans.tname.clone(),
+            input: Some(string_vec1.clone()),
+            output: Some(string_vec2.clone()),
             properties: None,
         };
-        let transition_items = TransitionItems {
-            r#type: "object".to_string(),
-            properties: transition_properties,
-            required: None,
-        };
-        transition_items_vec.push(transition_items.clone());
+
+        transitions_vec.push(transitions.clone());
     }
 
     // -----------------------------------------------------------
 
-    let states = States {
-        r#type: "array".to_string(),
-        items: state_items_vec,
-    };
-
-    let transitions = Transitions {
-        r#type: "array".to_string(),
-        items: transition_items_vec,
-    };
-
-    let model_properties = ModelProperties {
-        states: Some(states),
-        transitions: Some(transitions),
+    let model = Model {
+        states: states_vec,
+        transitions: transitions_vec,
         parameters: None,
     };
 
-    let model = Model {
-        r#type: "object".to_string(),
-        properties: model_properties,
-        required: None,
-        additionalproperties: None,
-    };
-
-    let string_name = StringType {
-        r#type: Some("PetriNet".to_string()),
-        format: None,
-    };
-
-    let string_schema = StringType {
-        r#type: Some("PetriNet".to_string()),
-        format: None,
-    };
-
-    let string_model_version = StringType {
-        r#type: Some("PetriNet".to_string()),
-        format: None,
-    };
-
-    let properties = Properties {
-        name: string_name,
-        schema: string_schema,
-        description: None,
-        model_version: string_model_version,
-        properties: None,
-        model: model,
-    };
-
     let mrp = ModelRepPn {
-        schema: "PetriNet".to_string(),
-        r#type: "PetriNet".to_string(),
-        properties: properties,
-        defs: None,
-        additionalproperties: false,
-        required: None,
+        name: "mathml model".to_string(),
+        schema: "https://raw.githubusercontent.com/DARPA-ASKEM/Model-Representations/petrinet_v0.1/petrinet/petrinet_schema.json".to_string(),
+        description: "This is a model from mathml equations".to_string(),
+        model_verison: "0.1".to_string(),
+        model: model.clone(),
+        metadata: None,
     };
 
     return mrp;
