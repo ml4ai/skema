da1a32fb9bc766a29c00516c7f43d3f36cc10ec3
ualiangzhang <liangzh@arizona.edu>
2023-05-26 20:20:10 -0700

    add choices in the evaluation

--- skema/img2mml/utils/bleu_score.py
+++ skema/img2mml/utils/bleu_score.py
@@ -1,12 +1,40 @@
 import os
 from torchtext.data.metrics import bleu_score
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--dataset",
+    choices=["arxiv", "im2mml", "arxiv_im2mml"],
+    default="arxiv_im2mml",
+    help="Choose which dataset to be used for training. Choices: arxiv, im2mml, arxiv_im2mml.",
+)
+parser.add_argument(
+    "--with_fonts",
+    action="store_true",
+    default=False,
+    help="Whether using the dataset with diverse fonts",
+)
+parser.add_argument(
+    "--with_boldface",
+    action="store_true",
+    default=False,
+    help="Whether having boldface in labels",
+)
+args = parser.parse_args()
 
 
 def calculate_bleu_score():
-    tt = open("logs/test_targets_100K.txt").readlines()
-    tp = open("logs/test_predicted_100K.txt").readlines()
-    _tt = open("logs/final_targets.txt", "w")
-    _tp = open("logs/final_preds.txt", "w")
+    dataset = args.dataset
+    if args.with_fonts:
+        dataset = args.mode + "_with_fonts"
+    if args.with_boldface:
+        dataset = args.mode + "_boldface"
+
+    tt = open(f"logs/test_{dataset}_targets_100K.txt").readlines()
+    tp = open(f"logs/test_{dataset}_predicted_100K.txt").readlines()
+    _tt = open(f"logs/{dataset}_final_targets.txt", "w")
+    _tp = open(f"logs/{dataset}_final_preds.txt", "w")
 
     for i, j in zip(tt, tp):
         eos_i = i.find("<eos>")
@@ -15,8 +43,8 @@ def calculate_bleu_score():
         eos_j = j.find("<eos>")
         _tp.write(j[6:eos_j] + "\n")
 
-    test = open("logs/final_targets.txt").readlines()
-    predicted = open("logs/final_preds.txt").readlines()
+    test = open(f"logs/{dataset}_final_targets.txt").readlines()
+    predicted = open(f"logs/{dataset}_final_preds.txt").readlines()
 
     candidate_corpus, references_corpus = [], []
 
