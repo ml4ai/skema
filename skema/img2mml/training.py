24e8a5ceb86db746712ab5f4d066c0e1da0b9f73
gauravsh0812 <gauravs@arizona.edu>
2023-06-02 15:32:33 -0400

    updating

--- skema/img2mml/training.py
+++ skema/img2mml/training.py
@@ -497,7 +497,7 @@ def train_model(
     if config["bin_comparison"]:
         print("comparing bin...")
         from bin_testing import bin_test_dataloader
-        test_dataloader = bin_test_dataloader(config, vocab, device, start=350, end=1000)
+        test_dataloader = bin_test_dataloader(config, vocab, device, start=0, end=50)
 
     test_loss = evaluate(
         model,
