28eeea2e414bae8156acb6259daeedf2fa9a91f4
gauravsh0812 <gauravs@arizona.edu>
2023-06-02 12:37:53 -0400

    update

--- skema/img2mml/temp_testing.py
+++ skema/img2mml/temp_testing.py
@@ -9,6 +9,193 @@ from torchtext.vocab import Vocab
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import SequentialSampler
 from functools import partial
+import subprocess
+import numpy as np
+import time
+import json
+import math
+import argparse
+import logging
+import itertools
+import torchvision
+import torch.nn as nn
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from tqdm import tqdm
+from torch.nn.parallel import DistributedDataParallel as DDP
+from skema.img2mml.preprocessing.preprocess import preprocess_dataset
+from skema.img2mml.models.encoders.cnn_encoder import CNN_Encoder
+from skema.img2mml.models.encoders.resnet_encoder import (
+    ResNet18_Encoder,
+    ResNetBlock,
+)
+from skema.img2mml.models.encoders.xfmer_encoder import Transformer_Encoder
+from skema.img2mml.models.decoders.lstm_decoder import LSTM_Decoder
+from skema.img2mml.models.decoders.xfmer_decoder import Transformer_Decoder
+from skema.img2mml.models.image2mml_lstm import Image2MathML_LSTM
+from skema.img2mml.models.image2mml_xfmer import Image2MathML_Xfmer
+from skema.img2mml.src.train import train
+from skema.img2mml.src.test import evaluate
+
+# opening config file
+
+with open("configs/xfmer_latex_config.json", "r") as cfg:
+    config = json.load(cfg)
+
+torch.backends.cudnn.enabled = False
+
+def set_random_seed(SEED):
+    # set up seed
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+
+def define_model(config, VOCAB, DEVICE):
+    """
+    defining the model
+    initializing encoder, decoder, and model
+    """
+
+    print("defining model...")
+
+    MODEL_TYPE = config["model_type"]
+    INPUT_CHANNELS = config["input_channels"]
+    OUTPUT_DIM = len(VOCAB)
+    EMB_DIM = config["embedding_dim"]
+    ENC_DIM = config["encoder_dim"]
+    DEC_HID_DIM = config["decoder_hid_dim"]
+    DROPOUT = config["dropout"]
+    MAX_LEN = config["max_len"]
+
+    print(f"building {MODEL_TYPE} model...")
+
+    if MODEL_TYPE == "opennmt":
+        ENCODING_TYPE = "row_encoding"
+        N_LAYERS = config["lstm_layers"]
+        TFR = config["teacher_force_ratio"]
+        ENC = CNN_Encoder(INPUT_CHANNELS, DEC_HID_DIM, DROPOUT, DEVICE)
+        DEC = LSTM_Decoder(
+            EMB_DIM,
+            ENC_DIM,
+            DEC_HID_DIM,
+            OUTPUT_DIM,
+            N_LAYERS,
+            DEVICE,
+            DROPOUT,
+            TFR,
+        )
+        model = Image2MathML_LSTM(
+            ENC, DEC, DEVICE, ENCODING_TYPE, MAX_LEN, VOCAB
+        )
+
+    elif MODEL_TYPE == "cnn_xfmer":
+        # transformers params
+        # ENCODING_TYPE will be PositionalEncoding
+        DIM_FEEDFWD = config["dim_feedforward_for_xfmer"]
+        N_HEADS = config["n_xfmer_heads"]
+        N_XFMER_ENCODER_LAYERS = config["n_xfmer_encoder_layers"]
+        N_XFMER_DECODER_LAYERS = config["n_xfmer_decoder_layers"]
+        LEN_DIM = 930
+
+        ENC = {
+            "CNN": CNN_Encoder(INPUT_CHANNELS, DEC_HID_DIM, DROPOUT, DEVICE),
+            "XFMER": Transformer_Encoder(
+                EMB_DIM,
+                DEC_HID_DIM,
+                N_HEADS,
+                DROPOUT,
+                DEVICE,
+                MAX_LEN,
+                N_XFMER_ENCODER_LAYERS,
+                DIM_FEEDFWD,
+                LEN_DIM,
+            ),
+        }
+        DEC = Transformer_Decoder(
+            EMB_DIM,
+            N_HEADS,
+            DEC_HID_DIM,
+            OUTPUT_DIM,
+            DROPOUT,
+            MAX_LEN,
+            N_XFMER_DECODER_LAYERS,
+            DIM_FEEDFWD,
+            DEVICE,
+        )
+
+        model = Image2MathML_Xfmer(ENC, DEC, VOCAB, DEVICE)
+
+    elif MODEL_TYPE == "resnet_xfmer":
+        # transformers params
+        DIM_FEEDFWD = config["dim_feedforward_for_xfmer"]
+        N_HEADS = config["n_xfmer_heads"]
+        N_XFMER_ENCODER_LAYERS = config["n_xfmer_encoder_layers"]
+        N_XFMER_DECODER_LAYERS = config["n_xfmer_decoder_layers"]
+        LEN_DIM = 32
+
+        ENC = {
+            "CNN": ResNet18_Encoder(
+                INPUT_CHANNELS, DEC_HID_DIM, DROPOUT, DEVICE, ResNetBlock
+            ),
+            "XFMER": Transformer_Encoder(
+                EMB_DIM,
+                DEC_HID_DIM,
+                N_HEADS,
+                DROPOUT,
+                DEVICE,
+                MAX_LEN,
+                N_XFMER_ENCODER_LAYERS,
+                DIM_FEEDFWD,
+                LEN_DIM,
+            ),
+        }
+        DEC = Transformer_Decoder(
+            EMB_DIM,
+            N_HEADS,
+            DEC_HID_DIM,
+            OUTPUT_DIM,
+            DROPOUT,
+            MAX_LEN,
+            N_XFMER_DECODER_LAYERS,
+            DIM_FEEDFWD,
+            DEVICE,
+        )
+
+        model = Image2MathML_Xfmer(ENC, DEC, VOCAB, DEVICE)
+
+    return model
+
+
+def init_weights(m):
+    """
+    initializing the model wghts with values
+    drawn from normal distribution.
+    else initialize them with 0.
+    """
+    for name, param in m.named_parameters():
+        if "weight" in name:
+            nn.init.normal_(param.data, mean=0, std=0.01)
+        else:
+            nn.init.constant_(param.data, 0)
+
+
+def count_parameters(model):
+    """
+    counting total number of parameters
+    """
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def epoch_time(start_time, end_time):
+    """
+    epoch timing
+    """
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
 
 class Img2MML_dataset(Dataset):
     def __init__(self, dataframe, vocab, tokenizer):
@@ -73,10 +260,10 @@ class My_pad_collate(object):
 def preprocess_dataset(device, max_len, start=None, end=None, ):
 
     # reading raw text files
-    img_tnsr_path = "/home/gauravs/data/odata/im2data/image_tensors"
+    img_tnsr_path = f"{config['data_path']}/{config['dataset_type']}/image_tensors"
     vocab = open("vocab.txt").readlines()
 
-    df = pd.read_csv("/home/gauravs/data/odata/im2data/test.csv")
+    df = pd.read_csv(f"{config['data_path']}/{config['dataset_type']}/test.csv")
     imgs, eqns = df["IMG"], df["EQUATION"]
 
     eqns_arr = list()
@@ -118,9 +305,109 @@ def preprocess_dataset(device, max_len, start=None, end=None, ):
 
     return test_dataloader, vocab
 
-if __name__ == "__main__":
+def test_model():
 
+    # parameters
+    EPOCHS = config["epochs"]
+    batch_size = config["batch_size"]
+    optimizer_type = config["optimizer_type"]
+    learning_rate = config["learning_rate"]
+    weight_decay = config["weight_decay"]
+    use_scheduler = config["use_scheduler"]
+    starting_lr = config["starting_lr"]
+    step_size = config["step_size"]
+    gamma = config["gamma"]
+    (beta_1, beta_2) = config["beta_1"], config["beta_2"]
+    momentum = config["momentum"]
+    CLIP = config["clip"]
+    SEED = config["seed"]
+    min_length_bean_search_normalization = config[
+        "min_length_bean_search_normalization"
+    ]
+    alpha = config["beam_search_alpha"]
+    beam_k = config["beam_k"]
+    model_type = config["model_type"]
+    dataset_type = config["dataset_type"]
+    load_trained_model_for_testing = config["testing"]
+    cont_training = config["continue_training_from_last_saved_model"]
+    g2p = config["garbage2pad"]
+    use_single_gpu = config["use_single_gpu"]
+    ddp = config["DDP"]
+    dataparallel = config["DataParallel"]
+    dataParallel_ids = config["DataParallel_ids"]
+    world_size = config["world_size"]
+    early_stopping = config["early_stopping"]
+    early_stopping_counts = config["early_stopping_counts"]
+
+    # set_random_seed
+    set_random_seed(SEED)
+    test_dataloader, vocab = preprocess_dataset(device,max_len)
     device = "cuda:5"
-    # start, end = sys.argv[-2], sys.argv[-1]
-    max_len = sys.argv[-1]
-    preprocess_dataset(device,max_len)
+    model = define_model(config, vocab, device).to(device)
+
+    print("MODEL: ")
+    print(f"The model has {count_parameters(model)} trainable parameters")
+
+    # intializing loss function
+    criterion = torch.nn.CrossEntropyLoss(ignore_index=vocab.stoi["<pad>"])
+
+    # optimizer
+    _lr = starting_lr if use_scheduler else learning_rate
+    optimizer = torch.optim.Adam(
+            params=model.parameters(),
+            lr=_lr,
+            weight_decay=weight_decay,
+            betas=(beta_1, beta_2),
+        )
+
+    trg_pad_idx = vocab.stoi["<pad>"]
+
+    # raw data paths
+    img_tnsr_path = f"{config['data_path']}/{config['dataset_type']}/image_tensors"
+    print(
+        "loading best saved model: ",
+        f"trained_models/{model_type}_{dataset_type}_{config['markup']}_best.pt",
+    )
+    try:
+        # loading pre_tained_model
+        model.load_state_dict(
+            torch.load(f"trained_models/{model_type}_{dataset_type}_{config['markup']}_best.pt")
+        )
+    except:
+        try:
+            # removing "module." from keys
+            pretrained_dict = {
+                key.replace("module.", ""): value
+                for key, value in model.state_dict().items()
+            }
+        except:
+            # adding "module." in keys
+            pretrained_dict = {
+                f"module.{key}": value
+                for key, value in model.state_dict().items()
+            }
+
+        model.load_state_dict(pretrained_dict)
+
+    test_loss = evaluate(
+        model,
+        model_type,
+        img_tnsr_path,
+        batch_size,
+        test_dataloader,
+        criterion,
+        device,
+        vocab,
+        beam_params=beam_params,
+        is_test=True,
+        ddp=ddp,
+        rank=rank,
+        g2p=g2p,
+    )
+
+
+    print(
+        f"| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |"
+    )
+
+test_model()
