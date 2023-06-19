07d215bf4a65f6806fc2461e4dcace05d4c50714
Enrique Noriega <enoriega@arizona.edu>
2023-06-19 12:58:58 -0700

    Added output path CLI parameter

--- skema/text_reading/model_linker/main.py
+++ skema/text_reading/model_linker/main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import json
+from pathlib import Path
 from typing import Optional
 
 import fire.fire_test
@@ -12,6 +13,7 @@ def link_amr(
         amr_path: str,                                                     # Path of the AMR model
         attribute_collection: str,                                         # Path to the attribute collection
         amr_type: str,                                                     # AMR model type. I.e. "petrinet" or "regnet"
+        output_path: Optional[str] = None,                                        # Output file path
         similarity_model: str = "sentence-transformers/all-MiniLM-L6-v2",  # Transformer model to compute similarities
         device: Optional[str] = None                                       # PyTorch device to run the model on
         ):
@@ -19,19 +21,25 @@ def link_amr(
 
     if amr_type == "petrinet":
         Linker = PetriNetLinker
+    # elif amr_type == "regnet":
+    #     Linker = RegNetLinker
     else:
         raise NotImplementedError(f"{amr_type} AMR currently not supported")
 
     with open(amr_path) as f:
-        amr_path = json.load(f)
+        amr = json.load(f)
 
     extractions = AttributeCollection.from_json(attribute_collection)
 
     linker = Linker(model_name=similarity_model, device=device)
 
-    linked_model = linker.link_model_to_text_extractions(amr_path, extractions)
+    linked_model = linker.link_model_to_text_extractions(amr, extractions)
 
-    with open('petrinet_aligned.json', 'w') as f:
+    if not output_path:
+        input_amr_name = str(Path(amr_path).name)
+        output_path = f'linked_{input_amr_name}'
+
+    with open(output_path, 'w') as f:
         json.dump(linked_model, f, default=str, indent=2)
 
 
