b86350e51db262f9c93494d596fda14566cb6c37
Gus Hahn-Powell <gushahnpowell@gmail.com>
2023-06-22 23:09:36 -0700

    Python examples in OpenAPI docs for eq2mml REST

--- skema/data/eq2mml/__init__.py
+++ skema/data/eq2mml/__init__.py
@@ -1,7 +1,14 @@
+from typing import Text
 from pathlib import Path
+import base64
 
 # NOTE: generate additional images with https://latex2png.com/
 
-img_path_bayes_rule_eqn = str(Path(__file__).parent / "images" / "bayes-rule.png")
+def _img2b64(img_name: Text) -> bytes:
+  p = Path(__file__).parent / "images" / img_name
+  with p.open("rb") as infile:
+    img_bytes = infile.read()
+    return base64.b64encode(img_bytes).decode("utf-8")
 
-# base64.encode(<my file bytes>).decode("utf-8")
+img_b64_bayes_transparent = _img2b64("bayes-rule-transparent.png")
+img_b64_bayes_white_bg = _img2b64("bayes-rule-white-bg.png")
--- skema/img2mml/eq2mml.py
+++ skema/img2mml/eq2mml.py
@@ -7,10 +7,11 @@ node data_generation/mathjax_server.js
 
 from typing import Text, Union
 from typing_extensions import Annotated
-from fastapi import FastAPI, File, Response, Query
+from fastapi import FastAPI, Body, File, Response, Request, Query
 from skema.img2mml.api import (get_mathml_from_bytes, get_mathml_from_latex)
+from skema.data.eq2mml import img_b64_bayes_white_bg
 from pydantic import BaseModel, Field
-
+import base64
 
 EquationQueryParameter = Annotated[
   Text,
@@ -45,7 +46,7 @@ ImageBytes = Annotated[
 ]
 
 class LatexEquation(BaseModel):
-    tex_src: Text = Field(description="The LaTeX equation to process")
+    tex_src: Text = Field(title="LaTeX equation", description="The LaTeX equation to process")
     class Config:
         schema_extra = {
             "example": {
@@ -67,17 +68,61 @@ def healthcheck():
     return "The eq2mml service is running."
 
 @app.post("/image/mml", summary="Get MathML representation of an equation image")
-async def post_image_to_mathml(data: ImageBytes) -> Text:
+async def post_image_to_mathml(data: ImageBytes) -> Response:
     """
     Endpoint for generating MathML from an input image.
+
+    ### Python example
+    ```
+    import requests
+
+    files = {
+      "data": open("bayes-rule-white-bg.png", "rb"),
+    }
+    r = requests.post("http://0.0.0.0:8000/image/mml", files=files)
+    print(r.text)
     """
     # convert bytes of png image to tensor
-    return get_mathml_from_bytes(data)
+    res =  get_mathml_from_bytes(data)
+    print(res)
+    print(type(res))
+    return Response(content=res, media_type="application/xml")
+
+@app.post("/image/base64/mml", summary="Get MathML representation of an equation image")
+async def post_b64image_to_mathml(request: Request) -> Response:
+    """
+    Endpoint for generating MathML from an input image.
+
+    ### Python example
+    ```
+    from pathlib import Path
+    import base64
+    import requests
+
+    url = "http://0.0.0.0:8000/image/base64/mml"
+    with Path("bayes-rule-white-bg.png").open("rb") as infile:
+      img_bytes = infile.read()
+    img_b64 = base64.b64encode(img_bytes).decode("utf-8")
+    r = requests.post(url, data=img_b64)
+    print(r.text)
+    """
+    img_b64 = await request.body()
+    img_bytes = base64.b64decode(img_b64)
+    # convert bytes of png image to tensor
+    res =  get_mathml_from_bytes(img_bytes)
+    return Response(content=res, media_type="application/xml")
 
 @app.get("/latex/mml", summary="Get MathML representation of a LaTeX equation")
 async def get_tex_to_mathml(tex_src: EquationQueryParameter) -> Response:
     """
     GET endpoint for generating MathML from an input LaTeX equation.
+
+    ### Python example
+    ```
+    import requests
+
+    r = requests.get("http://0.0.0.0:8000/latex/mml", params={"tex_src":"E = mc^{c}"})
+    print(r.text)
     """
     return process_latex_equation(tex_src)
 
@@ -85,6 +130,13 @@ async def get_tex_to_mathml(tex_src: EquationQueryParameter) -> Response:
 async def post_tex_to_mathml(eqn: LatexEquation) -> Response:
     """
     Endpoint for generating MathML from an input LaTeX equation.
+
+    ### Python example
+    ```
+    import requests
+
+    r = requests.post("http://0.0.0.0:8000/latex/mml", json={"tex_src":"E = mc^{c}"})
+    print(r.text)
     """
     # convert latex string to presentation mathml
     return process_latex_equation(eqn.tex_src)
