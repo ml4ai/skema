99a28dd1b31c0d8dce2cae1df521bff200798e7c
ualiangzhang <liangzh@arizona.edu>
2023-06-27 18:51:43 -0700

    training v3: update the training weight from 0.5 to 0.25

--- skema/img2mml/src/train.py
+++ skema/img2mml/src/train.py
@@ -230,7 +230,7 @@ def train(
     ddp=False,
     rank=None,
     vocab=None,
-    weight=0.5,
+    weight=0.25,
 ):
     # train mode is ON i.e. dropout and normalization tech. will be used
     model.train()
