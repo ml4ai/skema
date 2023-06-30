75763427a087ee6b796bec348c946ea6f1a74643
Vincent Raymond <vincent@lum.ai>
2023-06-30 15:08:30 -0400

    Add /fn-supported-file-extensions endpoint and additional skema-py endpoint updates (#288)
    - Adds /fn-supported-file-extensions endpoint which returns the file
    extensions that are currently supported by the code2fn pipeline
    - system_to_gromet() now returns a Dict instead of a raw JSON string.
    The placeholder json.load() calls have been removed from
    fn_given_filepaths and fn_given_filepaths_zip
    - Updates description of System fields
    - Updates endpoint descriptions and examples

--- skema/skema_py/server.py
+++ skema/skema_py/server.py
@@ -2,7 +2,7 @@ import json
 import os
 import tempfile
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Dict, Optional
 from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
@@ -14,7 +14,11 @@ import skema.skema_py.petris
 
 from skema.program_analysis.multi_file_ingester import process_file_system
 from skema.program_analysis.snippet_ingester import process_snippet
-from skema.utils.fold import dictionary_to_gromet_json, del_nulls
+from skema.program_analysis.fn_unifier import align_full_system
+from skema.program_analysis.JSON2GroMEt.json2gromet import json_to_gromet
+from skema.program_analysis.comments import CodeComments
+
+FN_SUPPORTED_FILE_EXTENSIONS = [".py", ".f95", ".f"]
 
 
 class Ports(BaseModel):
@@ -23,13 +27,29 @@ class Ports(BaseModel):
 
 
 class System(BaseModel):
-    files: List[str] = Field(description="The file name corresponding to each entry in `blobs`", example=["example.py"])
-    blobs: List[str] = Field(decription="Contents of each file to be analyzed", example=["greet = lambda: print('howdy!')\ngreet()"])
-    system_name: Optional[str] = Field(default=None, decription="A model name to associate with the provided code", example="my-system")
-    root_name: Optional[str] = Field(default=None, decription="????", example=None)
+    files: List[str] = Field(
+        description="The relative file path from the directory specified by `root_name`, corresponding to each entry in `blobs`",
+        example=["example1.py", "dir/example2.py"],
+    )
+    blobs: List[str] = Field(
+        decription="Contents of each file to be analyzed",
+        example=["greet = lambda: print('howdy!')\ngreet()"],
+    )
+    system_name: Optional[str] = Field(
+        default=None,
+        decription="A model name to associate with the provided code",
+        example="my-system",
+    )
+    root_name: Optional[str] = Field(
+        default=None,
+        decription="The name of the code system's root directory.",
+        example="my-system",
+    )
+    comments: Optional[CodeComments] = Field(
+        default=None, description="A dictionary containing the ", example=""
+    )
 
 
-# FIXME: why does this return a a string?
 def system_to_gromet(system: System):
     """Convert a System to Gromet JSON"""
 
@@ -41,16 +61,12 @@ def system_to_gromet(system: System):
 
         # Create files and intermediate directories
         for index, file in enumerate(system.files):
-            file_path = Path(
-                tmp_path, system.root_name or "", file
-            )
+            file_path = Path(tmp_path, system.root_name or "", file)
             file_path.parent.mkdir(parents=True, exist_ok=True)
             file_path.write_text(system.blobs[index])
 
         # Create system_filepaths.txt
-        system_filepaths = Path(
-            tmp_path, "system_filepaths.txt"
-        )
+        system_filepaths = Path(tmp_path, "system_filepaths.txt")
         system_filepaths.write_text("\n".join(system.files))
 
         ## Run pipeline
@@ -60,9 +76,12 @@ def system_to_gromet(system: System):
             str(system_filepaths),
         )
 
-    # Convert gromet data-model to json
-    gromet_collection_dict = gromet_collection.to_dict()
-    return dictionary_to_gromet_json(del_nulls(gromet_collection_dict))
+    # If comments are included in request, run the unifier to add them to the Gromet
+    if system.comments:
+        align_full_system(gromet_collection, system.comments)
+
+    # Convert Gromet data-model to dict for return
+    return gromet_collection.to_dict()
 
 
 app = FastAPI()
@@ -71,6 +90,26 @@ app = FastAPI()
 @app.get("/ping", summary="Ping endpoint to test health of service")
 def ping():
     return "The skema-py service is running."
+
+
+@app.get(
+    "/fn-supported-file-extensions",
+    summary="Endpoint for checking which files extensions are currently supported by code2fn pipeline.",
+    response_model=List[str],
+)
+def fn_supported_file_extensions():
+    """
+    Returns a List[str] where each entry in the list represents a file extension.
+
+    ### Python example
+    ```
+    import requests
+
+    response = requests.get("http://0.0.0.0:8000/fn-supported-file-extensions")
+    supported_extensions = response.json()
+
+    """
+    return FN_SUPPORTED_FILE_EXTENSIONS
 
 
 @app.post(
@@ -82,21 +121,32 @@ def ping():
 )
 async def fn_given_filepaths(system: System):
     """
-    Endpoint for generating Gromet JSON from a .
-
+    Endpoint for generating Gromet JSON from a serialized code system.
     ### Python example
+
     ```
     import requests
 
+    # Single file
     system = {
       "files": ["exp1.py"],
       "blobs": ["x=2"]
     }
     response = requests.post("http://0.0.0.0:8000/fn-given-filepaths", json=system)
     gromet_json = response.json()
+
+    # Multi file
+    system = {
+      "files": ["exp1.py", "exp1.f"],
+      "blobs": ["x=2", "program exp1\\ninteger::x=2\\nend program exp1"],
+      "system_name": "exp1",
+      "root_name": "exp1"
+    }
+    response = requests.post("http://0.0.0.0:8000/fn-given-filepaths", json=system)
+    gromet_json = response.json()
     """
-    # FIXME: remove json.loads() wrapper after use of dictionary_to_gromet_json is addressed
-    return json.loads(system_to_gromet(system))
+
+    return system_to_gromet(system)
 
 
 @app.post(
@@ -106,23 +156,32 @@ async def fn_given_filepaths(system: System):
         " get a GroMEt FN Module collection back."
     ),
 )
-async def root(zip_file: UploadFile = File()):
+async def fn_given_filepaths_zip(zip_file: UploadFile = File()):
     """
-    Endpoint for generating Gromet JSON from a zip archive.
+    Endpoint for generating Gromet JSON from a zip archive of arbitrary depth and structure.
+    All source files with a supported file extension (/fn-supported-file-extensions) will be processed as a single GrometFNModuleCollection.
 
     ### Python example
     ```
     import requests
+    import shutil
+    from pathlib import Path
+
+    # Format input/output paths
+    input_name = "system_test"
+    output_name = "system_test.zip"
+    input_path = Path("/data") / "skema" / "code" / input_name
+    output_path = Path("/data") / "skema" / "code" / output_name
+
+    # Convert source directory to zip archive
+    shutil.make_archive(input_path, "zip", input_path)
 
     files = {
-      "zip_file": open("code_system.zip", "rb"),
+      "zip_file": open(output_path, "rb"),
     }
     response = requests.post("http://0.0.0.0:8000/fn-given-filepaths-zip", files=files)
     gromet_json = response.json()
     """
-
-    # Currently, we rely on the file extension to know which language front end to send the source code to.
-    supported_file_extensions = [".py", ".f", ".f95"]
 
     # To process a zip file, we first convert it to a System object, and then pass it to system_to_gromet.
     files = []
@@ -130,7 +189,7 @@ async def root(zip_file: UploadFile = File()):
     with ZipFile(BytesIO(zip_file.file.read()), "r") as zip:
         for file in zip.namelist():
             file_obj = Path(file)
-            if file_obj.suffix in supported_file_extensions:
+            if file_obj.suffix in FN_SUPPORTED_FILE_EXTENSIONS:
                 files.append(file)
                 blobs.append(zip.open(file).read())
 
@@ -141,8 +200,8 @@ async def root(zip_file: UploadFile = File()):
     system = System(
         files=files, blobs=blobs, system_name=system_name, root_name=root_name
     )
-    # FIXME: remove json.loads() wrapper after use of dictionary_to_gromet_json is addressed
-    return json.loads(system_to_gromet(system))
+
+    return system_to_gromet(system)
 
 
 @app.post(
