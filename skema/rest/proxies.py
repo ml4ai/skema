53dde0271d146c03d769a6c464a9c8bb9b13dd2b
Enrique Noriega <enoriega@arizona.edu>
2023-07-08 12:57:40 -0700

    Changed the default OPENAI

--- skema/rest/proxies.py
+++ skema/rest/proxies.py
@@ -21,4 +21,4 @@ SKEMA_MATHJAX_ADDRESS = os.environ.get(
 # Text Reading services
 MIT_TR_ADDRESS = os.environ.get("MIT_TR_ADDRESS", "https://mit-tr.askem.lum.ai")
 SKEMA_TR_ADDRESS = os.environ.get("SKEMA_TR_ADDRESS", "https://skema-tr.askem.lum.ai")
-OPENAI_KEY = os.environ.get("OPENAI_KEY", "<YOUR KEY WITH GPT-4 HERE>")
+OPENAI_KEY = os.environ.get("OPENAI_KEY", "YOU_FORGOT_TO_SET_OPENAI_KEY")
