663f24040ed9e809fd308a8e59db8070337a9444
Vincent Raymond <vincent@lum.ai>
2023-07-13 12:00:50 -0400

    Adding support for multi-module CAST ingestion to multi_file_ingester.py

--- skema/program_analysis/multi_file_ingester.py
+++ skema/program_analysis/multi_file_ingester.py
@@ -1,8 +1,8 @@
 import argparse
 import glob
-
 import sys
 import os.path
+from typing import List
 
 from skema.gromet import GROMET_VERSION
 from skema.gromet.fn import (
@@ -64,46 +64,55 @@ def process_file_system(
             else:
                 print(f"File extension not supported for {full_file}")
             
-            cur_dir = os.getcwd()
-            os.chdir(os.path.join(os.getcwd(), path))
-            generated_gromet = ann_cast_pipeline(
-                cast, gromet=True, to_file=False, from_obj=True
-            )
-            os.chdir(cur_dir)
+            # The Fortran CAST inteface (TS2CAST) can produce multiple CAST modules.
+            # However, the Python interface (python2cast) will only return a single module.
+            # This workaround will normalize a single CAST module into a list for consistent processing.
+            if isinstance(cast, List):
+                cast_list = cast
+            else:
+                cast_list = [cast]
 
-            # Then, after we generate the GroMEt we store it in the 'modules' field
-            # and store its path in the 'module_index' field
-            module_collection.modules.append(generated_gromet)
-
-            # DONE: Change this so that it's the dotted path from the root
-            # i.e. like model.view.sir" like it shows up in Python
-            source_directory = os.path.basename(
-                os.path.normpath(root_dir)
-            )  # We just need the last directory of the path, not the complete path
-            os_module_path = os.path.join(source_directory, f)
-                       
-            # Normalize the path across os and then convert to module dot notation
-            python_module_path = ".".join(os.path.normpath(os_module_path).split(os.path.sep))
-            python_module_path = python_module_path.replace(".py", "").strip()
-            
-            module_collection.module_index.append(python_module_path)
-
-            # Done: Determine how we know a gromet goes in the 'executable' field
-            # We do this by finding all user_defined top level functions in the Gromet
-            # and check if the name 'main' is among them
-            function_networks = [
-                fn
-                for fn in generated_gromet.fn_array
-            ]
-            defined_functions = [
-                fn.b[0].name
-                for fn in function_networks
-                if fn.b[0].function_type == "FUNCTION"
-            ]
-            if "main" in defined_functions:
-                module_collection.executables.append(
-                    len(module_collection.module_index)
+            for cast_module in cast_list:
+                cur_dir = os.getcwd()
+                os.chdir(os.path.join(os.getcwd(), path))
+                generated_gromet = ann_cast_pipeline(
+                    cast_module, gromet=True, to_file=False, from_obj=True
                 )
+                os.chdir(cur_dir)
+
+                # Then, after we generate the GroMEt we store it in the 'modules' field
+                # and store its path in the 'module_index' field
+                module_collection.modules.append(generated_gromet)
+
+                # DONE: Change this so that it's the dotted path from the root
+                # i.e. like model.view.sir" like it shows up in Python
+                source_directory = os.path.basename(
+                    os.path.normpath(root_dir)
+                )  # We just need the last directory of the path, not the complete path
+                os_module_path = os.path.join(source_directory, f)
+                        
+                # Normalize the path across os and then convert to module dot notation
+                python_module_path = ".".join(os.path.normpath(os_module_path).split(os.path.sep))
+                python_module_path = python_module_path.replace(".py", "").strip()
+                
+                module_collection.module_index.append(python_module_path)
+
+                # Done: Determine how we know a gromet goes in the 'executable' field
+                # We do this by finding all user_defined top level functions in the Gromet
+                # and check if the name 'main' is among them
+                function_networks = [
+                    fn
+                    for fn in generated_gromet.fn_array
+                ]
+                defined_functions = [
+                    fn.b[0].name
+                    for fn in function_networks
+                    if fn.b[0].function_type == "FUNCTION"
+                ]
+                if "main" in defined_functions:
+                    module_collection.executables.append(
+                        len(module_collection.module_index)
+                    )
 
         except ImportError as e:
             print("FAILURE")
