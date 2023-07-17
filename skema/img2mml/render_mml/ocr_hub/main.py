3d26045fe5f823c5847ea78efb961fcf1668dafe
Adi-UA <abanerjee.email@gmail.com>
2023-07-17 14:16:37 -0700

    Performance Improvement and MathML Editing
    Performance improvements to Mathpix OCR Tweaker
    Added mathml editing via modal to OCR Verify
    Added pretty printing on save
    Minor Code Cleanup
    Minor search fix

--- skema/img2mml/render_mml/ocr_hub/main.py
+++ skema/img2mml/render_mml/ocr_hub/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+import lxml.etree as etree
 import json
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="./static"), name="static")
@@ -22,9 +24,10 @@ async def get_model_results_data(request: Request):
         request.query_params["search[value]"],
     )
     search = search.strip()
-    shouldSearch = search != ""
+    should_search = search != ""
+    search_exact = "search_exact" in request.query_params
 
-    if not shouldSearch:
+    if not should_search:
         # We don't need to search, so return the requested
         # page of data
         # Ref: https://datatables.net/manual/server-side
@@ -38,32 +41,98 @@ async def get_model_results_data(request: Request):
         # Otherwise search for the closest matches
         result_subset = []
         for record in model_results:
-            if search in record["id"]:
-                result_subset.append(record)
+            if not search_exact:
+                if search in record["id"]:
+                    result_subset.append(record)
+            else:
+                if search == record["id"]:
+                    result_subset.append(record)
+                    break
 
     return {
         "draw": draw,
         "recordsTotal": max_length,
-        "recordsFiltered": max_length if not shouldSearch else len(result_subset),
+        "recordsFiltered": max_length if not should_search else len(result_subset),
         "data": result_subset,
     }
 
 
 @app.get("/mathpix_annotation_data")
-async def get_mathpix_annotation_data():
+async def get_mathpix_annotation_data(index=-1, id=None):
+    index = int(index)
+
+    # Get data
     with open(MATHPIX_ANNOTATION_DATA_PATH, "r") as f:
         annotation_data = json.load(f)
-    return annotation_data
+
+    # Return all records or requested record (search by number or id)
+    response = {"data": annotation_data}
+    totalRecords = len(annotation_data)
+
+    if id:
+        response = {"error", f"Image {id} not found"}
+        for i, entry in enumerate(annotation_data):
+            if entry["id"] == id:
+                index = i
+                response = {"data": [entry]}
+                break
+    elif index != -1:
+        response = {"data": [annotation_data[index]]}
+
+    response["totalRecords"] = totalRecords
+    response["index"] = index
+    return response
 
 
 @app.post("/mathpix_annotation_data")
 async def post_mathpix_annotation_data(request: Request):
-    updated_annotation_data = await request.json()
+    request_body = await request.json()
+
+    index = request_body["index"]
+    data = request_body["data"]
+
+    # Update data
+    with open(MATHPIX_ANNOTATION_DATA_PATH, "r") as f:
+        annotation_data = json.load(f)
+    annotation_data[index] = data
 
     with open(MATHPIX_ANNOTATION_DATA_PATH, "w") as f:
-        json.dump(updated_annotation_data, f, indent=4)
+        json.dump(annotation_data, f, indent=4)
 
-    return {"message": "success"}
+    return True
+
+
+@app.post("/model_results")
+async def post_model_results(request: Request):
+    request_body = await request.json()
+
+    id, raw_mathml = request_body["id"], request_body["mathml"]
+
+    # Try to pretty print MathML before saving
+    mathml = raw_mathml
+    try:
+        mathml_tree = etree.fromstring(raw_mathml)
+        pretty_printed_mathml = etree.tostring(
+            mathml_tree, pretty_print=True, encoding="unicode"
+        )
+        mathml = pretty_printed_mathml
+    except:
+        print("Warning: Could not pretty print MathML before saving.")
+
+    # Grab data
+    with open(MODEL_RESULTS_DATA_PATH, "r") as f:
+        model_results = json.load(f)
+
+    # Update and save data
+    for result in model_results:
+        if result["id"] == id:
+            result["mathml"] = mathml
+            break
+
+    with open(MODEL_RESULTS_DATA_PATH, "w") as f:
+        json.dump(model_results, f, indent=4)
+
+    return True
 
 
 @app.get("/", response_class=FileResponse)
--- skema/img2mml/render_mml/ocr_hub/static/js/mathpix_ocr_tweaker.js
+++ skema/img2mml/render_mml/ocr_hub/static/js/mathpix_ocr_tweaker.js
@@ -4,42 +4,39 @@
 const BASE_IMG_URL =
   "https://raw.githubusercontent.com/imzoc/mathpix-annotation/master/mathml-images/images_filtered/";
 
-async function show_original_image(idx = 0) {
-  let image_id = eqn_src[idx]["id"];
+async function show_original_image() {
+  const image_id = data.id;
   $("#original-image-from-paper").attr("src", `${BASE_IMG_URL}${image_id}.png`);
 }
 
-async function show_image_info(idx = 0) {
-  $("#img-count").text(`${idx + 1}/${eqn_src.length}`); // Image number
+async function show_image_info() {
+  $("#img-count").text(`${cur_data_index + 1}/${total_data_size}`); // Image number
 
   // ID and confidence score
-  let image_id_section = `<p><b>Image ID:</b> ${eqn_src[idx]["id"]}</p>`;
-  let mathpix_confidence = (
-    parseFloat(eqn_src[idx]["confidence"]) * 100
-  ).toFixed(2);
-  let mathpix_confidence_section = `<b> Mathpix Confidence Score:</b> ${mathpix_confidence}%`;
+  const image_id_section = `<p><b>Image ID:</b> ${data.id}</p>`;
+  const mathpix_confidence = (parseFloat(data.confidence) * 100).toFixed(2);
+  const mathpix_confidence_section = `<b> Mathpix Confidence Score:</b> ${mathpix_confidence}%`;
   $("#image-info").html(`${image_id_section}${mathpix_confidence_section}`);
 }
 
-async function show_latex(idx = 0) {
-  let latex_code = eqn_src[idx]["latex"];
+async function show_latex() {
+  const latex_code = data.latex;
   $("#latex-code").text(latex_code); // code
-  $("#latex-render").text(`$$${eqn_src[idx]["latex"]}$$`); // render
+  $("#latex-render").text(`$$${data.latex}$$`); // render
 }
 
-async function show_mathml(idx = 0) {
-  let mathml_code = eqn_src[idx]["mathml"];
+async function show_mathml() {
+  const mathml_code = data.mathml;
   $("#mathml-code").text(mathml_code); // code
   $("#mathml-render").html(mathml_code);
 }
 
-async function show_data(idx = 0) {
-  $("#edit-latex-form")[0].reset();
-  $("#edit-mathml-form")[0].reset();
-  show_latex(idx);
-  show_mathml(idx);
-  show_image_info(idx);
-  show_original_image(idx);
+async function refresh_view() {
+  show_latex();
+  show_mathml();
+  show_image_info();
+  show_original_image();
+  update_btn_visibility();
   MathJax.typeset();
 }
 
@@ -50,43 +47,41 @@ async function update_btn_visibility() {
   $("#prev-btn").addClass("btn-secondary");
   $("#next-btn").addClass("btn-secondary");
 
-  if (IDX > 0) {
+  if (cur_data_index > 0) {
     $("#prev-btn").addClass("btn-primary");
     $("#prev-btn").removeClass("btn-secondary");
   }
-  if (IDX < eqn_src.length - 1) {
+  if (cur_data_index < total_data_size - 1) {
     $("#next-btn").addClass("btn-primary");
     $("#next-btn").removeClass("btn-secondary");
   }
 }
 
 $("#prev-btn").click(async () => {
-  if (IDX > 0) {
-    IDX -= 1;
-    show_data(IDX);
+  if (cur_data_index > 0) {
+    await fetch_data(cur_data_index - 1);
+    refresh_view();
   }
-  update_btn_visibility();
 });
 
 $("#next-btn").click(async () => {
-  if (IDX < eqn_src.length - 1) {
-    IDX += 1;
-    show_data(IDX);
+  if (cur_data_index < total_data_size - 1) {
+    await fetch_data(cur_data_index + 1);
+    refresh_view();
   }
-  update_btn_visibility();
 });
 
 /**
  * Preview changes being made to the code
  */
 $("#latex-code").on("keyup", () => {
-  let wip_latex_code = $("#latex-code").val();
+  const wip_latex_code = $("#latex-code").val();
   $("#latex-render").text(`$$${wip_latex_code}$$`);
   MathJax.typeset();
 });
 
 $("#mathml-code").on("keyup", () => {
-  let wip_mathml_code = $("#mathml-code").val();
+  const wip_mathml_code = $("#mathml-code").val();
   $("#mathml-render").html(wip_mathml_code);
   MathJax.typeset();
 });
@@ -94,22 +89,25 @@ $("#mathml-code").on("keyup", () => {
 /**
  * Save changes permanently
  */
-
 $("#edit-latex-form").submit(async (e) => {
   e.preventDefault();
   if (confirm("Permanently save changes to LaTeX code?")) {
-    eqn_src[IDX]["latex"] = $("#latex-code").val();
-    await update_data();
-    show_data(IDX);
+    const updatedData = data;
+    updatedData.latex = $("#latex-code").val();
+    await post_data(cur_data_index, updatedData);
+    $("#edit-latex-form")[0].reset();
+    refresh_view();
   }
 });
 
 $("#edit-mathml-form").submit(async (e) => {
   e.preventDefault();
   if (confirm("Permanently save changes to MathML code?")) {
-    eqn_src[IDX]["mathml"] = $("#mathml-code").val();
-    await update_data();
-    show_data(IDX);
+    const updatedData = data;
+    updatedData.mathml = $("#mathml-code").val();
+    await post_data(cur_data_index, updatedData);
+    $("#edit-mathml-form")[0].reset();
+    refresh_view();
   }
 });
 
@@ -119,29 +117,22 @@ $("#edit-mathml-form").submit(async (e) => {
 $("#image-search").submit(async (e) => {
   e.preventDefault();
   let searched_id = $("#image-id").val();
-  let searched_no = $("#image-no").val();
+  const searched_no = $("#image-no").val();
 
   if (searched_id !== "") {
     searched_id = searched_id.trim();
     // Search by ID if it was input
-    for (const [i, e] of eqn_src.entries()) {
-      if (e["id"] === searched_id) {
-        IDX = i;
-        show_data(IDX);
-        update_btn_visibility();
-        $("#image-search")[0].reset();
-        return;
-      }
-    }
-  } else {
-    // Search by number if it was input
-    if (searched_no > 0 && searched_no <= eqn_src.length) {
-      IDX = searched_no - 1;
-      show_data(IDX);
-      update_btn_visibility();
-      $("#image-search")[0].reset();
-      return;
-    }
+    await fetch_data(-1, searched_id);
+    refresh_view();
+    $("#image-search")[0].reset();
+    return;
+  }
+  // Search by number if it was input
+  if (searched_no > 0 && searched_no <= total_data_size) {
+    await fetch_data(searched_no - 1);
+    refresh_view();
+    $("#image-search")[0].reset();
+    return;
   }
 
   $("#image-search")[0].reset();
@@ -151,32 +142,36 @@ $("#image-search").submit(async (e) => {
 /**
  * Handle updating data between client and server
  */
-async function fetch_data() {
-  const response = await fetch("/mathpix_annotation_data/");
-  eqn_src = await response.json();
+async function fetch_data(index = 0, id = null) {
+  let response = null;
+  if (id) {
+    response = await fetch(`/mathpix_annotation_data?id=${id}`);
+  } else {
+    response = await fetch(`/mathpix_annotation_data?index=${index}`);
+  }
+
+  response_data = await response.json();
+  data = response_data.data[0];
+  total_data_size = response_data.totalRecords;
+  cur_data_index = response_data.index;
 }
 
-async function post_data() {
+async function post_data(index = cur_data_index, updatedData = data) {
+  const postData = { index, data: updatedData };
   await fetch("/mathpix_annotation_data/", {
     method: "POST",
-    body: JSON.stringify(eqn_src),
+    body: JSON.stringify(postData),
   });
-}
-
-async function update_data() {
-  // Makes sure data on the server and client are always in sync
-  await post_data();
-  await fetch_data();
 }
 
 /**
  * Initial View
  */
-var IDX = 0;
-var eqn_src = undefined;
+var cur_data_index = null;
+var total_data_size = null;
+var data = null;
 
-$(document).ready(async function () {
+$(document).ready(async () => {
   await fetch_data();
-  show_data(IDX);
-  update_btn_visibility();
+  refresh_view();
 });
