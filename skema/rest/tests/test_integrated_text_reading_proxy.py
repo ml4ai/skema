8c52fb80182753414e59eff7ffae3b94f6df9cd6
Enrique Noriega <enoriega@arizona.edu>
2023-07-19 12:50:27 -0400

    Added a unit test for the grounding endpoint

--- skema/rest/tests/test_integrated_text_reading_proxy.py
+++ skema/rest/tests/test_integrated_text_reading_proxy.py
@@ -5,6 +5,7 @@ from fastapi.testclient import TestClient
 from fastapi import status
 
 from skema.rest.integrated_text_reading_proxy import app, cosmos_client
+from skema.rest.schema import MiraGroundingOutputItem
 
 client = TestClient(app)
 
@@ -16,10 +17,23 @@ client = TestClient(app)
 def test_cosmos():
     """Test that we are able to fetch COSMOS data correctly"""
     path = Path(__file__).parents[0] / "data" / "integrated_text_reading" / "CHIME_SVIIvR_model.pdf"
-    print(path)
     with path.open("rb") as pdf:
         ret = cosmos_client(path.name, pdf)
     assert ret is not None and len(ret) > 0
+
+
+def test_mira_grounding():
+    """Test that we are getting grounding for entities"""
+    queries = {"queries": ["infected", "suceptible"]}
+    params = {"k": 5}
+    ret = client.post("/ground_to_mira", params=params, json=queries)
+
+    assert ret.status_code == status.HTTP_200_OK
+
+    data = [[MiraGroundingOutputItem(**r) for r in q] for q in ret.json()]
+    assert len(data) == 2, "Service didn't return results for all queries"
+    assert all(len(groundings) == params["k"] for groundings in data), "Service didn't return the requested number of candidates for each query"
+
 
 
 def test_healthcheck():
