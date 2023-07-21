f51670fb5b52af36154e57c5c87670b0b92a2489
Keith Alcock <github@keithalcock.com>
2023-07-20 23:07:01 -0700

    Use native order

--- skema/text_reading/scala/src/test/scala/org/ml4ai/skema/grounding/TestMemoryMappedWordEmbeddingMap.scala
+++ skema/text_reading/scala/src/test/scala/org/ml4ai/skema/grounding/TestMemoryMappedWordEmbeddingMap.scala
@@ -2,14 +2,14 @@ package org.ml4ai.skema.grounding
 
 import org.ml4ai.skema.test.Test
 
-import java.nio.{Buffer, ByteBuffer}
+import java.nio.{Buffer, ByteBuffer, ByteOrder}
 
 class TestMemoryMappedWordEmbeddingMap extends Test {
 
   behavior of "float buffer"
 
   it should "buffer" in {
-    val byteBuffer = ByteBuffer.allocate(8)
+    val byteBuffer = ByteBuffer.allocate(8).order(ByteOrder.nativeOrder())
     val floatBuffer = byteBuffer.asFloatBuffer
     val array = new Array[Float](2)
 
