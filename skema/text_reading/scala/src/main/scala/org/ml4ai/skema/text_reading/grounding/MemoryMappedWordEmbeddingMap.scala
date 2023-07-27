7117553f8f6318f46321a050c1ae050abe59d454
Keith Alcock <github@keithalcock.com>
2023-07-26 22:29:59 -0700

    Clean up

--- skema/text_reading/scala/src/main/scala/org/ml4ai/skema/text_reading/grounding/MemoryMappedWordEmbeddingMap.scala
+++ skema/text_reading/scala/src/main/scala/org/ml4ai/skema/text_reading/grounding/MemoryMappedWordEmbeddingMap.scala
@@ -1,9 +1,8 @@
 package org.ml4ai.skema.text_reading.grounding
 
 import org.clulab.embeddings.{CompactWordEmbeddingMap, WordEmbeddingMap}
-import requests.RequestBlob.ByteSourceRequestBlob
 
-import java.io.{BufferedOutputStream, ByteArrayOutputStream, DataOutputStream, File, FileOutputStream, OutputStream, RandomAccessFile}
+import java.io.{BufferedOutputStream, File, FileOutputStream, RandomAccessFile}
 import java.nio.{Buffer, ByteBuffer, ByteOrder, FloatBuffer}
 import java.nio.channels.FileChannel
 import scala.collection.mutable.{HashMap => MutableHashMap}
@@ -14,50 +13,10 @@ import scala.util.Using
 class MemoryMappedWordEmbeddingMap(buildType: CompactWordEmbeddingMap.BuildType) extends WordEmbeddingMap {
   protected val map: MutableHashMap[String, Int] = buildType.map // (word -> row)
   val columns: Int = buildType.columns
-  val rows: Int = map.size
   val unkEmbeddingOpt: Option[IndexedSeq[Float]] = buildType.unknownArray.map { inside =>
     inside: IndexedSeq[Float]
   }
-  val bytesPerRow = columns * java.lang.Float.BYTES
-  val rowsPerBuffer = Int.MaxValue / bytesPerRow
-  val bufferCount = math.ceil(rows.toDouble / rowsPerBuffer).toInt
-  val floatBuffers = Range(0, bufferCount).map { index =>
-    val file = {
-      val file = File.createTempFile(s"skema-$index-", ".tmp")
-
-      file.deleteOnExit()
-      file
-    }
-
-    // https://stackoverflow.com/questions/9346746/convert-float-to-byte-to-float-again
-    // https://stackoverflow.com/questions/12132595/mappedbytebuffer-asfloatbuffer-vs-in-memory-float-performance
-
-    {
-      val rowRange = Range(index * rowsPerBuffer, math.min((index + 1) * rowsPerBuffer, rows))
-      // Keep the size of this buffer small so that we don't have two sets of data at once.
-      val byteBuffer = ByteBuffer.allocate(bytesPerRow).order(ByteOrder.nativeOrder)
-      val floatBuffer = byteBuffer.asFloatBuffer
-      val maxBufferSize = 100000
-      val bufferSize = maxBufferSize - (maxBufferSize % bytesPerRow)
-
-      Using.resource(new BufferedOutputStream(new FileOutputStream(file), bufferSize)) { outputStream =>
-        rowRange.foreach { row =>
-          floatBuffer.put(buildType.array, row * columns, columns)
-          outputStream.write(byteBuffer.array)
-          (floatBuffer: Buffer).clear()
-        }
-      }
-    }
-
-    {
-      val randomAccessFile = new RandomAccessFile(file, "r")
-      val channel = randomAccessFile.getChannel
-      val mappedMemoryBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size).order(ByteOrder.nativeOrder)
-      val floatBuffer = mappedMemoryBuffer.asFloatBuffer
-
-      floatBuffer
-    }
-  }
+  val (rowsPerBuffer: Int, floatBuffers: IndexedSeq[FloatBuffer]) = mkRowsPerBufferAndFloatBuffers()
 
   /** The dimension of an embedding vector */
   override val dim: Int = columns
@@ -65,21 +24,69 @@ class MemoryMappedWordEmbeddingMap(buildType: CompactWordEmbeddingMap.BuildType)
   // Be careful because this word may not be sanitized!
   override def isOutOfVocabulary(word: String): Boolean = !map.contains(word)
 
+  def ceil(numerator: Int, denominator: Int): Int = {
+    import scala.math.Integral.Implicits._
+    val (quotient, remainder) = numerator /% denominator
+
+    quotient + math.signum(remainder)
+  }
+
+  def mkRowsPerBufferAndFloatBuffers(): (Int, IndexedSeq[FloatBuffer]) = {
+    val rows: Int = map.size
+    val bytesPerRow = columns * java.lang.Float.BYTES
+    val rowsPerBuffer = Int.MaxValue / bytesPerRow
+    val bufferCount = ceil(rows, rowsPerBuffer)
+
+    // Keep the size of this buffer small so that we don't have two sets of data at once.
+    val byteBuffer = ByteBuffer.allocate(bytesPerRow).order(ByteOrder.nativeOrder)
+    val floatBuffer = byteBuffer.asFloatBuffer
+    val maxOutputBufferSize = 100000
+    val outputBufferSize = maxOutputBufferSize - (maxOutputBufferSize % bytesPerRow)
+
+    val floatBuffers = Range(0, bufferCount).map { index =>
+      val file = {
+        val file = File.createTempFile(s"skema-$index-", ".tmp")
+
+        file.deleteOnExit()
+        file
+      }
+
+      // https://stackoverflow.com/questions/9346746/convert-float-to-byte-to-float-again
+      // https://stackoverflow.com/questions/12132595/mappedbytebuffer-asfloatbuffer-vs-in-memory-float-performance
+
+      {
+        val rowRange = Range(index * rowsPerBuffer, math.min((index + 1) * rowsPerBuffer, rows))
+
+        Using.resource(new BufferedOutputStream(new FileOutputStream(file), outputBufferSize)) { outputStream =>
+          rowRange.foreach { row =>
+            (floatBuffer: Buffer).clear()
+            floatBuffer.put(buildType.array, row * columns, columns)
+            outputStream.write(byteBuffer.array)
+          }
+        }
+      }
+
+      {
+        val randomAccessFile = new RandomAccessFile(file, "r")
+        val channel = randomAccessFile.getChannel
+        val mappedMemoryBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size).order(ByteOrder.nativeOrder)
+        val floatBuffer = mappedMemoryBuffer.asFloatBuffer
+
+        floatBuffer
+      }
+    }
+
+    (rowsPerBuffer, floatBuffers)
+  }
+
   protected def get(row: Int): IndexedSeq[Float] = {
-    val array = new Array[Float](columns)
     val bufferIndex = row / rowsPerBuffer
     val bufferOffset = (row % rowsPerBuffer) * columns
+    val floatBuffer = floatBuffers(bufferIndex)
+    val array = new Array[Float](columns)
 
-    try {
-      val floatBuffer = floatBuffers(bufferIndex)
-
-      (floatBuffer: Buffer).position(bufferOffset)
-      floatBuffer.get(array)
-    }
-    catch {
-      case e: Throwable =>
-        println("What happened?")
-    }
+    (floatBuffer: Buffer).position(bufferOffset)
+    floatBuffer.get(array)
     array
   }
 
--- skema/text_reading/scala/src/test/scala/org/ml4ai/skema/grounding/TestMemoryMappedTiming.scala
+++ skema/text_reading/scala/src/test/scala/org/ml4ai/skema/grounding/TestMemoryMappedTiming.scala
@@ -1,15 +1,15 @@
 package org.ml4ai.skema.grounding
 
 import org.clulab.embeddings.CompactWordEmbeddingMap
-import org.clulab.utils.InputStreamer
+import org.clulab.utils.{InputStreamer, Timer}
 import org.ml4ai.skema.test.Test
 import org.ml4ai.skema.text_reading.grounding.MemoryMappedWordEmbeddingMap
 
-class TestMemoryMappedWordEmbeddingMap extends Test {
+class TestMemoryMappedTiming extends Test {
 
   behavior of "MemoryMappedWordEmbeddingMap"
 
-  it should "produce the same results as the im-memory variety" in {
+  it should "be slower than the in-memory variety" in {
     val resourcePath = "/org/clulab/epimodel/epidemiology_embeddings_model.ser" // fits into 2 buffers
 //    val resourcePath = "/org/clulab/spaceweather/spaceweather_model_unigram.ser" // fits into one buffer
     val inputStreamer = new InputStreamer(this)
@@ -20,11 +20,22 @@ class TestMemoryMappedWordEmbeddingMap extends Test {
     val memoryMappedWordEmbeddingsMap = new MemoryMappedWordEmbeddingMap(buildType)
     val knownKeys = compactWordEmbeddingsMap.knownKeys
 
-    knownKeys.foreach { word =>
-      val compactArray = compactWordEmbeddingsMap.get(word).get
-      val memoryMappedArray = memoryMappedWordEmbeddingsMap.get(word).get
-
-      compactArray should equal (memoryMappedArray)
+    val compactTimer = new Timer("compactWordEmbeddingsMap")
+    compactTimer.time {
+      knownKeys.foreach { word =>
+        compactWordEmbeddingsMap.get(word).get
+      }
     }
+    println(compactTimer)
+
+    val memoryMappedTimer = new Timer("memoryMappedWordEmbeddingsMap")
+    memoryMappedTimer.time {
+      knownKeys.foreach { word =>
+        memoryMappedWordEmbeddingsMap.get(word).get
+      }
+    }
+    println(memoryMappedTimer)
+
+    memoryMappedTimer.elapsedTime should be > (compactTimer.elapsedTime)
   }
 }
