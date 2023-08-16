69fcfad936c756f3523e8f42198f5e6168e4197a
Joseph Astier <jastier@arizona.edu>
2023-08-16 11:44:39 -0700

    Added standalone features

--- skema/program_analysis/matlab2cast.py
+++ skema/program_analysis/matlab2cast.py
@@ -3,13 +3,8 @@ import sys
 import json
 import argparse
 
-# hack 
-CALLER_PATH = os.getcwd()
-sys.path.append(CALLER_PATH)
-
-
-from CAST2FN.cast import CAST
-from CAST2FN.visitors.cast_to_agraph_visitor import (
+from skema.program_analysis.CAST2FN.cast import CAST
+from skema.program_analysis.CAST2FN.visitors.cast_to_agraph_visitor import (
     CASTToAGraphVisitor,
 )
 
@@ -17,8 +12,32 @@ from skema.program_analysis.CAST.matlab.matlab_to_cast import MATLAB2CAST
 
 from typing import Optional
 
+def get_args():
+    parser = argparse.ArgumentParser(
+        "Runs MATLAB to CAST pipeline on input MATLAB source file."
+    )
+    parser.add_argument(
+        "--rawjson",
+        help="Dumps out raw JSON contents to stdout",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--stdout",
+        help="Dumps CAST JSON to stdout instead of a file",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--agraph",
+        help="Generates visualization of CAST as a PDF file",
+        action="store_true",
+    )
+    parser.add_argument("input_path", help="input MATLAB source file")
+    options = parser.parse_args()
+    return options
+
+
 def matlab_to_cast(
-    path,
+    input_path,
     agraph=False,
     std_out=False,
     rawjson=False,
@@ -27,7 +46,7 @@ def matlab_to_cast(
     """Create a CAST object from a MATLAB file and serialize it to JSON.
 
     Args:
-        path: Path to the MATLAB .m source file
+        input_path: Path to the MATLAB source file
         agraph: If true, a PDF visualization of the graph is created.
         astprint: View the AST using the astpp module.
         std_out: If true, the CAST JSON is printed to stdout instead
@@ -40,11 +59,11 @@ def matlab_to_cast(
         If cast_obj is set to True, returns the CAST as an object. Else,
         returns None.
     """
+    
 
+    out_cast = MATLAB2CAST(input_path).out_cast
 
-    out_cast = MATLAB2CAST(path).out_cast
-
-    file_name = os.path.basename(path)
+    file_name = os.path.basename(input_path)
     if agraph:
         V = CASTToAGraphVisitor(out_cast)
         last_slash_idx = file_name.rfind("/")
@@ -73,3 +92,11 @@ def matlab_to_cast(
                 out_handle = open(out_name + "--CAST.json", "w")
                 out_handle.write(out_cast.to_json_str())
 
+if __name__ == "__main__":
+    args = get_args()
+    matlab_to_cast(
+        args.input_path,
+        args.agraph,
+        args.stdout,
+        args.rawjson,
+    )
