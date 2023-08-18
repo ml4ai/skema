b43c06c87d20b526bcb68a18076bfddc55b9a48d
Vincent Raymond <vincent@lum.ai>
2023-08-18 09:31:43 -0400

    Adding tree-sitter comment extractor service

---
+++ skema/program_analysis/comment_extractor/tests/examples/comments.cpp
@@ -0,0 +1,19 @@
+// Single line comment 
+
+/* Multi line comment on single line*/
+
+/*
+    Multi line comment 
+    on multiple lines
+*/
+
+/* Docstring in function declaration*/
+int foo();
+
+int foo(){
+	/*
+        Docstring in function definition
+    */
+	return 1;
+}
+
