c81b44ed6fa519195ac1ada8ede71d595a141fec
Vincent Raymond <vincent@lum.ai>
2023-08-21 08:41:55 -0400

    [tree-sitter] - Adds tree-sitter build system and tree-sitter comment extractor  (#398)
    ## New tree-sitter build system
    
    - Creates a new system for building and using tree-sitter parsers under
    `skema.program_analysis.tree_sitter_parsers`.
    - Updates Dockerfile-skema_py and existing CAST/fortran implementation
    to use new build system
    - Removes tree-sitter-fortran submodule 
    - Adds CI tests for building parsers
    ### Adding new parsers
    See the
    [wiki](https://github.com/ml4ai/skema/wiki/Adding-support-for-a-new-tree%E2%80%90sitter-frontend)
    for information on how to add support for new languages.
    
    ### Building parsers
    To build the parsers, run build_parsers.py with command line arguments
    representing the languages you want to build.
    ```bash
    python skema/program_analysis/tree_sitter_parsers/build_parsers.py --c --cpp --fortran --python
    ```
    
    ### Accessing parsers
    You can access the shared object file by importing
    INSTALLED_LANGUAGES_FILEPATH like:
    ```python
    from skema.program_analysis.tree_sitter_parsers.build_parsers import INSTALLED_LANGUAGES_FILEPATH
    ```
    
    ## tree-sitter comment extractor
    
    - Adds a new comment extractor based off of tree-sitter under
    `skema.program_analysis.comment_extractor`.
    - Creates endpoints for extracting comments and checking language
    support
    - Adds CI tests for extracting comments and accessing endpoints.
    ### Supporting new languages
    To add a new language to the comment_extractor, add an entry to
    queries.yaml containing the queries to extract the different comment
    types. This assumes that you have already cloned and built the
    tree-sitter parser using the method above.
    
    ### Running the comment extraction service
    To use the comment extraction service, run the following from the
    `skema/program_analysis/comment_extractor/` directory:
    ```bash
    uvicorn server:app --reload
    ```
    ### Endpoints
    
    - /comments-get-supported-languages: Returns the languages that are
    supported for comment extraction as well as the types of comments that
    can be extracted
    - /comments-get-supported-file-extensions: Returns a list of file
    extensions that are supported for comment extraction
    - /comments-extract: Extract comments from a source file
    - /comments-extract-zip: Extract comments from zip archive of source
    files
    
    NOTE: The Rust comment extractor is not removed in the PR, as it is
    still being used for comment unification.

---
+++ skema/program_analysis/comment_extractor/tests/examples/comments.r
@@ -0,0 +1,15 @@
+# Single line comment
+
+foo <- function(x, y) {
+    # Docstring single line
+    result <- x + y
+}
+
+bar <- function(x,y,z){
+    # Docstring
+    # Multiple lines
+}
+
+# Multiline comment
+# Multiline comment
+# Multiline comment
---
+++ skema/program_analysis/comment_extractor/tests/test_comment_extractor.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+from fastapi.testclient import TestClient
+
+import skema.program_analysis.comment_extractor.comment_extractor as comment_service
+from skema.program_analysis.comment_extractor.server import app
+
+EXAMPLES_PATH = Path(__file__).parent / "examples"
+
+client = TestClient(app)
+
+def test_extract_c():
+    c_source_path = EXAMPLES_PATH / "comments.c"
+    request = {
+        "language": "c",
+        "source": c_source_path.read_text()
+    }
+    response = client.post("/comments-extract", json=request)
+    assert response.status_code == 200
+
+
+def test_extract_cpp():
+    cpp_source_path = EXAMPLES_PATH / "comments.cpp"
+    request = {
+        "language": "cpp",
+        "source": cpp_source_path.read_text()
+    }
+    response = client.post("/comments-extract", json=request)
+    assert response.status_code == 200
+
+def test_extract_fortran():
+    fortran_source_path = EXAMPLES_PATH / "comments.f95"
+    request = {
+        "language": "fortran",
+        "source": fortran_source_path.read_text()
+    }
+    response = client.post("/comments-extract", json=request)
+    assert response.status_code == 200
+
+def test_extract_matlab():
+    matlab_source_path = EXAMPLES_PATH / "comments.m"
+    request = {
+        "language": "matlab",
+        "source": matlab_source_path.read_text()
+    }
+    response = client.post("/comments-extract", json=request)
+    assert response.status_code == 200
+
+def test_extract_python():
+    python_source_path = EXAMPLES_PATH / "comments.py"
+    request = {
+        "language": "python",
+        "source": python_source_path.read_text()
+    }
+    response = client.post("/comments-extract", json=request)
+    assert response.status_code == 200
+
+def test_extract_r():
+    r_source_path = EXAMPLES_PATH / "comments.r"
+    request = {
+        "language": "r",
+        "source": r_source_path.read_text()
+    }
+    response = client.post("/comments-extract", json=request)
+    assert response.status_code == 200
