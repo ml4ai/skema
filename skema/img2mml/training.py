3ce879756ff15e2a50acb9a7eced1ee70b8a617e
gauravsh0812 <gauravs@arizona.edu>
2023-08-25 16:54:57 -0400

    updating training.py

--- skema/img2mml/training.py
+++ skema/img2mml/training.py
@@ -497,7 +497,7 @@ def train_model(rank=None,):
     """
     if config["bin_comparison"]:
         print("comparing bin...")
-        from .gitignore.bin_testing import bin_test_dataloader
+        from bin_testing import bin_test_dataloader
 
         test_dataloader = bin_test_dataloader(
             config, vocab, device, start=0, end=50
