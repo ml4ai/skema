19214b43994100e811169ef072d1a7718d2b6e6c
Vincent Raymond <vincent@lum.ai>
2023-09-08 10:56:12 -0400

    Implementing fixed2free into preprocessing pipeline

---
+++ skema/program_analysis/CAST/fortran/preprocessor/fixed2free.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# fixed2free2.py: Conversion of Fortran code from fixed to free
+#                 source form.
+#
+# Copyright (C) 2012    Elias Rabel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Script that converts fixed form Fortran code to free form
+Usage: file name as first command line parameter
+
+python fixed2free2.py file.f > file.f90
+"""
+
+# author: Elias Rabel, 2012
+# Let me know if you find this script useful:
+# ylikx.0 at gmail
+# https://www.github.com/ylikx/
+
+# TODO:
+# *) Improve command line usage
+
+from __future__ import print_function
+import sys
+
+class FortranLine:
+    def __init__(self, line):
+        self.line = line
+        self.line_conv = line
+        self.isComment = False
+        self.isContinuation = False
+        self.__analyse()
+        
+    def __repr__(self):
+        return self.line_conv
+        
+    def continueLine(self):
+        """Insert line continuation symbol at correct position in a free format line."""
+        if not self.isOMP:
+            before_inline_comment, inline_comment = extract_inline_comment(self.line_conv)
+        else:
+            tmp, inline_comment = extract_inline_comment(self.line_conv[1:].lstrip())
+            before_inline_comment = "!" + tmp
+        
+        if inline_comment == "":
+            self.line_conv = self.line_conv.rstrip() + " &\n"
+        else:
+            len_before = len(before_inline_comment)
+            before = before_inline_comment.rstrip() + " & "
+            self.line_conv = before.ljust(len_before) + inline_comment
+                  
+    def __analyse(self):
+        line = self.line
+        # MODIFICATION: Handle tabs that have been converted to spaces by GCC
+        if line[0] == " " and line[1] != " ":
+            line = "      " + line[1:]
+        
+        firstchar = line[0] if len(line) > 0 else ''
+        self.label = line[0:5].strip().lower() + ' ' if len(line) > 1 else ''
+        cont_char = line[5] if len(line) >= 6 else ''
+        fivechars = line[1:5] if len(line) > 1 else ''
+        self.isShort = (len(line) <= 6)
+        self.isLong  = (len(line) > 73)
+        
+        
+        self.isComment = firstchar in "cC*!"
+        self.isNewComment = '!' in fivechars and not self.isComment
+        self.isOMP = self.isComment and fivechars.lower() == "$omp"
+        if self.isOMP:
+            self.isComment = False
+            self.label = ''
+        self.isCppLine = (firstchar == '#')
+        self.is_regular = (not (self.isComment or self.isNewComment or 
+                           self.isCppLine or self.isShort))      
+        self.isContinuation = (not (cont_char.isspace() or cont_char == '0') and
+                               self.is_regular)
+
+        self.code = line[6:] if len(line) > 6 else '\n'
+
+        self.excess_line = '' 
+        if self.isLong and self.is_regular:
+            code, inline_comment = extract_inline_comment(self.code)
+            if inline_comment == "" or len(code) >= 72 - 6:
+                self.excess_line = line[72:]
+                line = line[:72] + '\n'
+                self.code = line[6:]
+        
+        self.line = line
+        self.__convert()
+
+    def __convert(self):
+        line = self.line
+
+        if self.isComment:
+            self.line_conv = '!' + line[1:]
+        elif self.isNewComment or self.isCppLine:
+            self.line_conv = line
+        elif self.isOMP:
+            self.line_conv = '!' + line[1:5] + ' ' + self.code
+        elif not self.label.isspace():
+            self.line_conv = self.label + self.code
+        else:
+            self.line_conv = self.code
+
+        if self.excess_line != '':
+            if self.excess_line.lstrip().startswith("!"):
+                marker = ""
+            else:
+                marker = "!"
+            
+            self.line_conv = self.line_conv.rstrip().ljust(72) + marker + self.excess_line
+
+def extract_inline_comment(code):
+    """Splits line of code into (code, inline comment)"""
+    stringmode = False
+    stringchar = ""
+    
+    for column, character in enumerate(code):
+        is_string_delimiter = (character == "'" or character == '"')
+        if not stringmode and is_string_delimiter:
+            stringmode = True
+            stringchar = character
+        elif stringmode and is_string_delimiter:
+            stringmode = (character != stringchar)
+        elif not stringmode and character == "!":
+            return code[:column], code[column:]
+            
+    return code, ""
+
+
+def convertToFree(stream):
+    """Convert stream from fixed source form to free source form."""
+    linestack = []
+        
+    for line in stream:
+        convline = FortranLine(line)
+        
+        if convline.is_regular:
+            if convline.isContinuation and linestack: 
+                linestack[0].continueLine()
+            for l in linestack:
+                yield str(l)
+            linestack = []
+            
+        linestack.append(convline)
+        
+    for l in linestack:
+        yield str(l)
+        
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        infile = open(sys.argv[1], 'r')
+        for line in convertToFree(infile):
+            print(line, end="")
+    
+        infile.close()
+    else:
+        print(__doc__)
--- skema/program_analysis/CAST/fortran/preprocessor/preprocess.py
+++ skema/program_analysis/CAST/fortran/preprocessor/preprocess.py
@@ -9,6 +9,12 @@ from subprocess import run, PIPE
 
 from tree_sitter import Parser, Node, Language, Tree
 
+from skema.program_analysis.tree_sitter_parsers.build_parsers import (
+    INSTALLED_LANGUAGES_FILEPATH,
+)
+from skema.program_analysis.CAST.fortran.preprocessor.fixed2free import convertToFree
+
+
 def preprocess(
     source_path: Path,
     out_dir=None,
@@ -16,7 +22,7 @@ def preprocess(
     out_missing_includes=False,
     out_gcc=False,
     out_unsupported=False,
-    out_corrected=False,
+    out_free=False,
 ) -> str:
     """Run the full preprocessing pipeline for Fortran->Tree-Sitter->CAST
     Takes the original source as input and will return the tree-sitter parse tree as output
@@ -24,7 +30,7 @@ def preprocess(
     1. A log of missing files that are included by preprocessor directives in the source code
     2. The intermediary product from running the c-preprocessor
     3. A log of unsupported idioms
-    4. The source code with unsupported idioms corrected
+    4. The source code converted to free-form
     """
     # NOTE: The order of preprocessing steps does matter. We have to run the GCC preprocessor before correcting the continuation lines or there could be issues
 
@@ -32,7 +38,7 @@ def preprocess(
 
     # Get paths for intermediate products
     if out_dir:
-        if not (out_missing_includes or out_gcc or out_unsupported or out_corrected):
+        if not (out_missing_includes or out_gcc or out_unsupported or out_free):
             logging.warning("out_dir is specified, but no out flags are set")
 
         out_dir.mkdir(parents=True, exist_ok=True)
@@ -40,7 +46,7 @@ def preprocess(
         missing_includes_path = Path(out_dir, "missing_includes.txt")
         gcc_path = Path(out_dir, "gcc.F")
         unsupported_path = Path(out_dir, "unsupported_idioms.txt")
-        corrected_path = Path(out_dir, "corrected.F")
+        free_path = Path(out_dir, "corrected.F")
         parse_path = Path(out_dir, "parse_tree.txt")
 
     # Step 1: Check for missing included files
@@ -78,10 +84,10 @@ def preprocess(
             "\n".join(search_for_unsupported_idioms(source, "idioms_regex.txt"))
         )
 
-    # Step 6 : Fix unsupported idioms
-    source = fix_unsupported_idioms(source)
-    if out_corrected:
-        corrected_path.write_text(source)
+    # Step 6 : Convert to free-form for tree-sitter parsing
+    source = convert_to_free_form(source)
+    if out_free:
+        free_path.write_text(source)
 
     return source
 
@@ -146,6 +152,7 @@ def search_for_unsupported_idioms(source: str, idioms_regex_path: str):
     return log
 
 
+''' TODO: Deprecated, replaced by fixed2free.py
 def fix_unsupported_idioms(source: str):
     """
     Preprocesses Fortran source code to convert continuation lines to tree-sitter supported format:
@@ -161,6 +168,7 @@ def fix_unsupported_idioms(source: str):
     source = "\n".join(processed_lines)
 
     return source
+'''
 
 
 def fix_include_directives(source: str) -> str:
@@ -190,6 +198,25 @@ def run_c_preprocessor(source: str, include_base_path: Path) -> str:
     return result.stdout
 
 
+def convert_to_free_form(source: str) -> str:
+    """If fixed-form Fortran source, convert to free-form"""
+
+    # We don't know for sure if a source is meant to be fixed-form or free-form
+    # So, we will run the parser
+    language = Language(INSTALLED_LANGUAGES_FILEPATH, "fortran")
+    parser = Parser()
+    parser.set_language(language)
+    
+    tree = parser.parse(bytes(source, encoding="utf-8"))
+    if "ERROR" not in tree.root_node.sexp():
+        return source
+    else:
+        # convertToFree takes a stream as input and returns a generator
+        source = "".join([line for line in convertToFree(source.splitlines(keepends=True))])
+
+    return source
+
+
 def main():
     """Run the preprocessor as a script"""
     parser = argparse.ArgumentParser(description="Fortran preprocessing script")
@@ -217,7 +244,7 @@ def main():
         help="Output unsupported idioms log",
     )
     parser.add_argument(
-        "--out_corrected",
+        "--out_free",
         action="store_true",
         help="Output source after fixing unsupported idioms",
     )
@@ -230,7 +257,7 @@ def main():
         args.out_missing_includes,
         args.out_gcc,
         args.out_unsupported,
-        args.out_corrected,
+        args.out_free,
     )
 
 
