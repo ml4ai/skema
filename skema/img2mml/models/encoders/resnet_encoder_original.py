3a76f7150355b685cd076f478f5891ccea0827cf
gauravsh0812 <gauravs@arizona.edu>
2023-09-20 17:42:46 -0700

    trying new resnet

--- skema/img2mml/models/encoders/resnet_encoder_original.py
+++ skema/img2mml/models/encoders/resnet_encoder_original.py
@@ -11,24 +11,22 @@ from skema.img2mml.models.encoding.positional_features_for_cnn_encoder import (
 
 
 class ResNetBlock(nn.Module):  # res_block
-    def __init__(self, in_channels, out_channels, stride, downsampling=False, matching=False):
+    def __init__(self, in_channels, out_channels, stride, downsampling=False):
         super(ResNetBlock, self).__init__()
 
         self.stride = stride
         self.downsampling = downsampling
-        self.matching = matching
-
-        # if stride != 1:
-        self.downsample = nn.Sequential(
-            nn.Conv2d(
-                in_channels,
-                out_channels,
-                kernel_size=1,
-                stride=stride,
-                bias=False,
-            ),
-            nn.BatchNorm2d(out_channels),
-        )
+        if stride != 1:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(
+                    in_channels,
+                    out_channels,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(out_channels),
+            )
 
         self.conv1 = nn.Conv2d(
             in_channels,
@@ -48,7 +46,6 @@ class ResNetBlock(nn.Module):  # res_block
         )
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
-        self.match_linear = nn.Linear(125,63)
 
     def forward(self, x):
         if self.downsampling:
@@ -59,6 +56,7 @@ class ResNetBlock(nn.Module):  # res_block
         x = self.relu(self.bn(self.conv1(x)))
         x = self.bn(self.conv2(x))
 
+        # residual connection
         x += res
         x = self.relu(x)
 
@@ -84,40 +82,41 @@ class ResNet18_Encoder(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
 
-        self.init_layer = self.initial_layer
-        self.l1 = res_block(64, 64, stride=1)
-        self.l2 = res_block(64, 64, stride=1)
-        self.l3 = res_block(64, 128, stride=1, downsampling=True)
-        self.l4 = res_block(128, 128, stride=1)
-        self.l5 = res_block(128, 256, stride=1, downsampling=True)
-        self.l6 = res_block(256, 256, stride=1)
-        self.l7 = res_block(256, 512, stride=1, downsampling=True)
-        self.l8 = res_block(512, 512, stride=1, matching=True)
-        self.linear1 = nn.Linear(125,63)
-        self.linear2 = nn.Linear(512, dec_hid_dim)
-        # self.init_weights()
+        self.resnet_encoder = nn.Sequential(
+            self.initial_layer,
+            res_block(64, 64, stride=1),
+            res_block(64, 64, stride=1),
+            res_block(64, 128, stride=2, downsampling=True),
+            res_block(128, 128, stride=1),
+            res_block(128, 256, stride=2, downsampling=True),
+            res_block(256, 256, stride=1),
+            # res_block(256, 512, stride=2, downsampling=True),
+            # res_block(512, 512, stride=1),
+        )
+
+        self.linear = nn.Linear(256, dec_hid_dim)
+        self.init_weights()
+
+    def init_weights(self):
+        """
+        initializing the model wghts with values
+        drawn from normal distribution.
+        else initialize them with 0.
+        """
+        for name, param in self.resnet_encoder.named_parameters():
+            if "nn.Conv2d" in name or "nn.Linear" in name:
+                if "weight" in name:
+                    nn.init.normal_(param.data, mean=0, std=0.1)
+                elif "bias" in name:
+                    nn.init.constant_(param.data, 0)
+            elif "nn.BatchNorm2d" in name:
+                if "weight" in name:
+                    nn.init.constant_(param.data, 1)
+                elif "bias" in name:
+                    nn.init.constant_(param.data, 0)
 
     def forward(self, src, encoding_type=None):
-        output = self.init_layer(src)
-        # print("init: ", output.shape)
-        output = self.l1(output)
-        # print("l1: ", output.shape)
-        output = self.l2(output)
-        # print("l2: ", output.shape)
-        output = self.l3(output)
-        # print("l3: ", output.shape)
-        output = self.l4(output)
-        # print("l4: ", output.shape)
-        output = self.l5(output)
-        # print("l5: ", output.shape)
-        output = self.l6(output)
-        # print("l6: ", output.shape)
-        output = self.l7(output)
-        # print("l7: ", output.shape)
-        output = self.l8(output)
-        # print("l8: ", output.shape)
-        output = self.linear1(output)
-
+        output = self.resnet_encoder(src)
         if encoding_type == "row_encoding":
             # output: (B, H*W, dec_hid_dim)
             # hidden, cell: [1, B, dec_hid_dim]
@@ -129,4 +128,5 @@ class ResNet18_Encoder(nn.Module):
             output = output.permute(0, 2, 1)  # (B, L, 512)
             if encoding_type == "positional_features":
                 output += add_positional_features(output)  # (B, L, 512)
-            return self.linear2(output)  # (B, L, dec_hid_dim)
+            return self.linear(output)  # (B, L, dec_hid_dim)
+        
