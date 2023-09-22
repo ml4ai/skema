7c84f0df05a9b2b885a0585deeca2135ab6d9649
Joseph Astier <jastier@arizona.edu>
2023-09-22 13:33:03 -0700

    Removed handler for non-existent grammar file

--- skema/program_analysis/CAST/matlab/tests/test_matlab_parse_tree.py
+++ skema/program_analysis/CAST/matlab/tests/test_matlab_parse_tree.py
@@ -7,17 +7,6 @@ from tree_sitter import Language, Parser
 # test parser
 from skema.program_analysis.CAST.matlab.matlab_to_cast import MatlabToCast
 
-# test for existence of shared grammar object
-from skema.program_analysis.tree_sitter_parsers.build_parsers import INSTALLED_LANGUAGES_FILEPATH
-if INSTALLED_LANGUAGES_FILEPATH.exists():
-    print(f"Shared object at: {str(INSTALLED_LANGUAGES_FILEPATH)}")
-else:  
-    # If not found, create it
-    print(f"Did not find shared object at {str(INSTALLED_LANGUAGES_FILEPATH)}")
-    from skema.program_analysis.tree_sitter_parsers.build_parsers import build_parsers
-    build_parsers(["matlab"])
-    
-
 TEST_DATA_PATH = Path(__file__).parent / "data"
 
 
