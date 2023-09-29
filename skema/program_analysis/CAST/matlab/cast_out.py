cdb90670097708ff0e3c1b3ebc8d62e29f6bca0a
Joseph Astier <jastier@arizona.edu>
2023-09-29 15:15:26 -0700

    Modified cast_out.py to parse one file per run

--- skema/program_analysis/CAST/matlab/cast_out.py
+++ skema/program_analysis/CAST/matlab/cast_out.py
@@ -1,34 +1,24 @@
 from skema.program_analysis.CAST.matlab.matlab_to_cast import MatlabToCast
-from pathlib import Path
 import json
 import sys
 
-""" A fast way to run any file type through the Tree-sitter MATLAB parser"""
+def cast_out(filename):
+    """ Parse the file with the MATLAB parser"""
+    parser = MatlabToCast(filename)
+    cast_list = parser.out_cast
+    for cast_index in range(0, len(cast_list)):
+        jd = json.dumps(
+            cast_list[cast_index].to_json_object(),
+            sort_keys=True,
+            indent=2,
+        )
+        print(jd)
 
-n_files = len(sys.argv)
 
-if n_files == 1:
-    print("Please enter one or more input filenames")
-else:
-    done = list()
-    for i in range (1, n_files):
-        filepath = Path(__file__).parent / sys.argv[i]
-        print(f"\nParsing MATLAB tree for {str(filepath)}:")
-        parser = MatlabToCast(filepath)
-        cast = parser.out_cast
-        print(f"\nNumber of CAST objects produced = {str(len(cast))}")
+""" Run a file of any type through the Tree-sitter MATLAB parser"""
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        cast_out(sys.argv[1])
+    else:
+        print("Please enter one filename to parse")
 
-        print('\nCAST object:')
-        for c in cast:
-            jd = json.dumps(
-                c.to_json_object(),
-                sort_keys=True,
-                indent=2,
-            )
-            print(jd)
-        done.append(filepath)
-
-    print("\nFiles processed:")
-    for d in done:
-        print(f"  {d}")
-    print()
