defa8e442806f016171457e238fe8e2906e941b2
Joseph Astier <jastier@arizona.edu>
2023-10-04 16:01:46 -0700

    Code cleanup in cast and tree utils

--- skema/program_analysis/CAST/matlab/cast_out.py
+++ skema/program_analysis/CAST/matlab/cast_out.py
@@ -1,43 +1,25 @@
-from skema.program_analysis.CAST.matlab.matlab_to_cast import MatlabToCast
-from skema.program_analysis.CAST.matlab.tree_builder import TreeBuilder
-from tree_sitter import Tree
-
 import json
 import sys
-
-def show_source(parser):
-    """ Show the input source file """
-    print('\nSOURCE:')
-    print(parser.source)
-
-def show_cast(parser):
-    """ Show the CAST structures """
-    print('\nCAST:')
-    cast_list = parser.out_cast
-    for cast_index in range(0, len(cast_list)):
-        jd = json.dumps(
-            cast_list[cast_index].to_json_object(),
-            sort_keys=True,
-            indent=2,
-        )
-        print(jd)
-
-def show_tree(parser):
-    """ Show the Tree-sitter syntax tree """
-    print('\nSYNTAX TREE:')
-    tree_builder = parser.tree_builder
-    tree_builder.print_tree(parser.tree)
-
+from skema.program_analysis.CAST.matlab.matlab_to_cast import MatlabToCast
 
 """ Run a file of any type through the Tree-sitter MATLAB parser"""
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             parser = MatlabToCast(sys.argv[i])
-            show_source(parser)
-            show_cast(parser)
-            show_tree(parser)
-
+            print("\n\nINPUT:")
+            print(parser.filename)
+            print("\nSOURCE:")
+            print(parser.source)
+            print('\nCAST:')
+            cast_list = parser.out_cast
+            for cast_index in range(0, len(cast_list)):
+                jd = json.dumps(
+                    cast_list[cast_index].to_json_object(),
+                    sort_keys=True,
+                    indent=2,
+                )
+                print(jd)
     else:
         print("Please enter one filename to parse")
-
--- skema/program_analysis/CAST/matlab/tree_out.py
+++ skema/program_analysis/CAST/matlab/tree_out.py
@@ -1,27 +1,21 @@
 import sys
 from tree_sitter import Language, Parser
-from tree_sitter import Tree
 from pathlib import Path
 from skema.program_analysis.CAST.matlab.tree_utils import TreeUtils
 from skema.program_analysis.tree_sitter_parsers.build_parsers import (
     INSTALLED_LANGUAGES_FILEPATH
 )
 
-
-
 """ Get the Tree-sitter syntax tree for MATLAB files"""
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-
         # Create the Tree-sitter parser for the language name
         parser = Parser()
         parser.set_language(
             Language(INSTALLED_LANGUAGES_FILEPATH, "matlab")
         )
-
         tree_utils = TreeUtils()
-
         for i in range(1, len(sys.argv)):
             path = Path(sys.argv[i])
             print("\n\nINPUT:")
@@ -29,13 +23,8 @@ if __name__ == "__main__":
             source = path.read_text().strip()
             print("\nSOURCE:")
             print(source)
-
             print("\nSYNTAX TREE:")
             tree = parser.parse(bytes(source, "utf8"))
-            # tree = tree_utils.clean_tree(tree)
-
             tree_utils.print_tree(tree)
-
     else:
         print("Please enter at least one input file")
-
