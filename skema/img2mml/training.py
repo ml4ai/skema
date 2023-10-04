6e5e50954db4cbfc13e4fb8df0997773e41863f2
gauravsh0812 <gauravs@arizona.edu>
2023-10-04 02:07:21 -0700

    updating

--- skema/img2mml/training.py
+++ skema/img2mml/training.py
@@ -510,13 +510,13 @@ def train_model(rank=None,):
         f"trained_models/{model_type}_{dataset_type}_{config['markup']}_best.pt",
     )
     try:
-        loading pre_tained_model
+        #loading pre_tained_model
         model.load_state_dict(
             torch.load(
                 f"trained_models/{model_type}_{dataset_type}_{config['markup']}_best.pt"
             )
         )
-        
+
     except:
         try:
             # removing "module." from keys
