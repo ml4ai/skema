59cde6f0047026b23a21657c23b1e9ea2577ae5e
Joseph Astier <jastier@arizona.edu>
2023-10-04 16:34:22 -0700

    Removed unused tree_utils.py

--- skema/program_analysis/CAST/matlab/tree_out.py
+++ skema/program_analysis/CAST/matlab/tree_out.py
@@ -1,12 +1,17 @@
 import sys
-from tree_sitter import Language, Parser
+from tree_sitter import Language, Node, Parser
 from pathlib import Path
-from skema.program_analysis.CAST.matlab.tree_utils import TreeUtils
 from skema.program_analysis.tree_sitter_parsers.build_parsers import (
     INSTALLED_LANGUAGES_FILEPATH
 )
 
 """ Get the Tree-sitter syntax tree for MATLAB files"""
+
+def print_tree(node: Node, indent = ''):
+    """Display the node branch in pretty format"""
+    for child in node.children:
+        print(f"{indent} node: {child.type}")
+        print_tree(child, indent + '  ')
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -15,7 +20,6 @@ if __name__ == "__main__":
         parser.set_language(
             Language(INSTALLED_LANGUAGES_FILEPATH, "matlab")
         )
-        tree_utils = TreeUtils()
         for i in range(1, len(sys.argv)):
             path = Path(sys.argv[i])
             print("\n\nINPUT:")
@@ -25,6 +29,6 @@ if __name__ == "__main__":
             print(source)
             print("\nSYNTAX TREE:")
             tree = parser.parse(bytes(source, "utf8"))
-            tree_utils.print_tree(tree)
+            print_tree(tree.root_node)
     else:
         print("Please enter at least one input file")
