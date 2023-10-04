eb30f86a2bb2d8950cea0f2f3156ca2b27b24622
gauravsh0812 <gauravs@arizona.edu>
2023-10-04 02:06:31 -0700

    upudate

--- skema/img2mml/src/train.py
+++ skema/img2mml/src/train.py
@@ -74,9 +74,9 @@ def train(
         if isBatchScheduler:
             # Calculating val_loss after every 1000 batches
             # of size 64.
-            if i > 0 and i % 200 == 0 and i < len(train_dataloader):
+            if i > 0 and i % 400 == 0 and i < len(train_dataloader):
                 # randomly choosing 50 samples for the validation
-                val_dataloader = random.sample(list(val_dataloader), 50)
+                val_dataloader = random.sample(list(val_dataloader), 100)
                 val_loss = evaluate(model,model_type,img_tnsr_path,
                                     batch_size,val_dataloader,criterion,
                                     device,vocab,ddp=ddp,rank=rank,)
