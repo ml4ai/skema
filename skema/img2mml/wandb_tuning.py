2d33fa2fe75a87dd69dc5f99c410f77714d7f38f
gauravsh0812 <gauravs@arizona.edu>
2023-10-05 20:52:40 -0700

    updating

--- skema/img2mml/wandb_tuning.py
+++ skema/img2mml/wandb_tuning.py
@@ -414,10 +414,10 @@ def ddp_main(config=None):
     world_size = main_config["world_size"]
     os.environ["CUDA_VISIBLE_DEVICES"] = main_config["DDP gpus"]
     ddp = main_config["DDP"]
-    if (not ddp) or (ddp and rank == 0):
-        with wandb.init(config=config, group="DDP"):
-            sweep_config = wandb.config
-            mp.spawn(train_model, args=(sweep_config,), nprocs=world_size, join=True)
+
+    with wandb.init(config=config, group="DDP"):
+        sweep_config = wandb.config
+        mp.spawn(train_model, args=(sweep_config,), nprocs=world_size, join=True)
 
 if __name__ == "__main__":
 
