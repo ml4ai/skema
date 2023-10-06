add1fb8055f985702421bfcb956761596321327f
gauravsh0812 <gauravs@arizona.edu>
2023-10-05 23:42:14 -0700

    updating

--- skema/img2mml/preprocessing/preprocess.py
+++ skema/img2mml/preprocessing/preprocess.py
@@ -73,7 +73,7 @@ def preprocess_dataset(config):
 
     # reading raw text files
     mml_path = f"{config['data_path']}/{config['dataset_type']}/{config['markup']}.lst"
-    mml_txt = open(mml_path).read().split("\n")[:10000]
+    mml_txt = open(mml_path).read().split("\n")[:5000]
     image_num = range(0, len(mml_txt))
 
     # split the image_num into train, test, validate
