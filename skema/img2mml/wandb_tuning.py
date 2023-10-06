c02766132b711cd54174c29087b005bd744c88ea
gauravsh0812 <gauravs@arizona.edu>
2023-10-05 20:09:00 -0700

    updating

--- skema/img2mml/wandb_tuning.py
+++ skema/img2mml/wandb_tuning.py
@@ -199,7 +199,7 @@ def epoch_time(start_time, end_time):
     return elapsed_mins, elapsed_secs
 
 
-def train_model(rank=None, config=config,):
+def train_model(rank=None, config,):
 
     # to save trained model and logs
     FOLDER = ["trained_models", "logs"]
