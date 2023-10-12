c747c05bf941a5cb039fd4ee0b5bac40af168b98
gauravsh0812 <gauravs@arizona.edu>
2023-10-12 02:32:34 -0700

    updating

--- skema/img2mml/optuna_training.py
+++ skema/img2mml/optuna_training.py
@@ -469,7 +469,7 @@ def tune(rank=None,):
 if __name__ == "__main__":
     if config["DDP"]:
         os.environ["MASTER_ADDR"] = "localhost"
-        os.environ["MASTER_PORT"] = "29880"
+        os.environ["MASTER_PORT"] = "29860"
         world_size = config["world_size"]
         os.environ["NCCL_DEBUG"] = "INFO"
         os.environ["CUDA_VISIBLE_DEVICES"] = config["DDP gpus"]
