e26d34d3816d7478fec6edd84d26d8e4ef03c684
Vincent Raymond <vincent@lum.ai>
2023-10-19 12:50:27 -0400

    Additional model header data plus links to data products in report

--- skema/program_analysis/magit/html_builder.py
+++ skema/program_analysis/magit/html_builder.py
@@ -15,26 +15,46 @@ class HTML_Instance():
         new_model_container = self.soup.new_tag("div", id=model_name, class_="model-container") 
         new_model_heading = self.soup.new_tag("h2")
         new_model_heading.string = model_name
-        new_model_table_container = self.soup.new_tag("div", id=f"{model_name}-basic" , class_="table-container")
-        new_model_table = self.soup.new_tag("table")
-        new_model_table_header = self.soup.new_tag("tr")
-        new_model_table_header_th1 = self.soup.new_tag('th')
-        new_model_table_header_th1.string = "File Name"
-        new_model_table_header_th2 = self.soup.new_tag('th')
-        new_model_table_header_th2.string = 'Num Lines'
-        new_model_table_header_th3 = self.soup.new_tag('th')
-        new_model_table_header_th3.string = 'Can Ingest'
+        # Create basic model table
+        new_model_table_container_basic = self.soup.new_tag("div", id=f"{model_name}-basic" , class_="table-container")
+        new_model_table_basic = self.soup.new_tag("table")
+        new_model_table_header_basic = self.soup.new_tag("tr")
+        new_model_table_header_basic_th1 = self.soup.new_tag('th')
+        new_model_table_header_basic_th1.string = "File Name"
+        new_model_table_header_basic_th2 = self.soup.new_tag('th')
+        new_model_table_header_basic_th2.string = 'Num Lines'
+        new_model_table_header_basic_th3 = self.soup.new_tag('th')
+        new_model_table_header_basic_th3.string = 'Can Ingest'
+        new_model_table_header_basic_th4 = self.soup.new_tag('th')
+        new_model_table_header_basic_th4.string = 'Tree-Sitter Parse Tree'
+        new_model_table_header_basic_th5 = self.soup.new_tag('th')
+        new_model_table_header_basic_th5.string = 'CAST'
+        new_model_table_header_basic_th6 = self.soup.new_tag('th')
+        new_model_table_header_basic_th6.string = 'Gromet'
 
         # Append the elements to each other
-        new_model_container.extend([new_model_heading, new_model_table_container])
-        new_model_table_container.append(new_model_table)
-        new_model_table.append(new_model_table_header)
-        new_model_table_header.extend([new_model_table_header_th1, new_model_table_header_th2, new_model_table_header_th3])
-        
+        new_model_container.extend([new_model_heading, new_model_table_container_basic])
+        new_model_table_container_basic.append(new_model_table_basic)
+        new_model_table_basic.append(new_model_table_header_basic)
+        new_model_table_header_basic.extend([new_model_table_header_basic_th1, new_model_table_header_basic_th2, new_model_table_header_basic_th3, new_model_table_header_basic_th4, new_model_table_header_basic_th5, new_model_table_header_basic_th6])
+    
         # Append to outer body
         self.soup.body.append(new_model_container)
 
-    def add_file_basic(self, model: str, file_name: str, num_lines: int, can_ingest: bool):
+    def add_model_header_data(self, model: str, supported_lines: int, total_lines: int, full_ingest: bool):
+        model_table = self.soup.select_one(f"#{model}-basic table") 
+
+        model_header = self.soup.new_tag("div", class_="model_header") 
+        model_header_supported_lines = self.soup.new_tag("div", class_="model_header") 
+        model_header_supported_lines.string = f"Total Supported Lines: {supported_lines}/{total_lines}"
+        model_header_full_ingest = self.soup.new_tag("div") 
+        model_header_full_ingest.string = f"Can ingest system into single GrometFNModuleCollection: {str(full_ingest)}"
+        
+
+        model_header.extend([model_header_supported_lines, model_header_full_ingest])
+        model_table.append(model_header)
+        
+    def add_file_basic(self, model: str, file_name: str, num_lines: int, can_ingest: bool, parse_tree_path: Path, cast_path: Path, gromet_path: Path):
         model_table = self.soup.select_one(f"#{model}-basic table") 
         
         new_row = self.soup.new_tag("tr")
@@ -48,17 +68,27 @@ class HTML_Instance():
         valid_cell = self.soup.new_tag("td")
         valid_cell.string = "✓" if can_ingest else "✗"
 
-        new_row.extend([file_name_cell, num_lines_cell, valid_cell])
+        parse_tree_cell = self.soup.new_tag("td")
+        parse_tree_a = self.soup.new_tag("a")
+        parse_tree_a["href"] = str(parse_tree_path)
+        parse_tree_a.string = "Open Parse Tree"
+        parse_tree_cell.append(parse_tree_a)
+
+        cast_cell = self.soup.new_tag("td")
+        cast_a = self.soup.new_tag("a")
+        cast_a["href"] = str(cast_path)
+        cast_a.string = "Open CAST"
+        cast_cell.append(cast_a)
+
+        gromet_cell = self.soup.new_tag("td")
+        gromet_a = self.soup.new_tag("a")
+        gromet_a["href"] = str(gromet_path)
+        gromet_a.string = "Open Gromet"
+        gromet_cell.append(gromet_a)
+
+        new_row.extend([file_name_cell, num_lines_cell, valid_cell, parse_tree_cell, cast_cell, gromet_cell])
 
         model_table.append(new_row)
-
-    def add_file_detailed():
-        pass
-
-    def add_file_experimental():
-        pass
-
-    
 
     def write_html(self):
         output_path = Path(__file__).resolve().parent / "output.html"
--- skema/program_analysis/magit/magit.py
+++ skema/program_analysis/magit/magit.py
@@ -8,25 +8,56 @@ from io import BytesIO
 from tempfile import TemporaryDirectory
 from pathlib import Path
 
+from tree_sitter import Language, Parser, Tree
+
 from skema.program_analysis.magit.html_builder import HTML_Instance
 from skema.program_analysis.multi_file_ingester import process_file_system
 from skema.program_analysis.single_file_ingester import process_file
 from skema.program_analysis.snippet_ingester import process_snippet
+from skema.program_analysis.tree_sitter_parsers.build_parsers import INSTALLED_LANGUAGES_FILEPATH, LANGUAGES_YAML_FILEPATH
+from skema.program_analysis.python2cast import python_to_cast
 
 THIS_PATH = Path(__file__).resolve().parent
 MODEL_YAML_PATH = Path(__file__).parent / "models.yaml"
 MODEL_YAML = yaml.safe_load(MODEL_YAML_PATH.read_text())
 
-def process_single_model(html: HTML_Instance, model_name: str):
+def generate_data_products(output_dir: str, source: str, file_name: str):
+    """Generate the data produce directory for the html output"""
+    file_path = Path(file_name)
+    language_name = ""
+    
+    parse_tree_path = Path(output_dir) / "data" / "tree-sitter" / f"{file_path.stem}.txt"
+    parse_tree_path.parent.mkdir(parents=True, exist_ok=True)
+    cast_path = Path(output_dir) / "data" / "cast" / f"{file_path.stem}.json"
+    cast_path.parent.mkdir(parents=True, exist_ok=True)
+    gromet_path = Path(output_dir) / "data" / "gromet" / f"{file_path.stem}.json"
+    gromet_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    # Determine the tree-sitter parser we need to use based on file extension
+    file_extension = Path(file_name).suffix
+    yaml_obj = yaml.safe_load(LANGUAGES_YAML_FILEPATH.read_text())
+    for language, language_dict in yaml_obj.items():
+        if file_extension in language_dict["extensions"]:
+            language_name = language
+            parser = Parser()
+            parser.set_language(Language(INSTALLED_LANGUAGES_FILEPATH, language))
+            tree = parser.parse(bytes(source, encoding="utf-8"))
+            parse_tree_path.write_text(tree.root_node.sexp())
+
+    # Relative to report/
+    return (str(parse_tree_path.relative_to("report/")), str(cast_path.relative_to("report/")), str(gromet_path.relative_to("report/")))
+def process_single_model(html: HTML_Instance, output_dir: str, model_name: str):
     html.add_model(model_name)
 
     if model_name in MODEL_YAML:
         model_url = MODEL_YAML[model_name]["zip_archive"]
         response = requests.get(model_url)
+        
     zip = ZipFile(BytesIO(response.content))
     with TemporaryDirectory() as temp:
-        system_filepaths = Path(temp) / "system_filepaths.txt"
-        system_filepaths.write_text("\n".join([file.filename for file in zip.filelist]))
+        # Ingest each file individually
+        supported_lines = 0
+        total_lines = 0
         for file in zip.filelist:
             source = str(zip.open(file).read(), encoding="utf-8")
             temp_path = Path(temp) / file.filename
@@ -35,55 +66,40 @@ def process_single_model(html: HTML_Instance, model_name: str):
             try:
                 process_file(temp_path)
                 can_ingest = True
+                supported_lines += len(source.splitlines())
             except (SystemExit, Exception) as e:
                 can_ingest = False
+            total_lines += len(source.splitlines())
 
             # The file ingestors modify the cwd during processing. If there is a failure, then the cwd may not be changed back.
             # This can result in the cwd begin a temp file that is deleted.
             # To account for this, we will set the cwd to the current directory
             # os.chdir(Path("."))
             os.chdir(THIS_PATH)
-            html.add_file_basic(model_name, file.filename, len(source), can_ingest)
-    
-    
-
-def process_all_models(html: HTML_Instance):
-    for model_name in MODEL_YAML:
-        print(f"PROCESSING {model_name}")
-        process_single_model(html, model_name)
-        print(f"Done {model_name}")
-    print("FINSIHED Processing modesl")
-
-"""
-html = HTML_Instance()
-
-for model, model_dict in MODEL_YAML.items():
-    html.add_model(model)
-
-    model_url = model_dict["zip_archive"]
-    response = requests.get(model_url)
-    zip = ZipFile(BytesIO(response.content))
-    with TemporaryDirectory() as temp:
+            
+            # Generate the data products for this report
+            output_paths = generate_data_products(output_dir, source, file.filename)
+            
+            html.add_file_basic(model_name, file.filename, len(source.splitlines()), can_ingest, output_paths[0], output_paths[1], output_paths[2])
+        
+        # Generate top level model header data
         system_filepaths = Path(temp) / "system_filepaths.txt"
         system_filepaths.write_text("\n".join([file.filename for file in zip.filelist]))
-        for file in zip.filelist:
-            source = str(zip.open(file).read(), encoding="utf-8")
-            temp_path = Path(temp) / file.filename
-            temp_path.parent.mkdir(parents=True, exist_ok=True)
-            temp_path.write_text(source)
-            
-            try:
-                process_file(temp_path)
-                can_ingest = True
-            except:
-                can_ingest = False
-            
-            html.add_file_basic(model, file.filename, len(source), can_ingest)
+        try:
+            process_file_system(model_name, Path(temp), system_filepaths)
+            can_ingest_full = True
+        except (SystemExit, Exception) as e:
+            can_ingest_full = False
 
-html.write_html()
-"""
+        html.add_model_header_data(model_name, supported_lines, total_lines, can_ingest_full )
+    
+def process_all_models(html: HTML_Instance, output_dir: str):
+    for model_name in MODEL_YAML:
+        process_single_model(html, output_dir, model_name)
+
 if __name__  == "__main__":
     parser = argparse.ArgumentParser(description="Process models.")
+    parser.add_argument("output_dir", help="Path to the output directory")
     subparsers = parser.add_subparsers(dest="mode")
     
     # Subparser for the "all" mode
@@ -94,10 +110,13 @@ if __name__  == "__main__":
     single_parser.add_argument("model_name", help="Name of the model to be processed")
 
     args = parser.parse_args()
-    
+        
     html = HTML_Instance()
     if args.mode == "all":
-        process_all_models(html)
+        process_all_models(html, args.output_dir)
     elif args.mode == "single":
-        process_single_model(html, args.model_name)
-    html.write_html()
+        process_single_model(html, args.output_dir, args.model_name)
+    
+    output_path = Path(args.output_dir) / "report.html"
+    output_path.write_text(html.soup.prettify())
+    
