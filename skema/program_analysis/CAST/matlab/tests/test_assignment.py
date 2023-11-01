77c96cb3216d4e65e88f1852eb54b555733633de
Joseph Astier <jastier@users.noreply.github.com>
2023-11-01 14:01:59 -0700

    CAST test upgrades (#619)
    ## CAST test upgrades
    This PR includes some test utilities for use when testing CAST output,
    so common tests such as an assignment or an expression can be done as
    single function calls. Some debug scripts are now executables. Minor
    edits and code cleanup
    
    ## What's New
    - Added `skema/program_analysis/CAST/matlab/tests/utils.py` with
    functions for common testing tasks
    - Added 3 conditional logic tests:
              `if`
              `if else`
              `if elseif else`
    
    ## Relevant issues
    Related to PR #613 and #583
    
    ---------
    
    Co-authored-by: Joseph Astier <jastier@arizona.edu>

--- skema/program_analysis/CAST/matlab/tests/test_assignment.py
+++ skema/program_analysis/CAST/matlab/tests/test_assignment.py
@@ -1,35 +1,17 @@
-from skema.program_analysis.CAST.matlab.matlab_to_cast import MatlabToCast
-from skema.program_analysis.CAST2FN.model.cast import (
-    Assignment,
-    LiteralValue,
-    Name,
-    Var
+from skema.program_analysis.CAST.matlab.tests.utils import (
+    assert_assignment,
+    first_cast_node
 )
 
 # Test the CAST returned by processing the simplest MATLAB assignment
 
 def test_assignment():
-    """ Tests parser assignment CAST """
+    """ Test CAST from MATLAB 'assignment' statement."""
 
-    source = 'x = 5;'
+    source = 'x = 5'
     
-    cast = MatlabToCast(source = source).out_cast
+    # The root of the CAST should be Assignment
+    assignment = first_cast_node(source)
 
-    # there should only be one CAST object in the cast output list
-    assert len(cast) == 1  
-
-    # The root of the CAST should be assignment
-    node = cast[0].nodes[0].body[0]
-    assert isinstance(node, Assignment)
-
-    # Left branch of an assignment node is the variable
-    left = node.left
-    assert isinstance(left, Var)
-    assert isinstance(left.val, Name)
-    assert left.val.name == "x"
-
-    # Right branch of this assignment is an Integer literal
-    right = node.right
-    assert isinstance(right, LiteralValue)
-    assert right.value_type == "Integer"
-    assert right.value == "5"
+    # The module body should contain a single assignment node
+    assert_assignment(assignment, left = "x", right = "5")
