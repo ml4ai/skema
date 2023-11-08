3075e148248222de1ce3ebca63f6dec86b0fd163
Joseph Astier <jastier@arizona.edu>
2023-11-07 23:17:00 -0700

    Added CI test case for switch case clauses with multiple values

--- skema/program_analysis/CAST/matlab/tests/test_switch.py
+++ skema/program_analysis/CAST/matlab/tests/test_switch.py
@@ -14,25 +14,57 @@ def test_switch_single_values():
             n = 1;
         case 'two'
             n = 2;
-            x = y
+            x = y;
         otherwise
             n = 0;
     end
     """
 
-    # The root of the CAST should a ModelIf instance
+    # case clause 'one'
     mi0 = cast_nodes(source)[0]
-
-    # case 'one'
     assert isinstance(mi0, ModelIf)
     assert_assignment(mi0.body[0], left="n", right = "1")
     assert_expression(mi0.expr, op="==", left = "s", right = "'one'")
 
-    # case 'two'
+    # case clause 'two'
     mi1 = mi0.orelse[0]
     assert isinstance(mi1, ModelIf)
     assert_assignment(mi1.body[0], left="n", right = "2")
     assert_assignment(mi1.body[1], left="x", right = "y")
 
-    # otherwise
+    # otherwise clause
+    assert_assignment(mi1.orelse[0], left="n", right = "0")
+
+def test_switch_multiple_values():
+    """ Test CAST from MATLAB switch statement."""
+
+    source = """
+    switch s
+        case {'one', 'two', 'three'}
+            n = 1;
+        case 2
+            n = 2;
+        otherwise
+            n = 0;
+    end
+    """
+
+    # case clause {'one', 'two', 'three'}
+    mi0 = cast_nodes(source)[0]
+    assert isinstance(mi0, ModelIf)
+    assert_assignment(mi0.body[0], left="n", right = "1")
+    assert_expression(
+        mi0.expr,
+        op="in",
+        left = 's',
+        right = ["'one'", "'two'", "'three'"]
+    )
+
+    # case clause 2
+    mi1 = mi0.orelse[0]
+    assert isinstance(mi1, ModelIf)
+    assert_assignment(mi1.body[0], left="n", right = "2")
+    assert_expression(mi1.expr, op="==", left = 's', right = 2)
+
+    # otherwise clause
     assert_assignment(mi1.orelse[0], left="n", right = "0")
