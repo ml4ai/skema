a847b88ebbe2360e7d31ded81185ca346f3b0ad4
Joseph Astier <jastier@arizona.edu>
2023-11-10 18:40:42 -0700

    Added a test file for functions

--- skema/program_analysis/CAST/matlab/tests/test_operators.py
+++ skema/program_analysis/CAST/matlab/tests/test_operators.py
@@ -1,4 +1,5 @@
 from skema.program_analysis.CAST.matlab.tests.utils import (
+    assert_assignment,
     assert_var,
     assert_expression,
     cast_nodes
@@ -6,7 +7,7 @@ from skema.program_analysis.CAST.matlab.tests.utils import (
 from skema.program_analysis.CAST2FN.model.cast import Assignment
 
 def test_binary_operator():
-    """ Test CAST from binary operation."""
+    """ Test CAST from binary operator."""
 
     source = 'z = x + y'
 
@@ -17,22 +18,54 @@ def test_binary_operator():
 
     # Left assignment operand is the variable
     assert_var(nodes[0].left, name = "z")
-
     # right assignment operand is a binary expression
     assert_expression(nodes[0].right, op = "+", left = "x", right = "y")
 
-def do_not_test_unary_operator():
-    """ Test CAST from unary operation."""
+def test_not_operator():
+    """ Test CAST from matrix not operator."""
 
-    source = 'z = -6'
+    source = 'a = ~mat_val'
 
-    # cast nodes should be one assignment
     nodes = cast_nodes(source)
     assert len(nodes) == 1
-    assert isinstance(nodes[0], Assignment)
 
-    # Left assignment operand is the variable
-    assert_var(nodes[0].left, name = "z")
+    # The expression becomes a CAST Assignment
+    assert_assignment(nodes[0], left = "a", right = "~mat_val")
 
-    # right assignment operand is a unary expression
-    assert_expression(nodes[0].right, op = "-", left = "x", right = "y")
+def test_unary_operator_literal():
+    """ Test CAST from unary operator."""
+
+    source = 'x = -6'
+
+    nodes = cast_nodes(source)
+    assert len(nodes) == 1
+
+    # Test with literal
+    assert_assignment(nodes[0], left = "x", right = "-6")
+
+def test_unary_operator_identifier():
+    """ Test CAST from unary operator."""
+
+    source = 'y = -x'
+
+    nodes = cast_nodes(source)
+    assert len(nodes) == 1
+
+    # Test with identifier
+    assert_assignment(nodes[0], left = "y", right = "-x")
+
+def test_unary_operator():
+    """ Test CAST from unary operator."""
+
+    source = """
+        x = -6
+        y = -x
+        end
+    """
+
+    nodes = cast_nodes(source)
+    # assert len(nodes) == 2
+
+    # Test with literal and identifier
+    assert_assignment(nodes[0], left = 'x', right = '-6')
+    assert_assignment(nodes[1], left = 'y', right = '-x')
