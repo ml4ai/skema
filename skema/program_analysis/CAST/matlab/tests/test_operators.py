788f2c93b83b6172578f348de8168de205d48fdd
Joseph Astier <jastier@arizona.edu>
2023-11-12 21:04:37 -0700

    Added no_test cases for unsupported operator tests

--- skema/program_analysis/CAST/matlab/tests/test_operators.py
+++ skema/program_analysis/CAST/matlab/tests/test_operators.py
@@ -8,7 +8,7 @@ from skema.program_analysis.CAST2FN.model.cast import Assignment
 
 def test_binary_operator():
     """ Test CAST from binary operator."""
-    nodes = cast_nodes('z = x + y')
+    nodes = cast_nodes("z = x + y")
     # CAST should be one Assignment node
     assert isinstance(nodes[0], Assignment)
     # Left assignment operand is the variable
@@ -16,12 +16,19 @@ def test_binary_operator():
     # right assignment operand represents binary operation
     assert_expression(nodes[0].right, op = "+", operands = ["x", "y"])
 
-def no_test_boolean_operator():
-    pass
+def test_boolean_operator():
+    """ Test CAST from boolean operator."""
+    nodes = cast_nodes("z = x && y")
+    # CAST should be one Assignment node
+    assert isinstance(nodes[0], Assignment)
+    # Left assignment operand is the variable
+    assert_var(nodes[0].left, name = "z")
+    # right assignment operand represents binary operation
+    assert_expression(nodes[0].right, op = "&&", operands = ["x", "y"])
 
 def test_comparison_operator(): 
-    """ Test CAST from binary operator."""
-    nodes = cast_nodes('z = x < y')
+    """ Test CAST from comparison operator."""
+    nodes = cast_nodes("z = x < y")
     # CAST should be one Assignment node
     assert isinstance(nodes[0], Assignment)
     # Left assignment operand is the variable
@@ -32,11 +39,31 @@ def test_comparison_operator():
 def no_test_not_operator():
     """ Test CAST from matrix not operator."""
     # logical matrix inversion, implement as nested for loops
-    pass
+    nodes = cast_nodes("z = x ~y")
+    # CAST should be one Assignment node
+    assert isinstance(nodes[0], Assignment)
 
 def no_test_postfix_operator():
-    pass
+    """ Test CAST from postfix operator."""
+    """"
+    SOURCE (op.m):
+    x = y'
 
+    SYNTAX TREE:
+    ERROR
+      assignment
+        identifier
+        =
+        postfix_operator
+          identifier
+          '
+    """
+    # this is a unary operator type
+
+    nodes = cast_nodes("z = y'")
+    # CAST should be one Assignment node
+    assert isinstance(nodes[0], Assignment)
+    
 def no_test_spread_operator():
     """ Test CAST from matrix not operator."""
 
@@ -68,7 +95,6 @@ def no_test_spread_operator():
 
     # right assignment operand an identifier
     assert_var(nodes[0].right, "new_beta_v1")
-
 
 def test_unary_operator():
     """ Test CAST from unary operator."""
