d193a5286b4df8859ed984071e94c5d9ce14f257
Joseph Astier <jastier@users.noreply.github.com>
2023-11-14 12:28:41 -0700

    MATLAB function call, operator and matrix support, code cleanup (#641)
    ## MATLAB function call, operator and matrix support, code cleanup
    This PR adds support for Operators, matrix literals, and function calls.
    I also got rid of some old Fortran routines that didn't work with
    MATLAB. Added support for some Operator types, we now support four of
    them and have three remaining. Those are going to have to be resolved by
    translating them to CAST loop constructs.
    
    ## Relevant features:
    - Added Operator support (Boolean, comparison, unary)
    - Added function call node support
    - Added n-dimensional matrix support for all primitive types
    - Added new CI test cases for all of the above
    - Added a file with Waterloo model Tree-sitter syntax tokens
    
    ## Bugfixes
    - Fixed a bug where CAST output was getting wrapped in an extra list.
    - Fixed a bug were the source_refs node wasn't getting included on
    certain structures. Our CI now checks them all.
    
    ## Related issues
    Resolves #590 #635 #636
    Work towards #562
    
    ---------
    
    Co-authored-by: Joseph Astier <jastier@arizona.edu>
    Co-authored-by: Gus Hahn-Powell <ghp@lum.ai>

--- skema/program_analysis/CAST/matlab/tests/test_switch.py
+++ skema/program_analysis/CAST/matlab/tests/test_switch.py
@@ -5,8 +5,8 @@ from skema.program_analysis.CAST.matlab.tests.utils import (
 )
 from skema.program_analysis.CAST2FN.model.cast import ModelIf
 
-def test_switch_single_values():
-    """ Test CAST from MATLAB switch statement."""
+def test_case_clause_1_argument():
+    """ Test CAST from single argument case clause."""
 
     source = """
     switch s
@@ -23,20 +23,21 @@ def test_switch_single_values():
     # case clause 'one'
     mi0 = cast_nodes(source)[0]
     assert isinstance(mi0, ModelIf)
-    assert_assignment(mi0.body[0], left="n", right = "1")
-    assert_expression(mi0.expr, op="==", left = "s", right = "'one'")
+    assert_expression(mi0.expr, op="==", operands = ["s", "'one'"])
+    assert_assignment(mi0.body[0], left="n", right = 1)
 
     # case clause 'two'
     mi1 = mi0.orelse[0]
     assert isinstance(mi1, ModelIf)
-    assert_assignment(mi1.body[0], left="n", right = "2")
+    assert_expression(mi1.expr, op="==", operands = ["s", "'two'"])
+    assert_assignment(mi1.body[0], left="n", right = 2)
     assert_assignment(mi1.body[1], left="x", right = "y")
 
     # otherwise clause
-    assert_assignment(mi1.orelse[0], left="n", right = "0")
+    assert_assignment(mi1.orelse[0], left="n", right = 0)
 
-def test_switch_multiple_values():
-    """ Test CAST from MATLAB switch statement."""
+def test_case_clause_n_arguments():
+    """ Test CAST from multipe argument case clause."""
 
     source = """
     switch s
@@ -52,19 +53,18 @@ def test_switch_multiple_values():
     # case clause {'one', 'two', 'three'}
     mi0 = cast_nodes(source)[0]
     assert isinstance(mi0, ModelIf)
-    assert_assignment(mi0.body[0], left="n", right = "1")
     assert_expression(
         mi0.expr,
         op="in",
-        left = 's',
-        right = ["'one'", "'two'", "'three'"]
+        operands = ['s', ["'one'", "'two'", "'three'"]]
     )
+    assert_assignment(mi0.body[0], left="n", right = 1)
 
     # case clause 2
     mi1 = mi0.orelse[0]
     assert isinstance(mi1, ModelIf)
-    assert_assignment(mi1.body[0], left="n", right = "2")
-    assert_expression(mi1.expr, op="==", left = 's', right = "2")
+    assert_expression(mi1.expr, op = "==", operands = ['s', 2])
+    assert_assignment(mi1.body[0], left="n", right = 2)
 
     # otherwise clause
-    assert_assignment(mi1.orelse[0], left="n", right = "0")
+    assert_assignment(mi1.orelse[0], left="n", right = 0)
