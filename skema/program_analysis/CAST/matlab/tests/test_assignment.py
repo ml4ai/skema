48212b8cb31711cfde3b7f510ec0489aa75d0fed
Joseph Astier <jastier@arizona.edu>
2023-11-14 03:38:55 -0700

    replaced files with those from jastier/function_call

--- skema/program_analysis/CAST/matlab/tests/test_assignment.py
+++ skema/program_analysis/CAST/matlab/tests/test_assignment.py
@@ -1,45 +1,39 @@
 from skema.program_analysis.CAST.matlab.tests.utils import (
     assert_assignment,
+    assert_identifier,
     cast_nodes
 )
 
 # Test CAST from assignment
 
-def test_literal():
-    """ Test assignment of literal types (number, string, boolean)."""
+def test_number():
+    """ Test assignment of integer and real numbers."""
+    nodes = cast_nodes("x = 5; y = 1.8")
+    assert len(nodes) == 2
+    assert_assignment(nodes[0], left = "x", right = 5)
+    assert_assignment(nodes[1], left = "y", right = 1.8)
 
+def test_string():
+    """ Test assignment of single and double quoted strings."""
     source = """
-        x = 5
-        y = 1.8
-        x = 'single'
-        y = "double"
-        yes = true
-        no = false
+    x = 'single'
+    y = "double"
     """
-    
     nodes = cast_nodes(source)
-    assert len(nodes) == 6
+    assert len(nodes) == 2
+    assert_assignment(nodes[0], left = "x", right = "'single'")
+    assert_assignment(nodes[1], left = "y", right = "\"double\"") 
 
-    # number
-    assert_assignment(nodes[0], left = "x", right = "5")
-    assert_assignment(nodes[1], left = "y", right = "1.8")
-    # string
-    assert_assignment(nodes[2], left = "x", right = "'single'")
-    assert_assignment(nodes[3], left = "y", right = "\"double\"")
-    # boolean
-    assert_assignment(nodes[4], left = 'yes', right = 'true')
-    assert_assignment(nodes[5], left = 'no', right = 'false')
-
+def test_boolean():
+    """ Test assignment of literal boolean types. """
+    nodes = cast_nodes("x = true; y = false")
+    assert len(nodes) == 2
+    assert_assignment(nodes[0], left = "x", right = "True")
+    assert_assignment(nodes[1], left = "y", right = "False")
 
 def test_identifier():
     """ Test assignment of identifiers."""
-
-    source = """
-        x = y
-    """
-
-    nodes = cast_nodes(source)
-    assert len(nodes) == 1
-
-    # identifier
+    nodes = cast_nodes("x = y; r = x")
+    assert len(nodes) == 2
     assert_assignment(nodes[0], left = 'x', right = 'y')
+    assert_assignment(nodes[1], left = 'r', right = 'x')
