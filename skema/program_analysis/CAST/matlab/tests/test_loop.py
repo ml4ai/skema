f4eedfc0a93a60d1e658ed26fdeb0f546626b51f
Joseph Astier <jastier@users.noreply.github.com>
2023-11-17 16:55:53 -0700

    Matrix and list handling bugfixes, improved testing (#656)
    ## Matrix and list handling bugfixes, improved testing
    This PR fixes an issue with matrix handling where nested empty matrices
    would be represented as a single empty matrix. We now show the empty
    matrices in their correct nested positions. Also some better CI testing.
    Instead of picking off individual fields to test, the only input now is
    another instance of the same class the way it should look, and the code
    will check it in one go. Added function call tests with operators and
    other functions as arguments.
    
    ## Relevant features
    - Correct representation of empty matrices.
    - CI testing with class instances.  More test cases.
    
     ## Related issues
    Partially addresses #562
    
    ---------
    
    Co-authored-by: Joseph Astier <jastier@arizona.edu>

--- skema/program_analysis/CAST/matlab/tests/test_loop.py
+++ skema/program_analysis/CAST/matlab/tests/test_loop.py
@@ -1,28 +1,13 @@
-from skema.program_analysis.CAST.matlab.tests.utils import (
-    assert_assignment,
-    cast_nodes
-)
+from skema.program_analysis.CAST.matlab.tests.utils import (check, cast)
+from skema.program_analysis.CAST2FN.model.cast import Loop
 
 # Test the for loop and others
-
 def no_test_for_loop():
     """ Test the MATLAB for loop syntax elements"""
-
     source = """
         for n = 1:10
             x = do_something(n)
         end
     """
-    
-    nodes = cast_nodes(source)
-    assert len(nodes) == 6
-
-    # number
-    assert_assignment(nodes[0], left = "x", right = "5")
-    assert_assignment(nodes[1], left = "y", right = "1.8")
-    # string
-    assert_assignment(nodes[2], left = "x", right = "'single'")
-    assert_assignment(nodes[3], left = "y", right = "\"double\"")
-    # boolean
-    assert_assignment(nodes[4], left = 'yes', right = 'true')
-    assert_assignment(nodes[5], left = 'no', right = 'false')
+    nodes = cast(source)
+    check(nodes[0], Loop())
--- skema/program_analysis/CAST/matlab/tests/test_operators.py
+++ skema/program_analysis/CAST/matlab/tests/test_operators.py
@@ -1,108 +1,59 @@
-from skema.program_analysis.CAST.matlab.tests.utils import (
-    assert_assignment,
-    assert_identifier,
-    assert_expression,
-    cast_nodes
+from skema.program_analysis.CAST.matlab.tests.utils import (check, cast)
+from skema.program_analysis.CAST2FN.model.cast import (
+    Assignment,
+    Operator
 )
-from skema.program_analysis.CAST2FN.model.cast import Assignment
+
+def operator_only_test(node, operator: Operator):
+    """ An assignment of 'x = operator' is presumed """
+    check(
+        node,
+        Assignment(
+            left = "x", 
+            right = operator
+        )
+    )
 
 def test_binary_operator():
     """ Test CAST from binary operator."""
-    nodes = cast_nodes("z = x + y")
-    # CAST should be one Assignment node
-    assert isinstance(nodes[0], Assignment)
-    # Left assignment operand is the variable
-    assert_identifier(nodes[0].left, name = "z")
-    # right assignment operand represents binary operation
-    assert_expression(nodes[0].right, op = "+", operands = ["x", "y"])
+    nodes = cast("x = y + z")
+    operator_only_test(nodes[0], Operator(op = "+", operands = ["y", "z"]))
 
 def test_boolean_operator():
     """ Test CAST from boolean operator."""
-    nodes = cast_nodes("z = x && y")
-    # CAST should be one Assignment node
-    assert isinstance(nodes[0], Assignment)
-    # Left assignment operand is the variable
-    assert_identifier(nodes[0].left, name = "z")
-    # right assignment operand represents binary operation
-    assert_expression(nodes[0].right, op = "&&", operands = ["x", "y"])
+    nodes = cast("x = yes && no")
+    operator_only_test(nodes[0], Operator(op = "&&", operands = ["yes", "no"]))
+
+def test_unary_operator():
+    """ Test CAST from unary operator."""
+    nodes = cast("x = -6;")
+    operator_only_test(nodes[0], Operator(op = "-", operands = [6]))
 
 def test_comparison_operator(): 
     """ Test CAST from comparison operator."""
-    nodes = cast_nodes("z = x < y")
-    # CAST should be one Assignment node
-    assert isinstance(nodes[0], Assignment)
-    # Left assignment operand is the variable
-    assert_identifier(nodes[0].left, name = "z")
-    # right assignment operand represents comparison operation
-    assert_expression(nodes[0].right, op = "<", operands = ["x", "y"])
+    nodes = cast("x = y < 4")
+    operator_only_test(nodes[0], Operator(op = "<", operands = ["y", 4]))
+
+# no test
 
 def no_test_not_operator():
     """ Test CAST from matrix not operator."""
     # logical matrix inversion, implement as nested for loops
-    nodes = cast_nodes("z = x ~y")
+    nodes = cast("z = x ~y")
     # CAST should be one Assignment node
-    assert isinstance(nodes[0], Assignment)
+    assert_assignment(nodes[0], left = "z")
 
 def no_test_postfix_operator():
     """ Test CAST from postfix operator."""
-    """"
-    SOURCE (op.m):
-    x = y'
-
-    SYNTAX TREE:
-    ERROR
-      assignment
-        identifier
-        =
-        postfix_operator
-          identifier
-          '
-    """
     # this is a unary operator type
-
-    nodes = cast_nodes("z = y'")
+    nodes = cast("z = y'")
     # CAST should be one Assignment node
-    assert isinstance(nodes[0], Assignment)
+    assert_assignment(nodes[0], left = "z")
     
 def no_test_spread_operator():
     """ Test CAST from matrix not operator."""
-
-    """
-    SOURCE:
-    beta_v1(:,1) = new_beta_v1;
-
-    SYNTAX TREE:
-    assignment
-        function_call
-            identifier
-            (
-            arguments
-                spread_operator
-                    :
-                ,
-                number
-            )
-        =
-        identifier
-    ;
-    """
-
-    nodes = cast_nodes("beta_v1(:,1) = new_beta_v1;")
+    nodes = cast("beta_v1(:,1) = new_beta_v1;")
     # CAST should be one Assignment node
-    assert isinstance(nodes[0], Assignment)
-    # Left assignment operand a function call
-    # assert_identifier(nodes[0].left, name = "a")
 
     # right assignment operand an identifier
     assert_identifier(nodes[0].right, "new_beta_v1")
-
-def test_unary_operator():
-    """ Test CAST from unary operator."""
-    nodes = cast_nodes("x = -6;")
-    # CAST should be one Assignment node
-    assert isinstance(nodes[0], Assignment)
-    # Left assignment operand is the variable
-    assert_identifier(nodes[0].left, name = "x")
-    # right assignment operand is a unary operator
-    assert_expression(nodes[0].right, op = "-", operands = [6])
-
