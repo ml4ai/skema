b8084871717e41165968a2e87ca186e9cac8ba3d
Gus Hahn-Powell <ghp@lum.ai>
2023-11-17 19:51:07 -0700

    Use local skema-rs service in CI testing of Python (#668)
    ## Summary of Changes
    
    - backgrounds skema-rs REST API in CI for testing Python-based REST
    workflows
    - adds a field to PR template to list related or resolved issues
    - log value of `SKEMA_RS_ADDRESS` env variable in python unit tests that
    use it
    
    ### Related issues
    
    Resolves #667

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -1,3 +1,6 @@
 ## Summary of Changes
 
 
+### Related issues
+
+Resolves ???
--- skema/rest/tests/test_model_to_amr.py
+++ skema/rest/tests/test_model_to_amr.py
@@ -11,6 +11,7 @@ from skema.rest.workflows import (
     code_snippets_to_pn_amr,
 )
 from skema.rest.llm_proxy import Dynamics
+from skema.rest.proxies import SKEMA_RS_ADDESS
 from skema.skema_py.server import System
 
 CHIME_SIR_URL = (
@@ -40,7 +41,7 @@ def test_any_amr_chime_sir():
                 .splitlines(keepends=True)[line_begin:line_end]
             )
         ]
-
+    print(f"SKEMA_RS_ADDESS:\t{SKEMA_RS_ADDESS}")
     amr = asyncio.run(
         code_snippets_to_pn_amr(
             System(
