f580fa9dd72179ff810d63f3046faaa468a1465d
Joseph Astier <jastier@arizona.edu>
2023-11-18 22:28:04 -0700

    Removed redundant assignments in matrix and operator tests

--- skema/program_analysis/CAST/matlab/tests/test_operators.py
+++ skema/program_analysis/CAST/matlab/tests/test_operators.py
@@ -4,56 +4,34 @@ from skema.program_analysis.CAST2FN.model.cast import (
     Operator
 )
 
-def operator_only_test(node, operator: Operator):
-    """ An assignment of 'x = operator' is presumed """
-    check(
-        node,
-        Assignment(
-            left = "x", 
-            right = operator
-        )
-    )
-
 def test_binary_operator():
     """ Test CAST from binary operator."""
-    nodes = cast("x = y + z")
-    operator_only_test(nodes[0], Operator(op = "+", operands = ["y", "z"]))
+    check(cast("y + z;")[0], Operator(op = "+", operands = ["y","z"]))
 
 def test_boolean_operator():
     """ Test CAST from boolean operator."""
-    nodes = cast("x = yes && no")
-    operator_only_test(nodes[0], Operator(op = "&&", operands = ["yes", "no"]))
+    check(cast("yes && no;")[0], Operator(op = "&&", operands = ["yes","no"]))
 
 def test_unary_operator():
     """ Test CAST from unary operator."""
-    nodes = cast("x = -6;")
-    operator_only_test(nodes[0], Operator(op = "-", operands = [6]))
+    check(cast("-6;")[0], Operator(op = "-", operands = [6]))
 
 def test_comparison_operator(): 
     """ Test CAST from comparison operator."""
-    nodes = cast("x = y < 4")
-    operator_only_test(nodes[0], Operator(op = "<", operands = ["y", 4]))
+    check(cast("y < 4;")[0], Operator(op = "<", operands = ["y", 4]))
 
-# no test
-
-def no_test_not_operator():
+def test_not_operator():
     """ Test CAST from matrix not operator."""
-    # logical matrix inversion, implement as nested for loops
-    nodes = cast("z = x ~y")
-    # CAST should be one Assignment node
-    assert_assignment(nodes[0], left = "z")
+    check(cast("~y")[0], Operator(op = "~", operands = ["y"]))
 
-def no_test_postfix_operator():
+def test_postfix_operator():
     """ Test CAST from postfix operator."""
-    # this is a unary operator type
-    nodes = cast("z = y'")
-    # CAST should be one Assignment node
-    assert_assignment(nodes[0], left = "z")
+    check(cast("y'")[0], Operator(op = "'", operands = ["y"]))
     
 def no_test_spread_operator():
-    """ Test CAST from matrix not operator."""
+    """ Test CAST from spread operator."""
     nodes = cast("beta_v1(:,1) = new_beta_v1;")
     # CAST should be one Assignment node
 
     # right assignment operand an identifier
-    assert_identifier(nodes[0].right, "new_beta_v1")
+    check_operator(nodes[0].right, "new_beta_v1")
