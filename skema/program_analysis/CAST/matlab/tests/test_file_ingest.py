96f7054dd3315e5929b4588411815e3ba866e072
Joseph Astier <jastier@arizona.edu>
2023-11-19 11:39:16 -0700

    Added a CI test for reading file input

---
+++ skema/program_analysis/CAST/matlab/tests/test_file_ingest.py
@@ -0,0 +1,14 @@
+from skema.program_analysis.CAST.matlab.matlab_to_cast import MatlabToCast
+from skema.program_analysis.CAST.matlab.tests.utils import (check, cast)
+import os
+
+from skema.program_analysis.CAST2FN.model.cast import Assignment
+
+def test_file_ingest():
+    """ Test the ability of the CAST translator to read from a file"""
+
+    filename = "data/matlab.m"
+    cast = MatlabToCast(source_path = filename).out_cast
+    module = cast.nodes[0]
+    nodes = module.body
+    check(nodes[0], Assignment(left = "y", right = "b"))
