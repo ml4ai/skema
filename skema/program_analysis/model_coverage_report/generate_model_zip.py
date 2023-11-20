dd7b5773da7c51ceeeb49e6519f2273978944201
Vincent Raymond <vincent@lum.ai>
2023-11-20 08:44:39 -0500

    Adding model zip archive generation script

--- skema/program_analysis/model_coverage_report/generate_model_zip.py
+++ skema/program_analysis/model_coverage_report/generate_model_zip.py
@@ -1,15 +1,27 @@
 import os
-import shutil
 import argparse
 from zipfile import ZipFile
-from tempfile import TemporaryDirectory
 from pathlib import Path
 
 from skema.skema_py.server import SUPPORTED_FILE_EXTENSIONS
 
 def filter_and_copy_files(src_dir, zip_file, target_extensions):
+    """Create zip archive for model, filtering out unsupported file types."""
     with ZipFile(zip_file, 'w') as zip:
         for root, dirs, files in os.walk(src_dir):
+            for dir_name in dirs:
+                if dir_name.startswith("include_"):
+                    dir_path = Path(root) / dir_name
+                    arcname = dir_path.relative_to(src_dir)
+                    zip.write(dir_path, arcname=arcname)
+
+                    # Iterate over files in the "include_" directory and add them to the zip
+                    for subdir_root, subdir_dirs, subdir_files in os.walk(dir_path):
+                        for file in subdir_files:
+                            file_path = Path(subdir_root) / file
+                            arcname = file_path.relative_to(src_dir)
+                            zip.write(file_path, arcname=arcname)
+
             for file in files:
                 file_path = Path(root) / file
                 file_extension = file_path.suffix
