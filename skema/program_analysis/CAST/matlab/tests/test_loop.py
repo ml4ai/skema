c5a98777bfa80850c4a73fe3ee8b40caa4eb552b
Joseph Astier <jastier@arizona.edu>
2023-11-20 17:10:14 -0700

    Early Loop construction

--- skema/program_analysis/CAST/matlab/tests/test_loop.py
+++ skema/program_analysis/CAST/matlab/tests/test_loop.py
@@ -1,8 +1,12 @@
 from skema.program_analysis.CAST.matlab.tests.utils import (check, cast)
-from skema.program_analysis.CAST2FN.model.cast import Loop
+from skema.program_analysis.CAST2FN.model.cast import (
+    Assignment,
+    Loop,
+    Operator
+)
 
-# Test the for loop and others
-def no_test_for_loop():
+# Test the for loop
+def no_test_for():
     """ Test the MATLAB for loop syntax elements"""
     source = """
         for n = 1:10
@@ -11,3 +15,28 @@ def no_test_for_loop():
     """
     nodes = cast(source)
     check(nodes[0], Loop())
+
+# Test the for loop
+def test_while():
+    """ Test the MATLAB for loop syntax elements"""
+    source = """
+        while k < 5
+            k = k + 2;
+            disp(k);
+        end
+    """
+    nodes = cast(source)
+    check(nodes[0], 
+        Loop(
+            pre = [],
+            expr = Operator(op = "<", operands = ["k",5]),
+            body = [
+                Assignment(
+                    left = "k",
+                    right = Operator(op = "+", operands = ["k",2])
+                )
+            ],
+            post = []
+        )
+    )
+
