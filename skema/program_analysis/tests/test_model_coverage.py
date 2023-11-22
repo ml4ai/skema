c0d14f6613740e05bfa4c05e9ef56d058902a8c6
Tito Ferra <tito@lum.ai>
2023-11-22 16:04:20 -0700

    modified coverage script, added test file for coverage regression

---
+++ skema/program_analysis/tests/test_model_coverage.py
@@ -0,0 +1,58 @@
+import json
+import pytest 
+import os
+
+# Dictionary that maps
+# "model" : [supported lines, total lines]
+# Last updated: November 22nd, 2023
+# (Initial Update)
+ALL_MODELS = {
+    "CHIME-penn-full": [1080, 1080], 
+    "CHIME-SIR": [633, 633], 
+    "CHIME-SVIIvR": [539, 539], 
+    "ABM-COmplexVID-19": [1133, 1133], 
+    "ABM-COVID-ABS": [1094, 2729], 
+    "MechBayes": [0, 0], 
+    "SIDARTHE": [193, 193], 
+    "Simple-SIR": [53, 53], 
+    "Climlab-v1": [4306, 4306], 
+    "Generated-Halfar": [128, 128], 
+    "SV2AIR3-Waterloo-MATLAB": [0, 1020], 
+    "Bucky": [7537, 7537], 
+    "ABM-REINA": [2622, 7078], 
+    "Cornell-COVID19-sim-Frazier": [7250, 8725], 
+    "ABM-Covasim": [14734, 31042], 
+    "TIE-GCM": [10316, 209076]
+}
+
+REPORTS_FILE_PATH = "/Users/ferra/Desktop/Work_Repos/skema/reports"
+# os.getenv("GITHUB_WORKSPACE")
+LINE_COVERAGE_FILE_NAME = "line_coverage.json"
+
+TEST_COVERAGE_LOCATION = os.path.join(REPORTS_FILE_PATH, LINE_COVERAGE_FILE_NAME)
+
+
+def load_line_coverage_information():
+    """ 
+        Loads the most recently generated test coverage 
+        information
+    """
+    return json.load(open(TEST_COVERAGE_LOCATION))
+
+def test_all_models():
+    """
+        Tests the coverage of every model we have support for
+    """
+    coverage_models = load_line_coverage_information()
+    for model in ALL_MODELS.keys():
+        baseline_supported_lines, baseline_total_lines = ALL_MODELS[model]
+        current_supported_lines, current_total_lines = coverage_models[model]
+        assert current_supported_lines >= baseline_supported_lines, f"model {model} supported line count has decreased"
+        assert current_total_lines >= baseline_total_lines, f"model {model} total line count has decreased"
+
+    return True
+
+def main():
+    test_all_models()
+
+main()
