c46438aa58088fda1d9934cdbfdb88ebcbdd008e
Tito Ferra <tito@lum.ai>
2023-12-14 17:50:41 -0700

    fixed some formatting

--- skema/program_analysis/gromet_wire_diagnosis.py
+++ skema/program_analysis/gromet_wire_diagnosis.py
@@ -7,11 +7,8 @@ from skema.gromet.metadata import SourceCodeReference
 # Check bf call FN indices
 # Boxes associated with ports
 
-def extract_metadata():
-    pass
-
 def disp_wire(wire):
-    return f"src:{wire.src}--tgt:{wire.tgt}"
+    return f"src:{wire.src}<-->tgt:{wire.tgt}"
 
 def get_length(gromet_item):
     # For any gromet object we can generically retrieve the length, since they all exist
@@ -25,29 +22,31 @@ def check_wire(gromet_wire, src_port_count, tgt_port_count, wire_type = "", meta
     error_detected = False
     if gromet_wire.src < 0:
         error_detected = True
-        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has negative src port")
+        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has negative src port.")
     if gromet_wire.src == 0:
         error_detected = True
-        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has zero src port")
+        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has zero src port.")
     if gromet_wire.src > src_port_count:
         error_detected = True
-        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has a src port that goes over the boundary of {src_port_count} src ports")
+        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has a src port that goes over the boundary of {src_port_count} src ports.")
 
     if gromet_wire.tgt < 0:
         error_detected = True
-        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has negative tgt port")
+        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has negative tgt port.")
     if gromet_wire.tgt == 0:
         error_detected = True
-        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has zero tgt port")
+        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has zero tgt port.")
     if gromet_wire.tgt > tgt_port_count:
         error_detected = True
-        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has a tgt port that goes over the boundary of {tgt_port_count} tgt ports")
+        print(f"Gromet Wire {wire_type} {disp_wire(gromet_wire)} has a tgt port that goes over the boundary of {tgt_port_count} tgt ports.")
+
 
     if error_detected:
-        if metadata:
+        if metadata == None:
             print("No line number information exists for this particular wire!")
         else:
-            print(f"Wire is associated with source code lines start:{-1} end:{-1}")
+            print(f"Wire is associated with source code lines start:{metadata.line_begin} end:{metadata.line_end}")
+        print()
 
 def find_metadata_idx(gromet_fn):
     """
@@ -68,8 +67,6 @@ def find_metadata_idx(gromet_fn):
 
 def analyze_fn_wiring(gromet_fn, metadata_collection):
     # Acquire information for all the ports, if they exist
-    metadata_idx = find_metadata_idx(gromet_fn)
-
     pif_length = get_length(gromet_fn.pif)
     pof_length = get_length(gromet_fn.pof)
     opi_length = get_length(gromet_fn.opi)
@@ -79,17 +76,22 @@ def analyze_fn_wiring(gromet_fn, metadata_collection):
     pic_length = get_length(gromet_fn.pic)
     poc_length = get_length(gromet_fn.poc)
     
+    # Find a SourceCodeReference metadata that we can extract line number information for
+    # so we can display some line number information about potential errors in the wiring 
+    # NOTE: Can we make this extraction more accurate?
+    metadata_idx = find_metadata_idx(gromet_fn)
     metadata = None
-    for md in metadata_collection[metadata_idx - 1]:
-        if isinstance(md, SourceCodeReference):
-            metadata = md
+    if metadata_idx != None:
+        for md in metadata_collection[metadata_idx - 1]:
+            if isinstance(md, SourceCodeReference):
+                metadata = md
 
     wopio_length = get_length(gromet_fn.wopio)
     if wopio_length > 0:
         for wire in gromet_fn.wff:
             check_wire(wire, opo_length, opi_length, "wff", metadata)
 
-    ######################## bl wiring
+    ######################## loop (bl) wiring
 
     wlopi_length = get_length(gromet_fn.wlopi)
     if wlopi_length > 0:
@@ -116,7 +118,7 @@ def analyze_fn_wiring(gromet_fn, metadata_collection):
         for wire in gromet_fn.wlopo:
             check_wire(wire, opo_length, pol_length, "wlopo", metadata)
 
-    ######################## bf wiring
+    ######################## function (bf) wiring
     wfopi_length = get_length(gromet_fn.wfopi)
     if wfopi_length > 0:
         for wire in gromet_fn.wfopi:
@@ -142,7 +144,7 @@ def analyze_fn_wiring(gromet_fn, metadata_collection):
         for wire in gromet_fn.wfopo:
             check_wire(wire, opo_length, pof_length, "wfopo", metadata)
 
-    ######################## bc wiring
+    ######################## condition (bc) wiring
     wcopi_length = get_length(gromet_fn.wcopi)
     if wcopi_length > 0:
         for wire in gromet_fn.wcopi:
@@ -182,9 +184,6 @@ def wiring_analyzer(gromet_obj):
         # Analyze the rest of the FN_array
         for fn in module.fn_array:
             analyze_fn_wiring(fn, module.metadata_collection) 
-
-def analyize_all_modules(gromet_collection):
-    pass
 
 def get_args():
     parser = argparse.ArgumentParser(
