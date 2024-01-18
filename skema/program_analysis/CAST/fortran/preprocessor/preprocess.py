d4f9651b7f891cc1241e156cf41cf16b953ae88a
Vincent Raymond <vincent@lum.ai>
2024-01-18 18:25:42 -0500

    Updating gcc invocation to support general gcc compilers

--- skema/program_analysis/CAST/fortran/preprocessor/preprocess.py
+++ skema/program_analysis/CAST/fortran/preprocessor/preprocess.py
@@ -173,7 +173,7 @@ def fix_include_directives(source: str) -> str:
 def run_c_preprocessor(source: str, include_base_path: Path) -> str:
     """Run the gcc c-preprocessor. Its run from the context of the include_base_path, so that it can find all included files"""
     result = run(
-        ["gcc", "-cpp", "-E", "-x", "f95", "-"],
+        ["gfortran", "-cpp", "-E", "-x", "f95", "-"],
         input=source,
         text=True,
         capture_output=True,
